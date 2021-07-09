e0a226927895b6ceb8b89dae23ea8a4a41c9048e
Kristian Suhartono <blackxephos2@gmail.com>
2021-07-09 10:06:25 +0800

    added edit and delete functionality to form editor

--- ui/src/components/formCreator/FormCreator.js
+++ ui/src/components/formCreator/FormCreator.js
@@ -14,15 +14,43 @@ const styles = {
 
 const FormCreator = props => {
   const [formData, setFormData] = useState([])
-  const { register, watch, handleSubmit } = useForm();
+  const [editData, setEditData] = useState(null)
+  const { register, watch, setValue, handleSubmit } = useForm();
 
   // Handler for when the form is submitted
   const onSubmit = data => {
     // Format the data
-    data.options = data.options.split(";")
-    let temp = formData
-    temp.push(data)
-    setFormData([...temp])
+    if (data.options) {
+      data.options = data.options.split(";")
+    }
+    let temp = [...formData]
+
+    if(editData) {
+      temp.splice(editData, 1, data)
+      setEditData(null)
+    }
+    else {
+      temp.push(data)
+    }
+    setFormData(temp)
+  }
+
+  const onEdit = e => {
+    const temp = formData[e.target.value]
+    setEditData(e.target.value)
+    setValue("fieldName", temp.fieldName)
+    setValue("fieldType", temp.fieldType)
+    if (temp.options) {
+      setValue("options", temp.options.join(';'))
+    }
+  }
+
+  const onDelete = e => {
+    if (window.confirm("Are you sure you want to delete this?")) {
+      let temp = [...formData]
+      temp.splice(e.target.value, 1)
+      setFormData(temp)
+    }
   }
 
   // Watch this to conditionally render custom things
@@ -37,6 +65,7 @@ const FormCreator = props => {
       opts.map(option => {
         let o = <option value={option}> {option} </option>
         items.push(o)
+        return o
       }) 
       
       inputField = <select {...register(fieldName)}> {items} </select>
@@ -45,8 +74,11 @@ const FormCreator = props => {
       inputField = []
       
       opts.map(option => {
-        let o = <input {...register} type="radio" value={option} />
+        let o = <input {...register} type="radio" id={fieldName+option} value={option} />
+        let l = <label for={fieldName+option}>{option}</label>
         inputField.push(o)
+        inputField.push(l)
+        return o
       }) 
     }
     else if (fieldType === "textarea") {
@@ -63,6 +95,11 @@ const FormCreator = props => {
   return (
     <div>
       <form onSubmit={handleSubmit(onSubmit)}>
+        {editData && (
+          <div>
+            <label>Currently editing field: {formData[editData].fieldName} </label>
+          </div>
+        )}
         <div>
           <label>Field Name</label>
           <input {...register("fieldName")} />
@@ -84,19 +121,29 @@ const FormCreator = props => {
             <label for="options">Enter options separated by ;</label>
           </div>
         )}
-        <input type="submit" value="Add New Field" />
+        <input type="submit" value="Save Field Data" />
       </form>
-
-      <form>
+      
+      <div>
         {
           formData.map( (fieldData, i) => (
             <div> 
               <label>{ fieldData.fieldName }</label>
               {createFormField(fieldData.fieldName, fieldData.fieldType, fieldData.options)} 
+              <button href="" 
+                value={i}
+                onClick={onEdit}>
+                  Edit
+              </button>
+              <button href="" 
+                value={i} 
+                onClick={onDelete}>
+                  Delete
+              </button>
             </div>
           ))
         }
-      </form>
+      </div>
       
     </div>
   );
