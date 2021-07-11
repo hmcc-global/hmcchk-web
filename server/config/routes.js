484c31fedeab1e21a00dd3c0eb3e24a70ac28107
tamara <tamarayustian@gmail.com>
2021-07-11 17:25:15 +0800

    testing add user API

--- server/config/routes.js
+++ server/config/routes.js
@@ -23,6 +23,7 @@ module.exports.routes = {
   'get /api/sermons/get-sermon-by-id' : 'sermons/get-sermon-by-id',
   'get /api/sermons/get-speaker' : 'sermons/get-speaker',
   'get /api/sermons/get-sermon-series' : 'sermons/get-sermon-series',
+  'post /api/users/add-user': 'users/add-user',
 
   /***************************************************************************
   *                                                                          *
