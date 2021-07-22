1269cbfe34dde721cf909099511378d4ea7a1fa0
tamara <tamarayustian@gmail.com>
2021-07-22 21:58:38 +0800

    update descriptions

--- server/api/controllers/users/get-user.js
+++ server/api/controllers/users/get-user.js
@@ -1,31 +1,19 @@
 module.exports = {
-<<<<<<< HEAD
-  friendlyName: "View users",
+  friendlyName: "Get users",
 
   description: "",
-=======
-  friendlyName: 'View users',
-
-  description: '',
->>>>>>> 7737213a820b65013d4eb59906db101cc8d74d67
 
   inputs: {
     userId: {
       required: false,
-<<<<<<< HEAD
       type: "string",
     },
-=======
-      type: 'string'
-    }
->>>>>>> 7737213a820b65013d4eb59906db101cc8d74d67
   },
 
   exits: {
     success: {
       description: "User account returned successfully",
     },
-<<<<<<< HEAD
     invalid: {
       description: "Failed to retrieve user account",
     },
@@ -35,17 +23,6 @@ module.exports = {
       if (userId) {
         let data = await User.find(userId);
         if (data.length === 0) throw "user not found";
-=======
-    invalid : {
-      description: "Failed to retrieve user account",
-    }
-  },
-  fn: async function({ userId }, exits) {
-    try {
-      if (userId) {
-        let data = await User.find(userId);
-        if (data.length === 0) throw('user not found');
->>>>>>> 7737213a820b65013d4eb59906db101cc8d74d67
         return exits.success(data);
       }
 
@@ -55,10 +32,5 @@ module.exports = {
       sails.log(err);
       return exits.invalid(err);
     }
-<<<<<<< HEAD
   },
 };
-=======
-  }
-}
->>>>>>> 7737213a820b65013d4eb59906db101cc8d74d67
