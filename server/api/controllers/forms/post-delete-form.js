95b5e49b4755c341ae35cb5c01570a700143c2b9
Kristian Suhartono <blackxephos2@gmail.com>
2021-07-24 18:30:36 +0800

    cleaned up soft-delete code

--- server/api/controllers/forms/post-delete-form.js
+++ server/api/controllers/forms/post-delete-form.js
@@ -15,15 +15,10 @@ module.exports = {
 
   fn: async function ({ id }, exits) {
     try {
-      let temp = await Form.find().where({ id: id, isDeleted: false });
-      if (!temp) {
+      const data = await Form.updateOne("id").set({isDeleted: true});
+      if (!data) {
         return exits.error("Invalid id");
       }
-
-      temp = temp[0];
-      temp = { formName: temp.formName, formFields: temp.formFields };
-      temp.isDeleted = true;
-      const data = await Form.updateOne(id).set(temp);
 
       return exits.success(id);
     } catch (err) {
