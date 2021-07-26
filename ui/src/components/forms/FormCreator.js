50c092d1f06b97038abeec0e6c5fa785f002b4b3
Kristian Suhartono <blackxephos2@gmail.com>
2021-07-26 23:35:24 +0800

    exposed form links

--- ui/src/components/forms/FormCreator.js
+++ ui/src/components/forms/FormCreator.js
@@ -168,6 +168,13 @@ const FormCreator = (props) => {
     }
   };
 
+  const validateDemoForm = (data, e) => {
+    alert("SUCCESS!! :-)\n\n" + JSON.stringify(data, null, 4));
+    reset();
+
+    return false;
+  };
+
   // Watch this to conditionally render custom things
   const ft = watch("fieldType");
 
