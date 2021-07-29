aff3264dbfa4a43d6fd05476e9af9a489a66756b
Kristian Suhartono <blackxephos2@gmail.com>
2021-07-29 11:00:43 +0800

    added prefillable form fields, fixed bug on delete API

--- ui/src/components/forms/Form.js
+++ ui/src/components/forms/Form.js
@@ -4,9 +4,10 @@ import { useForm } from "react-hook-form";
 const Form = (props) => {
   const { formName, formDescription, formImage, formData, submitHandler } =
     props;
-  const { register, reset, handleSubmit, formState } = useForm();
+  const { register, handleSubmit, formState } = useForm();
   const { errors } = formState;
 
+  // String conversion tools
   const camelize = (str) => {
     return str
       .replace(/(?:^\w|[A-Z]|\b\w)/g, function (word, index) {
@@ -14,6 +15,34 @@ const Form = (props) => {
       })
       .replace(/\s+/g, "");
   };
+
+  function sentencize(str) {
+    return str
+      .split(/([A-Z]|\d)/)
+      .map((v, i, arr) => {
+        // If first block then capitalise 1st letter regardless
+        if (!i) return v.charAt(0).toUpperCase() + v.slice(1);
+        // Skip empty blocks
+        if (!v) return v;
+        // Underscore substitution
+        if (v === "_") return " ";
+        // We have a capital or number
+        if (v.length === 1 && v === v.toUpperCase()) {
+          const previousCapital = !arr[i - 1] || arr[i - 1] === "_";
+          const nextWord =
+            i + 1 < arr.length && arr[i + 1] && arr[i + 1] !== "_";
+          const nextTwoCapitalsOrEndOfString =
+            i + 3 > arr.length || (!arr[i + 1] && !arr[i + 3]);
+          // Insert space
+          if (!previousCapital || nextWord) v = " " + v;
+          // Start of word or single letter word
+          if (nextWord || (!previousCapital && !nextTwoCapitalsOrEndOfString))
+            v = v.toLowerCase();
+        }
+        return v;
+      })
+      .join("");
+  }
 
   // Helper function to create the input fields
   const createFormField = (fieldData) => {
@@ -24,12 +53,14 @@ const Form = (props) => {
 
     let inputField = [];
 
-    if (fieldType === "select") {
+    if (fieldType === "prefill") {
+      return;
+    } else if (fieldType === "select") {
       let items = [];
 
       opts.map((option) => {
         let o = (
-          <option key={camelize(fieldName + option)} value={option}>
+          <option id={camelize(fieldName + option)} value={option}>
             {" "}
             {option}{" "}
           </option>
@@ -40,7 +71,7 @@ const Form = (props) => {
 
       inputField.push(
         <select
-          key={camelize(fieldName)}
+          id={camelize(fieldName)}
           {...register(fieldName, { required: required })}
         >
           {" "}
@@ -51,7 +82,7 @@ const Form = (props) => {
       opts.map((option) => {
         let o = (
           <input
-            key={camelize(fieldName + option)}
+            id={camelize(fieldName + option)}
             {...register(fieldName, { required: required })}
             type="radio"
             id={fieldName + option}
@@ -60,7 +91,7 @@ const Form = (props) => {
         );
         let l = (
           <label
-            key={camelize(fieldName + option + "label")}
+            id={camelize(fieldName + option + "label")}
             htmlFor={camelize(fieldName + option)}
           >
             {option}
@@ -73,7 +104,7 @@ const Form = (props) => {
     } else if (fieldType === "textarea") {
       inputField.push(
         <textarea
-          key={camelize(fieldName)}
+          id={camelize(fieldName)}
           {...register(fieldName, { required: required })}
         />
       );
@@ -81,20 +112,75 @@ const Form = (props) => {
       inputField.push(<h3>{fieldName}</h3>);
     } else {
       inputField.push(
-        <input
-          key={camelize(fieldName)}
-          type={fieldType}
-          {...register(fieldName, { required: required })}
-        />
+        createGeneralInputField(fieldName, fieldType, { required: required })
       );
     }
 
     if (required === true) {
-      let errorElement = fieldName + " is required";
-      inputField.push(<span>{errors[fieldName] && errorElement}</span>);
+      inputField.push(createErrorNotifier(fieldName));
     }
 
     return inputField;
+  };
+
+  // Custom changes for prefill form fields
+  const createPrefillFormFields = (fieldData) => {
+    if (!fieldData) return;
+    let fieldType = fieldData.fieldType;
+
+    if (fieldType !== "prefill") return;
+
+    let opts = fieldData.options;
+    let result = [];
+    result.push(<h3>Personal Information</h3>);
+
+    opts.forEach((fieldName) => {
+      // Generate a label
+      let label = (
+        <label id={fieldName + "label"}>{sentencize(fieldName)}</label>
+      );
+
+      // Generate the field itself
+      let field = null;
+      if (fieldName === "address") {
+        field = (
+          <textarea
+            key={camelize(fieldName)}
+            {...register(fieldName, { required: true })}
+          />
+        );
+      } else {
+        field = createGeneralInputField(fieldName, "text", { required: true });
+      }
+
+      // Generate validation errors
+      let error = createErrorNotifier(fieldName);
+
+      result.push(
+        <div>
+          {label}
+          {field}
+          {error}
+        </div>
+      );
+    });
+
+    return result;
+  };
+
+  const createErrorNotifier = (fieldName) => {
+    let errorElement = sentencize(fieldName) + " is required";
+    return <span>{errors[fieldName] && errorElement}</span>;
+  };
+
+  const createGeneralInputField = (fieldName, fieldType, validation) => {
+    return (
+      <input
+        key={camelize(fieldName)}
+        type={fieldType}
+        {...register(fieldName, validation)}
+      />
+    );
   };
 
   return (
@@ -102,16 +188,17 @@ const Form = (props) => {
       {formImage !== "" && <img src={formImage} />}
       <h1>{formName}</h1>
       <p>{formDescription}</p>
+      {formData && createPrefillFormFields(formData[0])}
       {formData.map((fieldData, i) => (
         <div key={fieldData.fieldName + i}>
-          {fieldData.fieldType !== "header" && (
-            <label key={camelize(fieldData.fieldName + "label")}>
+          {!["header", "prefill"].includes(fieldData.fieldType) && (
+            <label id={camelize(fieldData.fieldName + "label")}>
               {fieldData.fieldName}
             </label>
           )}
           {createFormField(fieldData)}
           {fieldData.fieldDescription !== "" && (
-            <label key={camelize(fieldData.fieldName + "description")}>
+            <label id={camelize(fieldData.fieldName + "description")}>
               {fieldData.fieldDescription}
             </label>
           )}
--- ui/src/components/forms/UserFormContainer.js
+++ ui/src/components/forms/UserFormContainer.js
@@ -1,8 +1,6 @@
 import React, { useState, useEffect } from "react";
-import { useForm } from "react-hook-form";
 import { withStyles } from "@material-ui/core/styles";
 import axios from "axios";
-import { Link } from "react-router-dom";
 import Form from "./Form";
 
 const styles = {};
