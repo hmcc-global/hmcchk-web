08304c4872764fec4e64751481b24285db83e8a1
Terence Leung <5566422+tyleung@users.noreply.github.com>
2021-08-14 03:19:10 +0800

    add FAQ section

---
+++ ui/src/components/visit-us/VisitUsPage.js
@@ -0,0 +1,12 @@
+import { Container } from "@chakra-ui/react";
+import Faq from "./Faq";
+
+const VisitUsPage = (props) => {
+  return (
+    <Container maxW="container.xl">
+      <Faq />
+    </Container>
+  );
+};
+
+export default VisitUsPage;
