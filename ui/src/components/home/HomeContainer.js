47385ddc1a2e78a8288f8749cdf6d4face929e28
Albert Paredandan <albertparedandan@gmail.com>
2021-08-13 22:32:57 +0800

    GH-54: Add 404 error page

--- ui/src/components/home/HomeContainer.js
+++ ui/src/components/home/HomeContainer.js
@@ -4,7 +4,6 @@ import EventsSection from "./EventsSection";
 import HeroSection from "./HeroSection";
 import LifeGroupSection from "./LifeGroupSection";
 import NewHereSection from "./NewHereSection";
-
 const HomeContainer = (props) => {
   return (
     <Flex direction="column">
