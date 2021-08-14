c0be3ebd5ef147b0d87e3e89d2f675f68788b585
Richie Wee Salim <ricia1212@gmail.com>
2021-08-14 22:30:23 +0800

    recreate from old branch
    svg not showing, waiting fixes
    
        pick 82f50972 recreate from old branch
        pick 5decc083 fixed image showing issue
        pick b7e2bda7 fixed footer pading and added inside xl container
    	modified:   ui/package.json
    	modified:   ui/src/App.js
    	modified:   ui/src/components/MainContainer.js
    	new file:   ui/src/components/NavigationBar/MainMenu.js
    	new file:   ui/src/components/NavigationBar/NavBar.js
    	new file:   ui/src/components/NavigationBar/hmcc-logo.svg
    	new file:   ui/src/components/NavigationBar/main-menu.css
    	new file:   ui/src/components/NavigationBar/menu.svg
    	new file:   ui/src/components/NavigationBar/ripple.png
    	new file:   ui/src/components/NavigationBar/ripple.svg
    	new file:   ui/src/components/NavigationBar/svg.svg
    	new file:   ui/src/components/footer/Apple_Store.png
    	new file:   ui/src/components/footer/ChurchAppDownloadButton.js
    	new file:   ui/src/components/footer/Copyright.js
    	new file:   ui/src/components/footer/FooterContainer.js
    	new file:   ui/src/components/footer/FooterHeading.js
    	new file:   ui/src/components/footer/Google_Play.png
    	new file:   ui/src/components/footer/LinkGrid.js
    	new file:   ui/src/components/footer/SoapDownloadButton.js
    	new file:   ui/src/components/footer/SocialMediaLinks.js
    	new file:   ui/src/components/footer/hmcc-logo.svg
    
       pick 061e99be recreate from old branch
       pick f655e6d6 fixed image showing issue
       pick e75425b9 fixed footer pading and added inside xl container
    	modified:   ui/package.json
    	modified:   ui/src/App.js
    	modified:   ui/src/components/MainContainer.js
    	new file:   ui/src/components/NavigationBar/MainMenu.js
    	new file:   ui/src/components/NavigationBar/NavBar.js
    	new file:   ui/src/components/NavigationBar/hmcc-logo.svg
    	new file:   ui/src/components/NavigationBar/main-menu.css
    	new file:   ui/src/components/NavigationBar/menu.svg
    	new file:   ui/src/components/NavigationBar/ripple.png
    	new file:   ui/src/components/NavigationBar/ripple.svg
    	new file:   ui/src/components/NavigationBar/svg.svg
    	new file:   ui/src/components/footer/Apple_Store.png
    	new file:   ui/src/components/footer/ChurchAppDownloadButton.js
    	new file:   ui/src/components/footer/Copyright.js
    	new file:   ui/src/components/footer/FooterContainer.js
    	new file:   ui/src/components/footer/FooterHeading.js
    	new file:   ui/src/components/footer/Google_Play.png
    	new file:   ui/src/components/footer/LinkGrid.js
    	new file:   ui/src/components/footer/SoapDownloadButton.js
    	new file:   ui/src/components/footer/SocialMediaLinks.js
    	new file:   ui/src/components/footer/hmcc-logo.svg

--- ui/src/components/MainContainer.js
+++ ui/src/components/MainContainer.js
@@ -18,6 +18,7 @@ import UserFormContainer from "./forms/UserFormContainer";
 import NoMatch from "./errors/NoMatch";
 import AdminLoginContainer from "./admin/AdminLoginContainer";
 import AdminContainer from "./AdminContainer";
+import FooterContainer from "./footer/FooterContainer";
 import ScrollToTop from "./helpers/ScrollToTop";
 
 const MainContainer = () => {
