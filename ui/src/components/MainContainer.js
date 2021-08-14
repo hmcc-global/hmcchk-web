4b514773528ff336560a97083c761280d9b5865e
Terence Leung <5566422+tyleung@users.noreply.github.com>
2021-08-15 03:43:36 +0800

    Add experience hmcc section

--- ui/src/components/MainContainer.js
+++ ui/src/components/MainContainer.js
@@ -8,6 +8,7 @@ import LoginContainer from "./auth/LoginContainer";
 import FormManager from "./forms/FormManager";
 import EventsPage from "./events/EventsPage";
 import VisitUsPage from "./visit-us/VisitUsPage";
+import ConnectPage from "./connect/ConnectPage";
 import PrivateRoute from "./helpers/PrivateRoute";
 import Signup from "./auth/Signup";
 import HomeContainer from "./home/HomeContainer";
@@ -70,6 +71,12 @@ const MainContainer = () => {
           path="/visit-us"
           permissions={["public"]}
           component={VisitUsPage}
+        />
+        <PrivateRoute
+          exact
+          path="/connect"
+          permissions={["public"]}
+          component={ConnectPage}
         />
       </Switch>
       <FooterContainer />
