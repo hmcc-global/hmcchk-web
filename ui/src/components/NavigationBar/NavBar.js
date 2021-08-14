7d5760ba4d47c6d0cd5e74cd08379502de97a126
Richie Wee Salim <ricia1212@gmail.com>
2021-08-14 19:51:53 +0800

    Navbar done mobile and website
    hmcc svg on navbar is still white, cant change it to black

--- ui/src/components/NavigationBar/NavBar.js
+++ ui/src/components/NavigationBar/NavBar.js
@@ -23,7 +23,7 @@ import {Link} from 'react-router-dom';
 import MainMenu from './MainMenu';
 
 const Navigationbar = () => {
-	const [loggedIn, setLoggedIn] = useState(false);
+	const [loggedIn, setLoggedIn] = useState(true);
 	const username = "name"
 	const welcomeMsg = ["Login or Sign up", `Hi ${username}`]
 	const { isOpen, onOpen, onClose } = useDisclosure()
@@ -32,41 +32,52 @@ const Navigationbar = () => {
              border: "5px", 
 						 backgroundColor:"#EB4335", 
 						 color:"white",
-						 fontSize: "13px"
+						 fontSize: "xs",
+						 justify:'center',
+						 fontWeight:'600',
+             align:'center'
 	};
 
   const handleLogin = () => {
     //waiting for login cookie
   }
 
-  let currDate = new Date().toDateString().substr(0,3);
+	let currDate = new Date().toDateString().substr(0,3);
 
-	return (
-		<>
+		return(
+			<>
 			<Flex
 				w='100vw'
-				bgGradient="linear(to-b, #C4C4C4, transparent)"
-				align='center'
-				color='gray.100'
+				background = "rgba(0, 0, 0, 0.4)"
 				justify='center'
+				>
+				<Flex 
+				w='100vw' 
+				justify='space-around' 
+				background = "linear-gradient(180deg, #FFFFFF 0%, rgba(255, 255, 255, 0.5) 100%, rgba(255, 255, 255, 0.9) 100%)"
+				backdrop-filter= "blur(39px)" 
+				fontFamily='inter'
 				align='center'
-				fontSize='md'
-				h='7vh'
-				p={2}>
-				<Flex w='100vw' justify='space-around'>
+				fontSize={{md:'xs', lg:'sm', xl:'md'}}
+				h='10.5vh'>
 					<Center>
 						<Box position='relative'>
 							<Link href='/'>
-								<Image h='4vh' src='hmcc-logo.svg' alt='Logo of HMCC' />
+								<Image 
+								h={{base:'3.5vh', sm:'4vh', md:'4.5vh',lg:'5vh',xl:'6vh'}} 
+								src='hmcc-logo.svg' 
+								alt='Logo of HMCC' />
 							</Link>
 						</Box> 
 					</Center>
 					<Stack
-						spacing={8}
+						fontWeight='600'
+						spacing={10}
 						color="black"
 						justify='center'
 						align='center'
-            marginLeft="100px"
+						display={{base:'none',md:'flex'}}
+            marginLeft="40px"
 						isInline>
 						<Box
 							position='relative'
@@ -116,8 +127,9 @@ const Navigationbar = () => {
 						color="black"
 						justify='right'
 						align='right'
+						alignItems='center'
 						isInline>
-						<Box marginTop="8px">
+						<Box fontWeight='600' color='#1A365D' display={{base:'none',md:'flex'}}>
 						{loggedIn ? 
 							<Menu>
 								<MenuButton>
@@ -131,40 +143,35 @@ const Navigationbar = () => {
 						</Box>
 						<Box>
 							<Button ref={btnRef} onClick={onOpen} style={{background:"none"}}>  
-								<Image h='4vh' src='menu.svg' alt='Menu Button' />
+								<Image h='5vh' src='menu.svg' alt='Menu Button' />
 							</Button> 
 						</Box>
 					</Stack>
 				</Flex>
 			</Flex>
-      {currDate == 'Sun' ?
+      {currDate == 'Tue' ?
         <Flex
           w='100vw'
-          bgGradient="white"
+          background ="#ffffff"
           border = "1px solid #E2E8F0"
-          box-shadow = "0px 1px 3px rgba(0, 0, 0, 0.1), 0px 1px 2px rgba(0, 0, 0, 0.06)"
+          box-shadow = "0px 4px 4px rgba(0, 0, 0, 0.25), 0px 1px 3px rgba(0, 0, 0, 0.1)"
           border-radius= "4px"
           align='center'
           color='gray.100'
           justify='center'
           align='center'
-          fontSize='md'
-          h='5.5vh'
+          h='6vh'
           p={2}>
           <Flex w='100vw' justify='space-around'>
             <Stack
-              spacing={8}
-              color="black"
               justify='center'
               align='center'
               isInline>
-              <Box
-                position='relative'
-                >
-                <Link href='/live'>
-                  <Button h='25px' style={liveScStyle}>WATCH SUNDAY CELEBRATION LIVE</Button>
+              <Center>
+                <Link href='/live' style={{lineHeight:"0"}}>
+                  <Button h='3.5vh' style={liveScStyle} fontSize='xs' lineHeight='0' w='95%' >WATCH SUNDAY CELEBRATION LIVE</Button>
                 </Link>
-              </Box>
+              </Center>
             </Stack>
           </Flex>
         </Flex> : null
@@ -177,16 +184,16 @@ const Navigationbar = () => {
       >
 			<DrawerOverlay />
 			<DrawerContent>
-				<DrawerCloseButton position='absolute' right='5%' top='5%' />
+				<DrawerCloseButton position='absolute' right='5%' top='5%' color='white' />
 				<DrawerHeader />
 				<DrawerBody>
 					<MainMenu login = {loggedIn}/>
 				</DrawerBody>
-				<DrawerFooter />
+				<DrawerFooter fontSize='sm' color='white' justifyContent='center' >Harvest Mission Community Church 2021</DrawerFooter>
 			</DrawerContent>
     </Drawer>
 		</>
-	);
-};
+		);
+	};
 
 export default Navigationbar;
