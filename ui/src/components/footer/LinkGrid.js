5c0c38203a917e312ec0895b048a2d70bf2018c4
bhryck <randall2000n@gmail.com>
2021-08-15 09:47:09 +0800

    fixed image showing issue

--- ui/src/components/footer/LinkGrid.js
+++ ui/src/components/footer/LinkGrid.js
@@ -1,7 +1,6 @@
 import { Box, SimpleGrid, Stack, Link, Text } from "@chakra-ui/react";
 import { Link as HashLink } from "react-router-dom";
 import * as React from "react";
-<<<<<<< HEAD
 
 export function LinkGrid() {
   return (
@@ -75,51 +74,6 @@ export function LinkGrid() {
           <Link href="https://hongkong.sub.hmcc.net/privacy-policy-2/">
             <Text fontWeight="bold">Privacy Policy</Text>
           </Link>
-=======
-import { FooterHeading } from "./FooterHeading";
-import { createBreakpoints } from "@chakra-ui/theme-tools";
-
-const breakpoints = createBreakpoints({
-  sm: "30em",
-  md: "48em",
-  lg: "62em",
-  xl: "80em",
-  "2xl": "96em",
-});
-export function LinkGrid() {
-  return (
-    <SimpleGrid columns={[2, 4, 4]} textAlign="left" minW="50%">
-      <Box minW="130px">
-        <Stack>
-          <Text fontWeight="bold">Visit</Text>
-          <Link>Church online</Link>
-        </Stack>
-      </Box>
-      <Box minW="130px">
-        <Stack>
-          <Text fontWeight="bold">Connect</Text>
-          <Link>Ministries</Link>
-          <Link>LIFE Groups</Link>
-        </Stack>
-      </Box>
-      <Box minW="130px">
-        <Stack>
-          <Text fontWeight="bold">About</Text>
-          <Link>Who we are</Link>
-          <Link>Beliefs</Link>
-          <Link>Our Values</Link>
-          <Link>Statement of Faith</Link>
-          <Link>Bold Vision</Link>
-          <Link>HMI</Link>
-        </Stack>
-      </Box>
-
-      <Box minW="130px">
-        <Stack>
-          <Link fontWeight="bold">Events</Link>
-          <Link fontWeight="bold">Sermons</Link>
-          <Link fontWeight="bold">Give</Link>
->>>>>>> 0d99933 (recreate from old branch)
         </Stack>
       </Box>
     </SimpleGrid>
