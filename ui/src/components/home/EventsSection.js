2ca5efc890d252d3fe24a0e735c17d5bc080e9e7
Albert Paredandan <albertparedandan@gmail.com>
2021-08-15 15:14:36 +0800

    GH-54: Add event section

--- ui/src/components/home/EventsSection.js
+++ ui/src/components/home/EventsSection.js
@@ -1,9 +1,55 @@
-import { Flex } from "@chakra-ui/react";
+import {
+  chakra,
+  Box,
+  Container,
+  Flex,
+  HStack,
+  Image,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
+import FeaturedEvent from "./FeaturedEvent";
 
 const EventsSection = () => {
   return (
-    <Flex w="full" h="100vh">
-      <h1 color="black">Events</h1>
+    <Flex w="full" h="100vh" direction="column">
+      <Container
+        maxW={["container.md", "container.lg"]}
+        justifyContent="center"
+        display="flex"
+        h="50%"
+      >
+        <VStack>
+          <FeaturedEvent />
+        </VStack>
+      </Container>
+      <Box
+        w="full"
+        height="50vh"
+        display="flex"
+        justifyContent="flex-start"
+        overflowX="auto"
+        overflowY="hidden"
+        whiteSpace="nowrap"
+      >
+        <HStack spacing={4} padding={5} justifyContent="flex-start">
+          <Box border="1px" bgColor="grey" w="500px" h={500 * 0.618}>
+            TEST
+          </Box>
+          <Box border="1px" bgColor="grey" w="500px" h={500 * 0.618}>
+            TEST
+          </Box>
+          <Box border="1px" bgColor="grey" w="500px" h={500 * 0.618}>
+            TEST
+          </Box>
+          <Box border="1px" bgColor="grey" w="500px" h={500 * 0.618}>
+            TEST
+          </Box>
+          <Box border="1px" bgColor="grey" w="500px" h={500 * 0.618}>
+            TEST
+          </Box>
+        </HStack>
+      </Box>
     </Flex>
   );
 };
---
+++ ui/src/components/home/FeaturedEvent.js
@@ -0,0 +1,49 @@
+import {
+  Box,
+  Container,
+  Flex,
+  Heading,
+  HStack,
+  Image,
+  Text,
+  chakra,
+} from "@chakra-ui/react";
+
+const scEventText = "Sunday Celebration: 10AM HKT in-person & Online";
+const FeaturedEvent = (props) => {
+  return (
+    <Flex w="full" h="100vh" justify="center">
+      <Box w="auto" mt="-10vh">
+        <HStack whiteSpace="nowrap">
+          <Text color="white">{scEventText}</Text>
+          <chakra.hr width="full" color="white" />
+          <Text color="white">HMCC of Hong Kong</Text>
+        </HStack>
+        {/* TODO figure out a way to put a placeholder container */}
+        <Image
+          borderRadius={10}
+          borderWidth={1}
+          borderColor="white"
+          mt={5}
+          w="auto"
+          h="50vh"
+          src="https://hongkong.hmcc.net/wp-content/uploads/whats-new-sss-final.png"
+          alt="Featured Event"
+        />
+        <HStack marginTop="2vh" whiteSpace="nowrap">
+          <Heading color="black">Upcoming Events</Heading>
+          <chakra.hr
+            width="full"
+            color="black"
+            border="none"
+            height="2px"
+            bgColor="black"
+          />
+          <Text color="black">all events ></Text>
+        </HStack>
+      </Box>
+    </Flex>
+  );
+};
+
+export default FeaturedEvent;
