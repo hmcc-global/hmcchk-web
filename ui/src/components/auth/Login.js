af8c1633b6586e6f29239e3fd68eda7271aabbb7
Richie <ricia1212@gmail.com>
2021-08-18 22:15:54 +0800

    Form validation done

--- ui/src/components/auth/Login.js
+++ ui/src/components/auth/Login.js
@@ -21,7 +21,7 @@ import {
 import { Route } from "react-router";
 
 const Login = (props) => {
-  const { register, handleSubmit } = useForm();
+  const { register, handleSubmit, formState:{errors} } = useForm();
   const [result, setResult] = useState("");
 
   const onSubmit = (data) => {
@@ -82,7 +82,6 @@ const Login = (props) => {
         background="#2C5282"
         color="white"
         padding="20px"
-        h='100vh'
       >
         <Flex>
           <Box>
@@ -106,18 +105,40 @@ const Login = (props) => {
             </Text>
             <form onSubmit={handleSubmit(onSubmit)} autoComplete="off">
               <VStack align="stretch" marginTop="40px">
+              <input 
+                    id='email'
+                    name = 'email'
+                    type='email' 
+                    placeholder='Email' 
+                    style={inputBoxStyle}
+                    {...register('email', {
+                      required: "Required",  
+                      pattern:{
+                        value:/(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/ ,
+                        message: "Invalid email address"
+                      }
+                    })} 
+                  />
+                  {errors.email  && (
+                  <Text color="#FED7D7" fontWeight='bold' fontSize={[12, 12, 12, 14]}>
+                    {errors.email.message}
+                  </Text>
+                )}
                 <input
-                  type="email"
-                  {...register("email", { required: true })}
-                  placeholder="Email"
-                  style={inputBoxStyle}
-                />
-                <input
+                  id="password"
                   type="password"
-                  {...register("password", { required: true })}
+                  name="password"
                   placeholder="Password"
                   style={inputBoxStyle}
+                  {...register("password", {
+                    required: "Required",
+                  })}
                 />
+                {errors.password && (
+                  <Text color="#FED7D7" fontWeight='bold' fontSize={[12, 12, 12, 14]}>
+                    {errors.password.message}
+                  </Text>
+                )}
                 <Link>
                   <Text textAlign="right" fontSize="xs" position="relative">
                     Forgot Password?
