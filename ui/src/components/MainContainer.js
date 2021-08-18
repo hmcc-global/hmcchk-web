d156b05362c806a3fc0e492589dd6448ff5cc87b
Albert Paredandan <albertparedandan@gmail.com>
2021-08-18 21:55:26 +0800

    GH-76: Add privateRoutes

--- ui/src/components/MainContainer.js
+++ ui/src/components/MainContainer.js
@@ -1,6 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Switch, Redirect, Route } from "react-router-dom";
+import { Switch, Redirect } from "react-router-dom";
 import { chakra } from "@chakra-ui/react";
 import SermonContainer from "./sermons/SermonContainer";
 import SermonCard from "./sermons/SermonCard";
@@ -8,18 +8,55 @@ import LoginContainer from "./auth/LoginContainer";
 import FormManager from "./forms/FormManager";
 import EventsPage from "./events/EventsPage";
 import VisitUsPage from "./visit-us/VisitUsPage";
+import PrivateRoute from "./helpers/PrivateRoute";
+import HomeContainer from "./home/HomeContainer";
 
 const MainContainer = () => {
   return (
     <chakra.main flexGrow={1} bg="#ffffff" overflowY="auto">
       <Switch>
-        <Route exact path="/login" component={LoginContainer} />
-        <Route exact path="/sermons" component={SermonContainer} />
-        <Route exact path="/sermons/:id" component={SermonCard} />
-        <Route exact path="/forms/" component={FormManager} />
-        <Route exact path="/events/" component={EventsPage} />
-        <Route exact path="/visit-us" component={VisitUsPage} />
-        <Redirect to="/sermons" />
+        <PrivateRoute
+          exact
+          path="/"
+          permissions={["public"]}
+          component={HomeContainer}
+        />
+        <PrivateRoute
+          exact
+          path="/login"
+          permissions={["noUser"]}
+          component={LoginContainer}
+        />
+        <PrivateRoute
+          exact
+          path="/sermons"
+          permissions={["public"]}
+          component={SermonContainer}
+        />
+        <PrivateRoute
+          exact
+          path="/sermons/:id"
+          permissions={["public"]}
+          component={SermonCard}
+        />
+        <PrivateRoute
+          exact
+          path="/forms/"
+          permissions={["unsigned", "signed", "alumni", "admin", "stewardship"]}
+          component={FormManager}
+        />
+        <PrivateRoute
+          exact
+          path="/events/"
+          permissions={["public"]}
+          component={EventsPage}
+        />
+        <PrivateRoute
+          exact
+          path="/visit-us"
+          permissions={["public"]}
+          component={VisitUsPage}
+        />
       </Switch>
     </chakra.main>
   );
