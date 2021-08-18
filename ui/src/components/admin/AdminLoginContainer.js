22c3af54a3612976961c3dff9eb8d90c0fb3075b
tamara <tamarayustian@gmail.com>
2021-08-19 02:00:11 +0800

    login error msg done

--- ui/src/components/admin/AdminLoginContainer.js
+++ ui/src/components/admin/AdminLoginContainer.js
@@ -5,28 +5,60 @@ import {
   Input,
   useColorMode,
   useColorModeValue,
+  useToast,
 } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
-import { Switch, Redirect, Route } from "react-router-dom";
+import { useForm } from "react-hook-form";
 import axios from "axios";
+import { Redirect } from "react-router";
 import GoogleLogin from "react-google-login";
 
 const AdminLoginContainer = (props) => {
   const { toggleColorMode } = useColorMode();
   const formBackground = useColorModeValue("gray.100", "gray.700");
+  const toast = useToast();
 
   const { classes } = props;
-  const [token, setToken] = useState("null token");
+  const [token, setToken] = useState(null);
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm();
 
-  const postLogin = async () => {
+  const postLogin = async (user) => {
+    console.log(user);
+
     try {
-      const { data } = await axios.post("/api/auth/login", {
-        emailAddress: "elon@example.com",
-        password: "123456",
-        fullName: "Elon Musk",
+      const result = await axios.post("/api/auth/login", {
+        emailAddress: user.emailAddress,
+        password: user.password,
       });
-      setToken(data);
+      console.log(result);
+
+      if (result.status === 200) {
+        if (
+          result.data.accessType == "admin" ||
+          result.data.accessType == "stewardship"
+        ) {
+          return <Redirect to="/admin/home" />;
+        } else {
+          toast({
+            title: "Access denied.",
+            status: "error",
+            duration: 3000,
+            isClosable: true,
+          });
+        }
+      }
     } catch (err) {
+      toast({
+        title: "Something went wrong.",
+        description: "Try again.",
+        status: "error",
+        duration: 3000,
+        isClosable: true,
+      });
       console.log(err);
     }
   };
@@ -36,19 +68,28 @@ const AdminLoginContainer = (props) => {
   return (
     <Flex height="85vh" alignItems="center" justifyContent="center">
       <Flex direction="column" bg={formBackground} p={12} rounded={6}>
-        <Heading mb={6} alignItems="center">
-          HMCC-HK Admin
-        </Heading>
-        <Input placeholder="Email" variant="filled" mb={3} type="email"></Input>
-        <Input
-          placeholder="Password"
-          variant="filled"
-          mb={6}
-          type="password"
-        ></Input>
-        <Button mb={6} colorScheme="teal">
-          Log in
-        </Button>
+        <form onSubmit={handleSubmit(postLogin)}>
+          <Heading mb={6} alignItems="center">
+            HMCC-HK Admin
+          </Heading>
+          <Input
+            placeholder="Email"
+            variant="filled"
+            mb={3}
+            type="email"
+            {...register("emailAddress")}
+          ></Input>
+          <Input
+            placeholder="Password"
+            variant="filled"
+            mb={6}
+            type="password"
+            {...register("password")}
+          ></Input>
+          <Button mb={6} colorScheme="teal" type="submit">
+            Log in
+          </Button>
+        </form>
         <Button onClick={toggleColorMode}>Toggle Color Mode</Button>
       </Flex>
     </Flex>
