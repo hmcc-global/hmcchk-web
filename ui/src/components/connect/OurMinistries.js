9942671aa8d8abda61e5bc52c41e953c79d87bf2
Terence Leung <5566422+tyleung@users.noreply.github.com>
2021-09-12 17:46:12 +0800

    fix fliptiles

--- ui/src/components/connect/OurMinistries.js
+++ ui/src/components/connect/OurMinistries.js
@@ -19,13 +19,12 @@ const OurMinistries = (props) => {
         adults, and families.
       </Text>
       <SimpleGrid columns={[2, 4]} spacing={4}>
-        <Box className="flip-container">
-          <Center
-            h={[180, 250]}
-            p={[4, 12]}
+        <Box className="flip-container" h={[190, 250]}>
+          <Box
+            p={[3, 4]}
             borderWidth="1px"
             borderRadius="20"
-            bgImage={` linear-gradient(180deg, rgba(118, 228, 247, 0.7) 0%, rgba(44, 82, 130, 0.7) 100%), url('${process.env.PUBLIC_URL}/images/connect/campus-ministry.jpg')`}
+            bgImage={`linear-gradient(180deg, rgba(118, 228, 247, 0.7) 0%, rgba(44, 82, 130, 0.7) 100%), url('${process.env.PUBLIC_URL}/images/connect/campus-ministry.jpg')`}
             bgPosition="center"
             bgSize="cover"
             shadow="lg"
@@ -36,22 +35,24 @@ const OurMinistries = (props) => {
               as="h4"
               fontSize={["1.5em", "1.5em", "2em"]}
               fontWeight={700}
-              textAlign="center"
               className="front"
             >
               CAMPUS
             </Heading>
-            <Box className="back" fontWeight={600} fontSize={["xs", "md"]}>
+            <Box
+              className="back"
+              fontWeight={600}
+              fontSize={["xs", "sm", "md"]}
+            >
               Reaching the college students in the university campuses of Hong
               Kong because we believe that college is a pivotal time for people
               to discover their God-given destiny.
             </Box>
-          </Center>
+          </Box>
         </Box>
-        <Box className="flip-container">
-          <Center
-            h={[180, 250]}
-            p={[4, 12]}
+        <Box className="flip-container" h={[190, 250]}>
+          <Box
+            p={[3, 4]}
             borderWidth="1px"
             borderRadius="20"
             bgImage={`linear-gradient(180deg, rgba(198, 246, 213, 0.7) 0%, rgba(39, 103, 73, 0.7) 100%), url('${process.env.PUBLIC_URL}/images/connect/focus-ministry.jpg')`}
@@ -65,22 +66,24 @@ const OurMinistries = (props) => {
               as="h4"
               fontSize={["1.5em", "1.5em", "2em"]}
               fontWeight={700}
-              textAlign="center"
               className="front"
             >
               SINGLE ADULTS
             </Heading>
-            <Box className="back" fontWeight={600} fontSize={["xs", "md"]}>
+            <Box
+              className="back"
+              fontWeight={600}
+              fontSize={["xs", "sm", "md"]}
+            >
               Creating an avenue for single working adults to fellowship and
               learn how to best exercise their God-given influence in their
               respective schools and workplaces.
             </Box>
-          </Center>
+          </Box>
         </Box>
-        <Box className="flip-container">
-          <Center
-            h={[180, 250]}
-            p={[4, 12]}
+        <Box className="flip-container" h={[190, 250]}>
+          <Box
+            p={[3, 4]}
             borderWidth="1px"
             borderRadius="20"
             bgImage={`linear-gradient(180deg, rgba(252, 129, 129, 0.7) 0%, rgba(151, 38, 109, 0.7) 100%), url('${process.env.PUBLIC_URL}/images/connect/married-couples-ministry.jpg')`}
@@ -94,21 +97,23 @@ const OurMinistries = (props) => {
               as="h4"
               fontSize={["1.5em", "1.5em", "2em"]}
               fontWeight={700}
-              textAlign="center"
               className="front"
             >
               MARRIED COUPLES
             </Heading>
-            <Box className="back" fontWeight={600} fontSize={["sm", "md"]}>
+            <Box
+              className="back"
+              fontWeight={600}
+              fontSize={["xs", "sm", "md"]}
+            >
               Bringing together Godly families that will love on and impact the
               next generation of their families and city.
             </Box>
-          </Center>
+          </Box>
         </Box>
-        <Box className="flip-container">
-          <Center
-            h={[180, 250]}
-            p={[4, 12]}
+        <Box className="flip-container" h={[190, 250]}>
+          <Box
+            p={[3, 4]}
             borderWidth="1px"
             borderRadius="20"
             bgImage={`linear-gradient(180deg, rgba(254, 252, 191, 0.7) 0%, rgba(246, 173, 85, 0.7) 100%), url('${process.env.PUBLIC_URL}/images/connect/children-ministry.jpg')`}
@@ -122,20 +127,25 @@ const OurMinistries = (props) => {
               as="h4"
               fontSize={["1.5em", "1.5em", "2em"]}
               fontWeight={700}
-              textAlign="center"
               className="front"
             >
               CHILDREN
             </Heading>
-            <Box className="back" fontWeight={600} fontSize={["sm", "md"]}>
-              Building Blocks is HMCC’s Children Ministry, learn more about it{" "}
-              <Text as="u">
-                <Link href="https://hongkong.hmcc.net/building-blocks/">
-                  here ➔
-                </Link>
+            <Box
+              className="back"
+              fontWeight={600}
+              fontSize={["xs", "sm", "md"]}
+            >
+              <Text>
+                Building Blocks is HMCC’s Children Ministry, learn more about it{" "}
+                <Text as="u">
+                  <Link href="https://hongkong.hmcc.net/building-blocks/">
+                    here ➔
+                  </Link>
+                </Text>
               </Text>
             </Box>
-          </Center>
+          </Box>
         </Box>
       </SimpleGrid>
     </Box>
