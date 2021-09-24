80b6ad71f90394555701310c291a9cec9a679451
bhryck <randall2000n@gmail.com>
2021-09-24 21:05:17 +0800

    GH-31: app download button size adjusted

--- ui/src/components/footer/ChurchAppDownloadButton.js
+++ ui/src/components/footer/ChurchAppDownloadButton.js
@@ -13,7 +13,7 @@ export function ChurchAppDownloadButton() {
           href="https://tithely.app.link/harvest-mission-community-church-hong-kong"
         >
           <Image
-            h="40px"
+            h={{ base: "10", md: "9", lg: "10" }}
             objectFit="cover"
             src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3c/Download_on_the_App_Store_Badge.svg/135px-Download_on_the_App_Store_Badge.svg.png"
           />
@@ -23,7 +23,7 @@ export function ChurchAppDownloadButton() {
           href="https://tithely.app.link/harvest-mission-community-church-hong-kong"
         >
           <Image
-            h="40px"
+            h={{ base: "10", md: "9", lg: "10" }}
             objectFit="cover"
             src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/78/Google_Play_Store_badge_EN.svg/320px-Google_Play_Store_badge_EN.svg.png"
           />
--- ui/src/components/footer/SoapDownloadButton.js
+++ ui/src/components/footer/SoapDownloadButton.js
@@ -14,7 +14,7 @@ export function SoapAppDownloadButton() {
           href="https://apps.apple.com/hk/app/daily-soap-bible-reading-app/id1448825436"
         >
           <Image
-            h="40px"
+            h={{ base: "10", md: "9", lg: "10" }}
             objectFit="cover"
             src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3c/Download_on_the_App_Store_Badge.svg/135px-Download_on_the_App_Store_Badge.svg.png"
           />
@@ -24,7 +24,7 @@ export function SoapAppDownloadButton() {
           href="https://play.google.com/store/apps/details?id=net.hmcc.hongkong.dailysoap"
         >
           <Image
-            h="40px"
+            h={{ base: "10", md: "9", lg: "10" }}
             objectFit="cover"
             src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/78/Google_Play_Store_badge_EN.svg/320px-Google_Play_Store_badge_EN.svg.png"
           />
