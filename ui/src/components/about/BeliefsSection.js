491e8fe13e558b03d7773f39ece5ed1bf4426e42
bhryck <randall2000n@gmail.com>
2021-11-07 02:21:17 +0800

    beliefs, values section

--- ui/src/components/about/BeliefsSection.js
+++ ui/src/components/about/BeliefsSection.js
@@ -1,10 +1,247 @@
-import { Flex, Text } from "@chakra-ui/layout";
+import {
+  Flex,
+  Text,
+  Heading,
+  Center,
+  Box,
+  SimpleGrid,
+  Container,
+} from "@chakra-ui/react";
 
 const BeliefsSection = (props) => {
   return (
-    <Flex direction="column">
-      <Text>Our Beliefs</Text>
-    </Flex>
+    <Container maxW="container.lg">
+      <Flex direction="column">
+        <Center padding="5">
+          <Heading>Our Beliefs </Heading>
+        </Center>
+        <SimpleGrid columns={[1, 1, 3]} spacingX="1" spacingY="1">
+          <Box
+            borderWidth="1px"
+            borderRadius="20"
+            shadow="lg"
+            bg="#F8F8F8"
+            px={[4, 8]}
+            pt={[4, 6]}
+            pb={[4, 6]}
+            m={4}
+            h={["7em", "18em"]}
+            w={["13em", "19em"]}
+          >
+            <Box>
+              <Text fontWeight="bold" color="#2C5282" fontSize="xl">
+                The Bible
+              </Text>
+              <Text color="#2C5282" fontSize="sm">
+                We believe the Bible, both the Old and New Testaments, is the
+                only inspired, infallible, and authoritative Word of God. It is
+                the supreme source of truth for Christian faith and living.
+              </Text>
+            </Box>
+          </Box>
+          <Box
+            borderWidth="1px"
+            borderRadius="20"
+            shadow="lg"
+            bg="#F8F8F8"
+            px={[4, 8]}
+            pt={[4, 6]}
+            pb={[4, 6]}
+            m={4}
+            h={["7em", "18em"]}
+            w={["13em", "19em"]}
+          >
+            <Box>
+              <Text fontWeight="bold" color="#2C5282" fontSize="xl">
+                God
+              </Text>
+              <Text color="#2C5282" fontSize="sm">
+                We believe in one God, Creator of all things, infinitely perfect
+                and eternally existing in three persons: Father, Son, and Holy
+                Spirit.
+              </Text>
+            </Box>
+          </Box>
+          <Box
+            borderWidth="1px"
+            borderRadius="20"
+            shadow="lg"
+            bg="#F8F8F8"
+            px={[4, 8]}
+            pt={[4, 6]}
+            pb={[4, 6]}
+            m={4}
+            h={["7em", "18em"]}
+            w={["13em", "19em"]}
+          >
+            <Box>
+              <Text fontWeight="bold" color="#2C5282" fontSize="xl">
+                Jesus Christ
+              </Text>
+              <Text color="#2C5282" fontSize="sm">
+                We believe that Jesus Christ is true (fully) God and true
+                (fully) man, having been conceived of the Holy Spirit and born
+                of the virgin Mary. He died on the cross, the complete and final
+                sacrifice for our sins according to the Scriptures. He arose
+                bodily from the dead, and ascended into heaven where, at God’s
+                right hand, He intercedes for His people and rules as Lord over
+                all.
+              </Text>
+            </Box>
+          </Box>
+          <Box
+            borderWidth="1px"
+            borderRadius="20"
+            shadow="lg"
+            bg="#F8F8F8"
+            px={[4, 8]}
+            pt={[4, 6]}
+            pb={[4, 6]}
+            m={4}
+            h={["7em", "18em"]}
+            w={["13em", "19em"]}
+          >
+            <Box>
+              <Text fontWeight="bold" color="#2C5282" fontSize="xl">
+                The Holy Spirit
+              </Text>
+              <Text color="#2C5282" fontSize="sm">
+                We believe that the ministry of the Holy Spirit is to glorify
+                the Lord Jesus Christ, and during this age to convict the world
+                of sin. He also regenerates the believing sinner, indwelling,
+                guiding, instructing, and empowering us for godly living and
+                sacrifice.
+              </Text>
+            </Box>
+          </Box>
+          <Box
+            borderWidth="1px"
+            borderRadius="20"
+            shadow="lg"
+            bg="#F8F8F8"
+            px={[4, 8]}
+            pt={[4, 6]}
+            pb={[4, 6]}
+            m={4}
+            h={["7em", "18em"]}
+            w={["13em", "19em"]}
+          >
+            <Box>
+              <Text fontWeight="bold" color="#2C5282" fontSize="xl">
+                Man
+              </Text>
+              <Text color="#2C5282" fontSize="sm">
+                We believe that the man was created in the image of God, but
+                fell into sin and is therefore destined for eternal death. Only
+                through regeneration by the Holy Spirit can salvation and
+                spiritual life be obtained.
+              </Text>
+            </Box>
+          </Box>
+          <Box
+            borderWidth="1px"
+            borderRadius="20"
+            shadow="lg"
+            bg="#F8F8F8"
+            px={[4, 8]}
+            pt={[4, 6]}
+            pb={[4, 6]}
+            m={4}
+            h={["7em", "18em"]}
+            w={["13em", "19em"]}
+          >
+            <Box>
+              <Text fontWeight="bold" color="#2C5282" fontSize="xl">
+                Salvation
+              </Text>
+              <Text color="#2C5282" fontSize="sm">
+                We believe that the shed blood of Jesus Christ and His
+                resurrection provide the only grounds for justification and
+                salvation for all who believe, and only those who receive Jesus
+                Christ by faith are born of the Holy Spirit and thus become
+                children of God.
+              </Text>
+            </Box>
+          </Box>
+          <Box
+            borderWidth="1px"
+            borderRadius="20"
+            shadow="lg"
+            bg="#F8F8F8"
+            px={[4, 8]}
+            pt={[4, 6]}
+            pb={[4, 6]}
+            m={4}
+            h={["7em", "18em"]}
+            w={["13em", "19em"]}
+          >
+            <Box>
+              <Text fontWeight="bold" color="#2C5282" fontSize="xl">
+                Future Events
+              </Text>
+              <Text color="#2C5282" fontSize="sm">
+                We believe in the visible, personal, glorious, imminent return
+                of Jesus Christ, His bodily resurrection of the dead, the
+                judgment of the just and the unjust, and the fulfillment of
+                Christ’s Kingdom in the new heavens and the new earth. Then
+                shall the eager expectation of creation be fulfilled and the
+                whole earth shall proclaim the glory of God who makes all things
+                new.
+              </Text>
+            </Box>
+          </Box>
+          <Box
+            borderWidth="1px"
+            borderRadius="20"
+            shadow="lg"
+            bg="#F8F8F8"
+            px={[4, 8]}
+            pt={[4, 6]}
+            pb={[4, 6]}
+            m={4}
+            h={["7em", "18em"]}
+            w={["13em", "19em"]}
+          >
+            <Box>
+              <Text fontWeight="bold" color="#2C5282" fontSize="xl">
+                The Church
+              </Text>
+              <Text color="#2C5282" fontSize="sm">
+                We believe that the true Church is composed of all such persons
+                who through saving faith in Jesus Christ have been regenerated
+                by the Holy Spirit and are united together in the body of
+                Christ. We believe that Jesus Christ is the Head of the Church,
+                and that every local church has the right under Christ to decide
+                and govern its own affairs.
+              </Text>
+            </Box>
+          </Box>
+          <Box
+            borderWidth="1px"
+            borderRadius="20"
+            shadow="lg"
+            bg="#F8F8F8"
+            px={[4, 8]}
+            pt={[4, 6]}
+            pb={[4, 6]}
+            m={4}
+            h={["7em", "18em"]}
+            w={["13em", "19em"]}
+          >
+            <Box>
+              <Text fontWeight="bold" color="#2C5282" fontSize="xl">
+                Ordinances
+              </Text>
+              <Text color="#2C5282" fontSize="sm">
+                We believe that water baptism and the Lord’s Supper are
+                ordinances to be observed by the Church during the present age.
+                They are, however, not to be regarded as means of salvation.
+              </Text>
+            </Box>
+          </Box>
+        </SimpleGrid>
+      </Flex>
+    </Container>
   );
 };
 
--- ui/src/components/about/ValuesSection.js
+++ ui/src/components/about/ValuesSection.js
@@ -1,9 +1,132 @@
-import { Flex, Text } from "@chakra-ui/layout";
+import {
+  Accordion,
+  Flex,
+  TextProps,
+  MinusIcon,
+  AddIcon,
+  Text,
+  Heading,
+  Center,
+  Box,
+  AccordionItem,
+  AccordionButton,
+  AccordionPanel,
+  AccordionIcon,
+} from "@chakra-ui/react";
 
 const ValuesSection = (props) => {
   return (
     <Flex direction="column">
-      <Text>Our Values</Text>
+      <Center padding="5">
+        <Heading>Our Values </Heading>
+      </Center>
+      <Center>
+        <Accordion w={["90%", "70%"]} allowMultiple>
+          <AccordionItem border="1px solid #63B3ED" my="3" borderRadius="5">
+            <h2>
+              <AccordionButton>
+                <Box flex="1" textAlign="left" fontWeight="bold">
+                  <Text>S – Spirit-led ministry</Text>
+                </Box>
+                <AccordionIcon />
+              </AccordionButton>
+            </h2>
+            <AccordionPanel pb={4}>
+              <Text>
+                To be connected with God and anointed by His Spirit, in order to
+                hear His direction and step out in faith to minister to His
+                people with His power.
+              </Text>
+            </AccordionPanel>
+          </AccordionItem>
+
+          <AccordionItem border="1px solid #63B3ED" my="3" borderRadius="5">
+            <h2>
+              <AccordionButton>
+                <Box flex="1" textAlign="left" fontWeight="bold">
+                  <Text>L – Leadership ​development</Text>
+                </Box>
+                <AccordionIcon />
+              </AccordionButton>
+            </h2>
+            <AccordionPanel pb={4}>
+              <Text>
+                To develop people to their full AC and in Christ-like character
+                in order to impact their spheres of influence and increase the
+                spread of the Gospel to future generations.
+              </Text>
+            </AccordionPanel>
+          </AccordionItem>
+          <AccordionItem border="1px solid #63B3ED" my="3" borderRadius="5">
+            <h2>
+              <AccordionButton>
+                <Box flex="1" textAlign="left" fontWeight="bold">
+                  <Text>T – Transculturalism</Text>
+                </Box>
+                <AccordionIcon />
+              </AccordionButton>
+            </h2>
+            <AccordionPanel pb={4}>
+              <Text>
+                To make the decision to go through discomforts and difficulties,
+                in order to develop understanding and delight in people from a
+                different culture.
+              </Text>
+            </AccordionPanel>
+          </AccordionItem>
+          <AccordionItem border="1px solid #63B3ED" my="3" borderRadius="5">
+            <h2>
+              <AccordionButton>
+                <Box flex="1" textAlign="left" fontWeight="bold">
+                  <Text>C – Community</Text>
+                </Box>
+                <AccordionIcon />
+              </AccordionButton>
+            </h2>
+            <AccordionPanel pb={4}>
+              <Text>
+                To willingly come together as a diverse group of people to
+                experience and live out the Gospel, in order to fulfill God’s
+                purposes.
+              </Text>
+            </AccordionPanel>
+          </AccordionItem>
+          <AccordionItem border="1px solid #63B3ED" my="3" borderRadius="5">
+            <h2>
+              <AccordionButton>
+                <Box flex="1" textAlign="left" fontWeight="bold">
+                  <Text>M – Ministry inside and outside</Text>
+                </Box>
+                <AccordionIcon />
+              </AccordionButton>
+            </h2>
+            <AccordionPanel pb={4}>
+              <Text>
+                To be a visible display and viable demonstration of God’s
+                Kingdom by stewarding our God-given spiritual gift(s), resources
+                and experiences to build God’s Church and transform the world.
+              </Text>
+            </AccordionPanel>
+          </AccordionItem>
+          <AccordionItem border="1px solid #63B3ED" my="3" borderRadius="5">
+            <h2>
+              <AccordionButton>
+                <Box flex="1" textAlign="left" fontWeight="bold">
+                  <Text>M – Missions through church planting</Text>
+                </Box>
+                <AccordionIcon />
+              </AccordionButton>
+            </h2>
+            <AccordionPanel pb={4}>
+              <Text>
+                To obey and fulfill the Great Commission through the local
+                church, which God has established so that we can be witnesses
+                and servants of His redemptive purpose.
+              </Text>
+            </AccordionPanel>
+          </AccordionItem>
+        </Accordion>
+      </Center>
     </Flex>
   );
 };
