59ed745f521fc247430ea50e4f1278d7a98876f7
Albert Paredandan <albertparedandan@gmail.com>
2021-11-07 08:51:25 +0800

    GH-104: Add AboutUs banner and selector

--- ui/src/components/about/AboutBanner.js
+++ ui/src/components/about/AboutBanner.js
@@ -1,14 +1,40 @@
-import { Flex, Text } from "@chakra-ui/layout";
+import { Box, Container, Flex, Heading, Text, VStack } from "@chakra-ui/layout";
 
 const AboutBanner = (props) => {
   return (
-    <Flex direction="column">
-      <Text>About Us</Text>
-      {/* <Text>
-        Harvest Mission Community Church of Hong Kong is an international church
-        that was planted in 2015, when a team of 20 people.
-      </Text> */}
-    </Flex>
+    <Container maxW="container.lg" py={10}>
+      <VStack>
+        <Box
+          borderWidth="1px"
+          borderRadius="20"
+          bgImage={`url('${process.env.PUBLIC_URL}/images/visitus/header-banner.jpg')`}
+          bgPosition="center"
+          bgSize="cover"
+          px={[6, 12, 36]}
+          py={[8, 16, 20]}
+          mb={[4, 0]}
+        >
+          <Heading
+            as="h2"
+            fontSize={["4xl", "6xl"]}
+            fontWeight={700}
+            lineHeight={1}
+            color="white"
+            textAlign="center"
+            mb={5}
+          >
+            About Us
+          </Heading>
+          <Text
+            color="white"
+            fontSize={["md", "xl"]}
+            fontWeight={600}
+            textAlign="center"
+            mb={[0, 5]}
+          ></Text>
+        </Box>
+      </VStack>
+    </Container>
   );
 };
 
--- ui/src/components/about/AboutUsContainer.js
+++ ui/src/components/about/AboutUsContainer.js
@@ -1,23 +1,107 @@
-import { Flex } from "@chakra-ui/layout";
-import AboutBanner from "./AboutBanner";
+import {
+  Box,
+  Container,
+  Heading,
+  HStack,
+  Text,
+  VStack,
+} from "@chakra-ui/layout";
 import BeliefsSection from "./BeliefsSection";
 import StaffSection from "./StaffSection";
 import StorySection from "./StorySection";
 import StrategySection from "./StrategySection";
 import ValuesSection from "./ValuesSection";
 import VisionMissionSection from "./VisionMissionSection";
+import blurbs from "./about.json";
+import { useState } from "react";
+
+const sections = [
+  "Our Story",
+  "Vision & Mission",
+  "Our Strategy",
+  "Our Staff",
+  "Beliefs",
+  "Values",
+];
 
 const AboutUsContainer = (props) => {
+  const [selected, setSelected] = useState(0);
+  const banner = blurbs.banner;
+
   return (
-    <Flex direction="column" justifyContent="center">
-      <AboutBanner />
-      <StorySection />
-      <VisionMissionSection />
-      <ValuesSection />
-      <StrategySection />
-      <StaffSection />
-      <BeliefsSection />
-    </Flex>
+    <Container maxW="container.lg" py={10}>
+      <VStack>
+        <Box
+          borderWidth="1px"
+          borderRadius="20"
+          // TODO-aparedan: change bgimage
+          bgImage={`url('${process.env.PUBLIC_URL}/images/visitus/header-banner.jpg')`}
+          bgPosition="center"
+          bgSize="cover"
+          px={[6, 12, 35]}
+          py={[8, 16, 20]}
+          mb={[4, 0]}
+        >
+          <Heading
+            as="h2"
+            fontSize={["4xl", "6xl"]}
+            fontWeight={700}
+            lineHeight={1}
+            color="white"
+            textAlign="center"
+            mb={5}
+          >
+            {banner.title}
+          </Heading>
+          <Text
+            color="white"
+            fontSize={["sm", "md"]}
+            fontWeight={600}
+            textAlign="center"
+            mb={[0, 5]}
+          >
+            {banner.blurb}
+          </Text>
+          <HStack justifyContent="space-evenly">
+            {sections &&
+              sections.map((e, i) => {
+                return (
+                  <>
+                    <Text
+                      color="white"
+                      fontWeight={600}
+                      textDecoration="none"
+                      borderBottom="#FFFFFF 0.1em solid"
+                      cursor="pointer"
+                      onClick={() => setSelected(i)}
+                    >
+                      {e}
+                    </Text>
+                    {i < sections.length - 1 ? (
+                      <Text
+                        mx={[3, 4]}
+                        color="white"
+                        // display="inline-block"
+                        fontSize={["md", "2xl"]}
+                        verticalAlign="baseline"
+                      >
+                        &bull;
+                      </Text>
+                    ) : null}
+                  </>
+                );
+              })}
+          </HStack>
+          {/* TODO-aparedan: navigation menu */}
+        </Box>
+        {selected == 0 && <StorySection />}
+        {selected == 1 && <VisionMissionSection />}
+        {selected == 2 && <StrategySection />}
+        {selected == 3 && <StaffSection />}
+        {selected == 4 && <BeliefsSection />}
+        {selected == 5 && <ValuesSection />}
+      </VStack>
+    </Container>
   );
 };
 
