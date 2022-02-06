8f4854178d30b94e5d8167881e4233919e58368b
tamara <tamarayustian@gmail.com>
2022-02-07 00:36:52 +0800

    GH-186: look of about title

--- ui/src/components/retreat/retreatTheme.js
+++ ui/src/components/retreat/retreatTheme.js
@@ -1,24 +1,15 @@
 import { extendTheme } from '@chakra-ui/react';
-import '@fontsource/sora';
-import '@fontsource/inter';
 
 const retreatTheme = extendTheme({
   textStyles: {
-    title: {
-      font: 'Sora',
+    sora: {
+      fontFamily: 'Sora',
       fontSize: '50px',
-      fontWeight: 'normal',
+      fontWeight: 'bold',
       lineHeight: '95.5%',
     },
-    subtitle: {
-      font: 'Sora',
-      fontSize: '25px',
-      fontWeight: 'bold',
-      lineHeight: '35px',
-      letterSpacing: '-0.02em',
-    },
-    normal: {
-      font: 'Inter',
+    inter: {
+      fontFamily: 'Inter',
       fontSize: '25px',
       fontWeight: 'normal',
     },
