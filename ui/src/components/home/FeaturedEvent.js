b524072b0ea3d852dd2377360b82d14439ce1e69
Kristian Suhartono <blackxephos2@gmail.com>
2022-02-25 17:04:38 +0800

    GH-1: convert featured event to link

--- ui/src/components/home/FeaturedEvent.js
+++ ui/src/components/home/FeaturedEvent.js
@@ -1,43 +1,43 @@
-import { Box, Flex, Image, Text, chakra, Stack, Link } from "@chakra-ui/react";
+import { Box, Flex, Image, Text, chakra, Stack, Link } from '@chakra-ui/react';
 
 // const scEventText = "Sunday Celebration: 10AM HKT in-person & Online";
 const scEventText =
-  "Sunday Celebration: 10AM HKT Online only until further notice";
+  'Sunday Celebration: 10AM HKT Online only until further notice';
 const FeaturedEvent = (props) => {
   return (
     <Flex w="full" h="full" justify="center">
-      <Box w="full" mt={["-5em", "-8em"]}>
+      <Box w="full" mt={['-5em', '-8em']}>
         <Stack
           whiteSpace="nowrap"
           w="full"
-          direction={["column", "row"]}
+          direction={['column', 'row']}
           alignItems="center"
           fontWeight="bold"
         >
-          <Text fontSize={["0.7em", "1em"]} color="white">
+          <Text fontSize={['0.7em', '1em']} color="white">
             {scEventText}
           </Text>
-          <chakra.hr display={["none", "block"]} width="full" color="white" />
-          <Text display={["none", "block"]} color="white">
+          <chakra.hr display={['none', 'block']} width="full" color="white" />
+          <Text display={['none', 'block']} color="white">
             HMCC of Hong Kong
           </Text>
-          <Text display={["block", "none"]} fontSize="0.7em" color="white">
+          <Text display={['block', 'none']} fontSize="0.7em" color="white">
             Harvest Mission Community Church of Hong Kong
           </Text>
         </Stack>
         {/* TODO figure out a way to put a placeholder container */}
-        {/* <Link href="/giving-tuesday"> */}
-        <Image
-          borderRadius={10}
-          borderWidth={1}
-          borderColor="white"
-          mt={5}
-          w={["100vw", "100vw", "100vw", "65vw", "100vw", "100vw"]}
-          h="auto"
-          src={process.env.PUBLIC_URL + "/images/home/featured.jpeg"}
-          alt="Featured Event"
-        />
-        {/* </Link> */}
+        <Link href="/with-everything">
+          <Image
+            borderRadius={10}
+            borderWidth={1}
+            borderColor="white"
+            mt={5}
+            w={['100vw', '100vw', '100vw', '65vw', '100vw', '100vw']}
+            h="auto"
+            src={process.env.PUBLIC_URL + '/images/home/featured.jpeg'}
+            alt="Featured Event"
+          />
+        </Link>
       </Box>
     </Flex>
   );
