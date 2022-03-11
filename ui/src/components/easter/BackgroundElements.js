07e54c8bc31535de498c550c00c30b736ba81763
Kristian Suhartono <blackxephos2@gmail.com>
2022-03-12 01:55:42 +0800

    GH-272: background layouting (forced container vh to illustrate)

---
+++ ui/src/components/easter/BackgroundElements.js
@@ -0,0 +1,61 @@
+import { Image, Box } from '@chakra-ui/react';
+
+const BackgroundElements = () => {
+  return (
+    <Box h="100%" position="absolute" top="0" zIndex="0" w="100%">
+      {/* Desktop */}
+      <Image
+        position="absolute"
+        top="90vh"
+        right="0"
+        src={`${process.env.PUBLIC_URL}/images/easter/background/blue-gradient-desktop.svg`}
+        w="40%"
+        display={['none', 'block']}
+      />
+      <Image
+        position="absolute"
+        top="140vh"
+        left="0"
+        w="50%"
+        src={`${process.env.PUBLIC_URL}/images/easter/background/pink-gradient-desktop.svg`}
+        display={['none', 'block']}
+      />
+      <Image
+        position="absolute"
+        bottom="0"
+        right="0"
+        w="45%"
+        src={`${process.env.PUBLIC_URL}/images/easter/background/yellow-gradient-desktop.svg`}
+        display={['none', 'block']}
+      />
+
+      {/* Mobile */}
+      <Image
+        position="absolute"
+        top="70vh"
+        right="0"
+        src={`${process.env.PUBLIC_URL}/images/easter/background/blue-gradient-mobile.svg`}
+        w="40%"
+        display={['block', 'none']}
+      />
+      <Image
+        position="absolute"
+        top="115vh"
+        left="0"
+        w="70%"
+        src={`${process.env.PUBLIC_URL}/images/easter/background/pink-gradient-mobile.svg`}
+        display={['block', 'none']}
+      />
+      <Image
+        position="absolute"
+        bottom="0"
+        right="0"
+        w="70%"
+        src={`${process.env.PUBLIC_URL}/images/easter/background/yellow-gradient-mobile.svg`}
+        display={['block', 'none']}
+      />
+    </Box>
+  );
+};
+
+export default BackgroundElements;
--- ui/src/components/easter/index.js
+++ ui/src/components/easter/index.js
@@ -1,16 +1,29 @@
-import HeroContainer from "./hero/HeroContainer";
-import InvitationContainer from "./invitation/InvitationContainer";
-import StoryContainer from "./story/StoryContainer";
+import HeroContainer from './hero/HeroContainer';
+import InvitationContainer from './invitation/InvitationContainer';
+import StoryContainer from './story/StoryContainer';
+import { Container, Box } from '@chakra-ui/react';
+import React from 'react';
+import BackgroundElements from './BackgroundElements';
 
 const EasterContainer = (props) => {
-  console.log(props);
   return (
     <>
       <HeroContainer />
-      <StoryContainer />
-      <InvitationContainer />
+      {/* relative positioning to stack the elements */}
+      <Box w="100vw" minH="350vh" position="relative">
+        {/* z index is 0, appear behind */}
+        <BackgroundElements />
+
+        {/* z index is 1 *, appear in front */}
+        <Box w="100%" position="absolute" top="0" zIndex="1">
+          <Container maxW="container.lg">
+            <StoryContainer />
+            <InvitationContainer />
+          </Container>
+        </Box>
+      </Box>
     </>
-  )
-}
+  );
+};
 
 export default EasterContainer;
