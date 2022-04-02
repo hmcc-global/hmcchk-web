cc331b83f5ec4ae7669ff190bd1e3a6abcd58a4e
Alvin Harjanto <alvin.chip99@yahoo.com>
2022-04-03 01:58:00 +0800

    GH-311: Add Easter streaming page

--- ui/src/components/sermons/OnlinePageButtons.js
+++ ui/src/components/sermons/OnlinePageButtons.js
@@ -13,7 +13,7 @@ import {
 } from '@chakra-ui/react';
 
 const OnlinePageButtons = (props) => {
-  const { isGoodFri } = props;
+  const { isGoodFri, isEaster } = props;
   const { isOpen, onOpen, onClose } = useDisclosure();
 
   const GFConnectionButton = () => {
@@ -61,12 +61,98 @@ const OnlinePageButtons = (props) => {
     );
   }
 
+  const GFResponseCardButton = () => {
+    return (
+      <Button
+          variant="outline"
+          width={['100%', '25%']}
+          color="#935963"
+          fontSize={['sm', 'md']}
+          as="a"
+          // TODO-aparedan: Put link to response form
+          href="/give"
+          target="_blank"
+          flex="auto"
+          textStyle="Quicksand"
+          bg="white"
+        >
+          Response Card
+      </Button>
+    )
+  }
 
-  return (
-    <Stack direction={['column', 'row']}>
-      { isGoodFri ? (
-        <GFConnectionButton />
-      ) : (
+  const EasterConnectionButton = () => {
+    return (
+      <Button
+        variant="outline"
+        bg="white"
+        color="#538EC7"
+        width={['100%', '25%']}
+        fontSize={['sm', 'md']}
+        style={{
+          whiteSpace: 'normal',
+          wordWrap: 'break-word',
+        }}
+        onClick={onOpen}
+        flex="auto"
+        textStyle="Quicksand"
+      >
+        Connection Issues
+      </Button>
+    )
+  }
+
+  const EasterPrayerButton = () => {
+    return (
+      <Button
+        bg="white"
+        variant="outline"
+        width={['100%', '25%']}
+        fontSize={['sm', 'md']}
+        style={{
+          whiteSpace: 'normal',
+          wordWrap: 'break-word',
+        }}
+        align="center"
+        as="a"
+        href="https://bit.ly/hmcc-prayer"
+        target="_blank"
+        flex="auto"
+        color="#538EC7"
+        textStyle="Quicksand"
+      >
+        Need Prayer?
+      </Button>
+    );
+  }
+
+  const EasterResponseCardButton = () => {
+    return (
+      <Button
+          variant="outline"
+          width={['100%', '25%']}
+          color="#538EC7"
+          fontSize={['sm', 'md']}
+          as="a"
+          // TODO-aharjanto: Put link to response form
+          href="/give"
+          target="_blank"
+          flex="auto"
+          textStyle="Quicksand"
+          bg="white"
+        >
+          Response Card
+      </Button>
+    )
+  }
+
+  const renderConnectionButton = () => {
+    if (isGoodFri) {
+      return <GFConnectionButton />;
+    } else if (isEaster) {
+      return <EasterConnectionButton />;
+    } else {
+      return (
         <Button
           width={['100%', '25%']}
           fontSize={['sm', 'md']}
@@ -78,7 +164,47 @@ const OnlinePageButtons = (props) => {
         >
           Connection Issues
         </Button>
-      )}
+      )
+    }
+  }
+
+  const renderPrayerButton = () => {
+    if (isGoodFri) {
+      return <GFPrayerButton />;
+    } else if (isEaster) {
+      return <EasterPrayerButton />;
+    } else {
+      return (
+        <Button
+          variant="outline"
+          width={['100%', '25%']}
+          fontSize={['sm', 'md']}
+          style={{
+            whiteSpace: 'normal',
+            wordWrap: 'break-word',
+          }}
+          align="center"
+          as="a"
+          href="https://bit.ly/hmcc-prayer"
+          target="_blank"
+        >
+          Need Prayer?
+        </Button>
+      )
+    }
+  }
+
+  const renderResponseCard = () => {
+    if (isGoodFri) {
+      return <GFResponseCardButton />;
+    } else if (isEaster) {
+      return <EasterResponseCardButton />;
+    }
+  }
+
+  return (
+    <Stack direction={['column', 'row']}>
+      {renderConnectionButton()}
       <Modal size="2xl" isOpen={isOpen} onClose={onClose}>
         <ModalOverlay />
         <ModalContent>
@@ -114,42 +240,10 @@ const OnlinePageButtons = (props) => {
           <ModalFooter />
         </ModalContent>
       </Modal>
-      { isGoodFri ? (
-        <GFPrayerButton />
-      ) : (
-        <Button
-          variant="outline"
-          width={['100%', '25%']}
-          fontSize={['sm', 'md']}
-          style={{
-            whiteSpace: 'normal',
-            wordWrap: 'break-word',
-          }}
-          align="center"
-          as="a"
-          href="https://bit.ly/hmcc-prayer"
-          target="_blank"
-        >
-          Need Prayer?
-        </Button>
-      )}
-      { isGoodFri ? (
-        <Button
-          variant="outline"
-          width={['100%', '25%']}
-          color="#935963"
-          fontSize={['sm', 'md']}
-          as="a"
-          // TODO-aparedan: Put link to response form
-          href="/give"
-          target="_blank"
-          flex="auto"
-          textStyle="Quicksand"
-          bg="white"
-        >
-          Response Card
-        </Button>
-      ) : (
+      {renderPrayerButton()}
+      { (isGoodFri || isEaster) ?
+        renderResponseCard()
+      : (
         <>
           <Button
             width={['100%', '25%']}
