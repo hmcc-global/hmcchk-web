8f5770bcdf1ccc21e48e547994a25f5f5da66ba0
Kristian Suhartono <blackxephos2@gmail.com>
2022-04-17 10:07:22 +0800

    GH-362: fix sizing of popup

--- ui/src/components/easter/hero/PopUp.js
+++ ui/src/components/easter/hero/PopUp.js
@@ -12,10 +12,10 @@ import {
 const PopUp = () => {
   const [displayModal, setDisplayModal] = useState('none');
   useEffect(() => {
-    setTimeout(() =>{
-      setDisplayModal('unset')
-    }, 2000)
-  },[]);
+    setTimeout(() => {
+      setDisplayModal('unset');
+    }, 2000);
+  }, []);
 
   const closeModal = () => {
     setDisplayModal('none');
@@ -28,7 +28,8 @@ const PopUp = () => {
         right="25px"
         top="10px"
         height="auto"
-        maxW="85%"
+        maxW="30%"
+        textAlign="center"
         width={{ base: 'none', md: '200' }}
         background="#FFFFFF"
         boxShadow="0px 3px 4px 2px rgba(0, 0, 0, 0.3)"
@@ -38,41 +39,37 @@ const PopUp = () => {
         py={[2, 4]}
         zIndex="sticky"
       >
-        <Stack direction="column">
+        <Box w="100%" mb={5}>
+          <CloseButton float="right" onClick={closeModal} size="sm" />
+        </Box>
+        <Stack w="100%" direction="column">
           <Stack direction="row" spacing="auto">
-            <Text
-              fontSize="30"
-              marginTop="10px"
-              textStyle="Quicksand_bold"
-              color="#642444"
-            >
-              The online Good Friday stream is now
+            <Text fontSize="lg" textStyle="Quicksand_bold" color="#642444">
+              The online Good Friday stream is now{' '}
               <Text
-                fontSize="40"
+                fontSize="lg"
                 textStyle="Quicksand_bolder"
                 textDecorationLine="underline"
                 background="none"
-                paddingLeft="45%"
+                as="span"
               >
                 LIVE
               </Text>
             </Text>
-            <CloseButton onClick={closeModal} size="sm" />
           </Stack>
-          <Stack direction="row"></Stack>
           <Button
             alignSelf="center"
             height="auto"
             as={Link}
-            p="2"
+            px={4}
+            py={2}
             background="#E79CA6"
             color="#ffffff"
-            width="30%"
             backdropFilter="blur(6px)"
             borderRadius="10px"
             href="/online"
           >
-            <Text fontSize="20" textStyle="Quicksand_bold">
+            <Text fontSize="md" textStyle="Quicksand_bold">
               Check it out!
             </Text>
           </Button>
@@ -91,50 +88,47 @@ const PopUp = () => {
         width="90vw"
         background="#FFFFFF"
         display={{ base: displayModal, md: 'none' }}
-        p="6"
+        p="4"
         zIndex="sticky"
         boxShadow="0px 3px 4px 2px rgba(0, 0, 0, 0.3)"
         borderRadius="20px"
       >
         <Stack direction="column">
-          <Stack direction="column" spacing={0}>
-            <CloseButton onClick={closeModal} size="lg" alignSelf="flex-end" />
+          <Stack direction="column" spacing={1}>
+            <CloseButton onClick={closeModal} size="md" alignSelf="flex-end" />
             <Text
-              fontSize="25"
-              marginTop="10px"
+              fontSize="lg"
               textStyle="Quicksand_bold"
               color="#642444"
               justifyContent={'center'}
               textAlign="center"
+              lineHeight="2em"
             >
               The online Good Friday stream is now{' '}
               <Text
-                fontSize="40"
-                //paddingLeft = "35%"
+                fontSize="lg"
                 textStyle="Quicksand_bolder"
                 textDecorationLine="underline"
                 display="inline"
                 color="#642444"
+                as="span"
               >
                 LIVE
               </Text>
             </Text>
           </Stack>
-          <Center>
+          <Center w="100%">
             <Button
-              alignSelf="center"
-              height="auto"
               as={Link}
-              p="2"
+              py="2"
+              px="8"
               background="#E79CA6"
-              top="2"
               color="#ffffff"
-              width="80%"
               backdropFilter="blur(6px)"
               borderRadius="10px"
               href="/online"
             >
-              <Text fontSize="20" textStyle="Quicksand_bold">
+              <Text fontSize="md" textStyle="Quicksand_bold">
                 Check it out!
               </Text>
             </Button>
@@ -151,7 +145,8 @@ const PopUp = () => {
         right="25px"
         top="10px"
         height="auto"
-        maxW="85%"
+        maxW="30%"
+        textAlign="center"
         width={{ base: 'none', md: '200' }}
         background="#FFFFFF"
         boxShadow="0px 3px 4px 2px rgba(0, 0, 0, 0.3)"
@@ -161,41 +156,37 @@ const PopUp = () => {
         py={[2, 4]}
         zIndex="sticky"
       >
-        <Stack direction="column">
+        <Box w="100%" mb={5}>
+          <CloseButton float="right" onClick={closeModal} size="sm" />
+        </Box>
+        <Stack w="100%" direction="column">
           <Stack direction="row" spacing="auto">
-            <Text
-              fontSize="30"
-              marginTop="10px"
-              textStyle="Quicksand_bold"
-              color="#003984"
-            >
-              The online Easter Celebration stream is now
+            <Text fontSize="lg" textStyle="Quicksand_bold" color="#003984">
+              The online Easter Celebration stream is now{' '}
               <Text
-                fontSize="40"
+                fontSize="md"
                 textStyle="Quicksand_bolder"
                 textDecorationLine="underline"
                 background="none"
-                paddingLeft="45%"
+                as="span"
               >
                 LIVE
               </Text>
             </Text>
-            <CloseButton onClick={closeModal} size="sm" />
           </Stack>
-          <Stack direction="row"></Stack>
           <Button
             alignSelf="center"
             height="auto"
             as={Link}
-            p="2"
+            px={4}
+            py={2}
             background="#537893"
             color="#ffffff"
-            width="30%"
             backdropFilter="blur(6px)"
             borderRadius="10px"
             href="/online"
           >
-            <Text fontSize="20" textStyle="Quicksand_bold">
+            <Text fontSize="md" textStyle="Quicksand_bold">
               Check it out!
             </Text>
           </Button>
@@ -214,17 +205,16 @@ const PopUp = () => {
         width="90vw"
         background="#FFFFFF"
         display={{ base: displayModal, md: 'none' }}
-        p="6"
+        p="4"
         zIndex="sticky"
         boxShadow="0px 3px 4px 2px rgba(0, 0, 0, 0.3)"
         borderRadius="20px"
       >
         <Stack direction="column">
-          <Stack direction="column" spacing={0}>
-            <CloseButton onClick={closeModal} size="lg" alignSelf="flex-end" />
+          <Stack direction="column" spacing={1}>
+            <CloseButton onClick={closeModal} size="md" alignSelf="flex-end" />
             <Text
-              fontSize="25"
-              marginTop="10px"
+              fontSize="lg"
               textStyle="Quicksand_bold"
               color="#003984"
               justifyContent={'center'}
@@ -232,32 +222,29 @@ const PopUp = () => {
             >
               The online Easter Celebration stream is now{' '}
               <Text
-                fontSize="40"
-                //paddingLeft = "35%"
+                fontSize="lg"
                 textStyle="Quicksand_bolder"
                 textDecorationLine="underline"
                 display="inline"
                 color="#003984"
+                as="span"
               >
                 LIVE
               </Text>
             </Text>
           </Stack>
-          <Center>
+          <Center w="100%">
             <Button
-              alignSelf="center"
-              height="auto"
               as={Link}
-              p="2"
+              py="2"
+              px="8"
               background="#537893"
-              top="2"
               color="#ffffff"
-              width="80%"
               backdropFilter="blur(6px)"
               borderRadius="10px"
               href="/online"
             >
-              <Text fontSize="20" textStyle="Quicksand_bold">
+              <Text fontSize="md" textStyle="Quicksand_bold">
                 Check it out!
               </Text>
             </Button>
@@ -269,24 +256,23 @@ const PopUp = () => {
   // let currDate = new Date('17 Apr 2022 02:30:00 UTC');
   let currDate = new Date();
   const goodFriday = new Date('15 Apr 2022 11:25:00 UTC'); //11:30 am UTC is 19:30 PM HKT
-  const goodFridayEnd = new Date ('15 Apr 2022 15:30:00 UTC');
+  const goodFridayEnd = new Date('15 Apr 2022 15:30:00 UTC');
   const easterSunday = new Date('17 Apr 2022 01:55:00 UTC'); //2:30 am UTC is 10:00 AM HKT
   const easterSundayEnd = new Date('17 Apr 2022 05:00:00 UTC');
   return (
     <>
-      {(currDate >= goodFriday && currDate <= goodFridayEnd) ? 
+      {currDate >= goodFriday && currDate <= goodFridayEnd ? (
         <>
-        <WebViewFriday /> 
-        <MobileViewFriday />
+          <WebViewFriday />
+          <MobileViewFriday />
         </>
-        : null}
-      {(currDate >= easterSunday && currDate <= easterSundayEnd) ? 
+      ) : null}
+      {currDate >= easterSunday && currDate <= easterSundayEnd ? (
         <>
-        <WebViewSunday />
-        <MobileViewSunday />
-        </> 
-        : null}
-     
+          <WebViewSunday />
+          <MobileViewSunday />
+        </>
+      ) : null}
     </>
   );
 };
