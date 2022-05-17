8a48b08eed9297b9b38b0475084c74bf3a346717
tamara <tamarayustian@gmail.com>
2022-05-17 23:56:31 +0800

    GH-392: restrict token and route

--- server/api/helpers/auth/verify-jwt.js
+++ server/api/helpers/auth/verify-jwt.js
@@ -1,33 +1,33 @@
-const jwt = require("jsonwebtoken");
+const jwt = require('jsonwebtoken');
 
 module.exports = {
-  friendlyName: "Verify JWT",
-  description: "Verify a JWT token.",
+  friendlyName: 'Verify JWT',
+  description: 'Verify a JWT token.',
   inputs: {
     token: {
-      type: "string",
-      friendlyName: "token",
-      description: "A reference to the request object (req).",
+      type: 'string',
+      friendlyName: 'token',
+      description: 'A reference to the request object (req).',
       required: true,
     },
   },
   exits: {
     invalid: {
-      description: "Invalid token",
+      description: 'Invalid token',
     },
   },
   fn: async function ({ token }, exits) {
-    if (token == null || token === "") return exits.invalid("Unauthorised");
+    if (token == null || token === '') return exits.invalid('Unauthorised');
 
     try {
       let valid = jwt.verify(token, process.env.JWT_KEY);
-      if (valid == null) return exits.invalid("Forbidden");
+      if (valid == null) return exits.invalid('Forbidden');
 
       // at this point should return a user object
       // with id, email and accessType
       let user = await User.findOne(valid.id);
 
-      if (!user) return exits.invalid("Unauthorised");
+      if (!user) return exits.invalid('Unauthorised');
 
       const {
         createdAt,
@@ -37,10 +37,16 @@ module.exports = {
         ...result
       } = user;
 
+      const whitelistData = await Whitelist.findOne({
+        eventName: 'ignite-2022',
+        data: { contains: valid.emailAddress },
+      });
+      result.whitelisted = whitelistData ? true : false;
+
       return exits.success(result);
     } catch (err) {
-      if (err instanceof jwt.TokenExpiredError, err) {
-        return exits.invalid("token-expired")
+      if ((err instanceof jwt.TokenExpiredError, err)) {
+        return exits.invalid('token-expired');
       }
       return exits.invalid(err);
     }
