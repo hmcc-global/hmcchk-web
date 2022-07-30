d639b97f44ca736787a23ec828efa1d3936c2558
tamara <tamarayustian@gmail.com>
2022-07-31 01:29:04 +0800

    GH-456: change format

--- ui/src/components/home/NewHereSection.js
+++ ui/src/components/home/NewHereSection.js
@@ -8,17 +8,18 @@ import {
   Heading,
   Input,
   VStack,
-} from "@chakra-ui/react";
-import ReCAPTCHA from "react-google-recaptcha";
-import { useState } from "react";
-import axios from "axios";
+  Stack,
+} from '@chakra-ui/react';
+import ReCAPTCHA from 'react-google-recaptcha';
+import { useState } from 'react';
+import axios from 'axios';
 
 const NewHereSection = () => {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [phoneNumber, setPhoneNumber] = useState("");
-  const [lifestage, setLifestage] = useState("");
-  const [notes, setNotes] = useState("");
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [lifestage, setLifestage] = useState('');
+  const [notes, setNotes] = useState('');
   const [isBot, setIsBot] = useState(true);
   const [submitted, setSubmitted] = useState(false);
 
@@ -29,7 +30,7 @@ const NewHereSection = () => {
   const onSubmit = async (event) => {
     event.preventDefault();
     if (!isBot) {
-      await axios.post("/api/forms/connect-with-us", {
+      await axios.post('/api/forms/connect-with-us', {
         name: name,
         email: email,
         notes: notes,
@@ -45,13 +46,13 @@ const NewHereSection = () => {
   };
 
   const resetForm = () => {
-    setName("");
-    setEmail("");
-    setPhoneNumber("");
-    setLifestage("");
-    setNotes("");
-    setPhoneNumber("");
-    setLifestage("");
+    setName('');
+    setEmail('');
+    setPhoneNumber('');
+    setLifestage('');
+    setNotes('');
+    setPhoneNumber('');
+    setLifestage('');
     setIsBot(true);
     setSubmitted(false);
   };
@@ -61,96 +62,93 @@ const NewHereSection = () => {
       <Flex
         w="full"
         h="100vh"
-        bgImage={`linear-gradient( rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5) ),url(${
-          process.env.PUBLIC_URL + "/images/home/connect.png"
-        })`}
-        bgSize="cover"
-        bgPosition="center center"
         justify="center"
+        background="#F9F9F9"
+        style={{ backdropFilter: 'blur(7px)' }}
       >
-        <Flex
-          w="full"
-          h="100%"
-          justify="center"
-          background='linear-gradient(90deg, rgba(6, 40, 163, 0.6) 0%, rgba(196, 241, 249, 0.6) 100%);"    '
-          style={{ backdropFilter: "blur(7px)" }}
-        >
-          <Container maxW="container.lg" justifyContent="center" display="flex">
-            <VStack color="white" justify="center" spacing={5}>
-              <Heading fontSize={["1.3em", "3em"]}>
-                New here? Connect with us
+        <Container maxW="container.xl" justifyContent="center" display="flex">
+          <Stack
+            direction={['column', 'row']}
+            color="white"
+            justify="center"
+            alignItems="center"
+            spacing={5}
+          >
+            <VStack alignItems={'left'}>
+              <Heading fontSize={['1.3em', '3em']} color="#505050">
+                New here?
               </Heading>
+              <Heading fontSize={['1.3em', '3em']} color="#0628A3">
+                Connect with us
+              </Heading>
+            </VStack>
+            <Box w={'55%'} borderWidth={'1px'} px={5} py={10} borderRadius={10}>
               <form onSubmit={onSubmit}>
                 <VStack spacing={[4, 2.5]} alignItems="center">
-                  <FormControl id="name" isRequired w={["85%", "100%"]}>
+                  <FormControl id="name" isRequired w={['85%', '100%']}>
                     <Input
-                      variant="flushed"
+                      variant="filled"
                       type="name"
                       placeholder="Name"
                       onChange={(e) => setName(e.target.value)}
                     />
                   </FormControl>
-                  <FormControl id="email" isRequired w={["85%", "100%"]}>
+                  <FormControl id="email" isRequired w={['85%', '100%']}>
                     <Input
-                      variant="flushed"
+                      variant="filled"
                       type="email"
                       placeholder="Email"
                       onChange={(e) => setEmail(e.target.value)}
                     />
                   </FormControl>
-                  <FormControl id="phoneNumber" isRequired w={["85%", "100%"]}>
+                  <FormControl id="phoneNumber" isRequired w={['85%', '100%']}>
                     <Input
-                      variant="flushed"
+                      variant="filled"
                       placeholder="Phone Number"
                       onChange={(e) => setPhoneNumber(e.target.value)}
                     />
                   </FormControl>
-                  <FormControl id="lifestage" isRequired w={["85%", "100%"]}>
+                  <FormControl id="lifestage" isRequired w={['85%', '100%']}>
                     <Input
-                      variant="flushed"
+                      variant="filled"
                       placeholder="Campus/Lifestage"
                       onChange={(e) => setLifestage(e.target.value)}
                     />
                   </FormControl>
-                  <FormControl id="notes" isRequired w={["85%", "100%"]}>
+                  <FormControl id="notes" isRequired w={['85%', '100%']}>
                     <Input
                       type="notes"
                       placeholder="Notes and remarks"
-                      variant="flushed"
+                      variant="filled"
                       onChange={(e) => setNotes(e.target.value)}
                       _autofill={{
-                        background: "rgba(0,0,0,0)",
+                        background: 'rgba(0,0,0,0)',
                       }}
                     />
                   </FormControl>
-                  <Box transform={["scale(0.77)", "scale(1)"]}>
+                  <Box transform={['scale(0.77)', 'scale(1)']}>
                     <ReCAPTCHA
                       sitekey={process.env.REACT_APP_CAPTCHA}
                       onChange={onChange}
                     />
                   </Box>
                   <Button
-                    bg="rgb(0, 0, 0, 0)"
-                    variant="outline"
+                    bg="#0628A3"
+                    variant="filled"
                     color="white"
-                    _hover={{
-                      bg: "white",
-                      color: "#1A365D",
-                      borderColor: "#1A365D",
-                    }}
                     type="submit"
-                    borderRadius={10}
+                    width="full"
                   >
-                    {"Connect with us >"}
+                    {'Submit'}
                   </Button>
-                  <FormLabel visibility={submitted ? "visible" : "hidden"}>
+                  <FormLabel visibility={submitted ? 'visible' : 'hidden'}>
                     Thank you. We will be in touch soon!
                   </FormLabel>
                 </VStack>
               </form>
-            </VStack>
-          </Container>
-        </Flex>
+            </Box>
+          </Stack>
+        </Container>
       </Flex>
     </>
   );
