adb00d8b1cbe9aea220be181a71bf41f4468a69c
Albert Paredandan <albertparedandan@gmail.com>
2022-09-25 09:21:56 +0800

    GH-1: Migrate safe model in local/uat

--- server/config/custom.js
+++ server/config/custom.js
@@ -9,6 +9,31 @@
  */
 
 module.exports.custom = {
+
+  models: {
+    /***************************************************************************
+     *                                                                          *
+     * To help avoid accidents, Sails automatically sets the automigration      *
+     * strategy to "safe" when your app lifts in production mode.               *
+     * (This is just here as a reminder.)                                       *
+     *                                                                          *
+     * More info:                                                               *
+     * https://sailsjs.com/docs/concepts/models-and-orm/model-settings#?migrate *
+     *                                                                          *
+     ***************************************************************************/
+    migrate: "safe",
+
+    /***************************************************************************
+     *                                                                          *
+     * If, in production, this app has access to physical-layer CASCADE         *
+     * constraints (e.g. PostgreSQL or MySQL), then set those up in the         *
+     * database and uncomment this to disable Waterline's `cascadeOnDestroy`    *
+     * polyfill.  (Otherwise, if you are using a databse like Mongo, you might  *
+     * choose to keep this enabled.)                                            *
+     *                                                                          *
+     ***************************************************************************/
+    // cascadeOnDestroy: false,
+  },
   /**************************************************************************
    *                                                                         *
    * The base URL to use during development.                                 *
