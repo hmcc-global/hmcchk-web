42db0aa0ebd4d81401939de93044f346b8c956e5
Albert Paredandan <albertparedandan@gmail.com>
2022-10-02 18:06:15 +0800

    GH-521: Enable refresh on error pages

--- ui/src/components/helpers/formsHelpers.js
+++ ui/src/components/helpers/formsHelpers.js
@@ -1,3 +1,6 @@
+import { DateTime } from "luxon";
+import { customAxios as axios } from "./customAxios";
+
 // String conversion tools
 const camelize = (str) => {
   return str
@@ -5,7 +8,7 @@ const camelize = (str) => {
       return index === 0 ? word.toLowerCase() : word.toUpperCase();
     })
     .replace(/\s+/g, "");
-};
+}
 
 const sentencize = (str) => {
   return str
@@ -34,4 +37,84 @@ const sentencize = (str) => {
     .join("");
 }
 
-export { camelize, sentencize };
+const validateForm = async (id, user) => {
+  try {
+    const { data } = await axios.get('/api/forms/get-form', {
+      params: { id }
+    });
+
+    if (!data[0]) {
+      return {
+        pathname: '/form-unavailable',
+        state: { id }
+      };
+    }
+
+    const formAvailableFrom = data[0].formAvailableFrom && DateTime.fromISO(data[0].formAvailableFrom).setZone('Asia/Hong_Kong');
+    const formAvailableUntil = data[0].formAvailableUntil && DateTime.fromISO(data[0].formAvailableUntil).setZone('Asia/Hong_Kong');
+
+    // One of these values are set
+    if (formAvailableFrom.isValid || formAvailableUntil.isValid) {
+      // If form available from is set & valid, check if current time is
+      // after formAvailableFrom
+      let afterStartTime = formAvailableFrom.isValid
+        ? DateTime.now().setZone('Asia/Hong_Kong') >= formAvailableFrom
+        : true;
+
+      // Compound checking: if form available until:
+      // is valid: check if it's within the time of form availability
+      // is not valid: default to whatever value was already there
+      let beforeEndTime = formAvailableUntil.isValid
+        ? DateTime.now().setZone('Asia/Hong_Kong') < formAvailableUntil
+        : true;
+      let isInRange = afterStartTime && beforeEndTime;
+
+      if (!isInRange) {
+        if (!beforeEndTime) {
+          return {
+            pathname: '/form-unavailable',
+            state: { id: data[0].id },
+          };
+        } else {
+          return {
+            pathname: '/form-will-open',
+            state: { 
+              availableAfter: formAvailableFrom.toFormat('dd MMM yyyy, HH:mm'),
+              id: data[0].id
+            }
+          };
+        }
+      }
+    }
+    // end form open check
+
+    // If there is a logged in user, check whether they filled or not
+    // Else default to true to allow public to access
+    let filledInProfileCheck = user.id ? user.hasFilledProfileForm : true;
+
+    if (!filledInProfileCheck) {
+      return {
+        pathname: '/need-fill-profile'
+      };
+    }
+
+    // Check if form requires login
+    else if (data[0].requireLogin && !user.id) {
+      return {
+        pathname: '/need-login'
+      };
+    }
+    return {
+      data
+    };
+  } catch (err) {
+    console.log(err);
+    return {
+      pathname: '/form-unavailable'
+    };
+  }
+
+
+}
+
+export { camelize, sentencize, validateForm };
