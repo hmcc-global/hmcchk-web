e7e29f8fc18429caa89157860d594fe339dcbbc4
tamara <tamarayustian@gmail.com>
2022-10-07 02:05:50 +0800

    GH-550: add banner and font

--- ui/src/components/witness/WitnessContainer.js
+++ ui/src/components/witness/WitnessContainer.js
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 import React from 'react';
+=======
+import { useState } from 'react';
+>>>>>>> e042517... GH-550: add banner and font
 import {
   Box,
   Container,
@@ -8,7 +12,15 @@ import {
   TabPanel,
   TabPanels,
   TabList,
+<<<<<<< HEAD
 } from '@chakra-ui/react';
+=======
+  Image,
+  Text,
+} from '@chakra-ui/react';
+import witnessTheme from './witnessTheme';
+import '@fontsource/dm-sans';
+>>>>>>> e042517... GH-550: add banner and font
 import VideoTestimonyContainer from './video-testimony/VideoTestimonyContainer';
 
 const WitnessContainer = (props) => {
@@ -16,18 +28,30 @@ const WitnessContainer = (props) => {
     <>
       <Container maxW="100%" m={0} p={0}>
         <Box
-          bgImage={`url('${process.env.PUBLIC_URL}/images/sermons/sermons-banner.png')`}
-          bgPosition="center"
+          bgImage={`url('${process.env.PUBLIC_URL}/images/witness/banner-desktop.png')`}
+          // bgPosition="center"
           bgSize="cover"
-          textAlign="center"
           justifyContent="center"
-          py={[8, 150]}
-          m={0}
-          width="100%"
+          py={[8, 155]}
+          // w={'100%'}
+          // m={0}
         >
-          <Heading size="2xl" color="white" fontWeight="900">
-            Witness
-          </Heading>
+          <Image
+            src={process.env.PUBLIC_URL + '/images/witness/title.png}'}
+            objectFit="cover"
+            margin="auto"
+          />
+          <Box pl={'12'} w={'55%'}>
+            <Text textStyle={'dm_sans'} color="white" fontSize={['md', 'lg']}>
+              The theme for HMCC 2022-2023 is Witness. As a church, it is our
+              hope that our daily and personal witness of Jesus will translate
+              into our passionate witness for Jesus to others around us.
+              <br />
+              <br />
+              Throughout this year, we want to invite you to share your personal
+              witness of Jesus here and witness God’s work in our church!
+            </Text>
+          </Box>
         </Box>
         <Container maxW="container.xl" padding={[0, 2]}>
           <Tabs size="lg" justifyContent="center" m={1}>
--- ui/src/components/witness/witnessTheme.js
+++ ui/src/components/witness/witnessTheme.js
@@ -1,5 +1,12 @@
 import { extendTheme } from '@chakra-ui/react';
 
-const witnessTheme = extendTheme({});
+const witnessTheme = extendTheme({
+  textStyles: {
+    dm_sans: {
+      fontFamily: 'DM Sans',
+      fontWeight: '500',
+    },
+  },
+});
 
 export default witnessTheme;
