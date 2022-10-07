c2ace5f863042328f204d2c99a01b2c6eb6f9590
tamara <tamarayustian@gmail.com>
2022-10-08 02:53:23 +0800

    GH-550: banner complete with button

--- ui/src/components/witness/WitnessContainer.js
+++ ui/src/components/witness/WitnessContainer.js
@@ -9,7 +9,10 @@ import {
   TabList,
   Image,
   Text,
+  Stack,
+  Button,
 } from '@chakra-ui/react';
+import { EditIcon } from '@chakra-ui/icons';
 import '@fontsource/dm-sans';
 import witnessTheme from './witnessTheme';
 import VideoTestimonyContainer from './video-testimony/VideoTestimonyContainer';
@@ -23,23 +26,58 @@ const WitnessContainer = (props) => {
           url('${process.env.PUBLIC_URL}/images/witness/banner-desktop.png')`}
           bgSize="cover"
           justifyContent="center"
-          py={[8, 120]}
+          py={[20, 120]}
         >
-          <Image
-            src={process.env.PUBLIC_URL + '/images/witness/title.png'}
-            pl={'12'}
-          />
-          <Box pl={'12'} pt={'5'} w={'55%'}>
-            <Text textStyle={'dm_sans'} color="white" fontSize={['md']}>
-              The theme for HMCC 2022-2023 is Witness. As a church, it is our
-              hope that our daily and personal witness of Jesus will translate
-              into our passionate witness for Jesus to others around us.
-              <br />
-              <br />
-              Throughout this year, we want to invite you to share your personal
-              witness of Jesus here and witness God’s work in our church!
-            </Text>
-          </Box>
+          <Stack
+            direction={['column', 'row']}
+            justifyContent={['center', 'space-evenly']}
+          >
+            <Box w={['85%', '45%']}>
+              <Image
+                src={process.env.PUBLIC_URL + '/images/witness/title.png'}
+              />
+              <Box pt={'5'}>
+                <Text
+                  textStyle={'dm_sans'}
+                  color="white"
+                  fontSize={['md']}
+                  textAlign={['center', 'left']}
+                >
+                  The theme for HMCC 2022-2023 is Witness. As a church, it is
+                  our hope that our daily and personal witness of Jesus will
+                  translate into our passionate witness for Jesus to others
+                  around us.
+                  <br />
+                  <br />
+                  Throughout this year, we want to invite you to share your
+                  personal witness of Jesus here and witness God’s work in our
+                  church!
+                </Text>
+              </Box>
+            </Box>
+            <Box pt={['3', '12']} justifyContent="center" w={['85%', '40%']}>
+              <Text
+                textStyle={'dm_sans_bold'}
+                color="white"
+                fontSize={['2xl']}
+                textAlign="center"
+              >
+                HOW ARE YOU WITNESSING GOD?
+              </Text>
+              {/* TODO: Testimony Form Modal for this Button*/}
+              <Button
+                my={3}
+                size="lg"
+                w="100%"
+                leftIcon={<EditIcon />}
+                bg="#8D2C72"
+                color="white"
+                _hover={{ bg: 'white', color: '#8D2C72' }}
+              >
+                SHARE YOUR TESTIMONY
+              </Button>
+            </Box>
+          </Stack>
         </Box>
         <Tabs
           size="lg"
