440423aad78dd7a7817fb8df1685799d16da003a
Yoo Yuen Yau <yuenyau989@hotmail.com>
2022-10-10 13:18:28 +0800

    GH-559: Leaderboard Model and APIs, Admin and Public Page [FIRST DRAFT]

---
+++ server/api/controllers/harvestGames/get-hg-rankings.js
@@ -0,0 +1,39 @@
+module.exports = {
+  friendlyName: 'Get Harvest Games rankings',
+
+  description: 'Get Harvest Games rankings',
+
+  inputs: {
+    lgRankingId: {
+      required: false,
+      type: 'string',
+    },
+  },
+
+  exits: {
+    success: {
+      description: 'Harvest Games rankings returned successfully',
+    },
+    invalid: {
+      description: 'Failed to retrieve Harvest Games rankings',
+    },
+  },
+
+  fn: async function ({ lgRankingId }, exits) {
+    try {
+      if (lgRankingId) {
+        let data = await HGRankings.find({ _id: lgRankingId }).populateAll();
+        if (data.length === 0) throw 'HG ranking not found';
+        return exits.success(data);
+      }
+
+      let data = await HGRankings.find().populateAll();
+      sails.log.info('Retrieving Harvest Games rankings..');
+
+      return exits.success(data);
+    } catch (err) {
+      sails.log(err);
+      return exits.invalid(err);
+    }
+  },
+};
