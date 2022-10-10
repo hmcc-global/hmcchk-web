e325abdcb4e5a81172c7c8b4c2b7da4cbefb576b
Alvin Harjanto <alvinharjanto@Alvins-MacBook-Pro.local>
2022-10-10 17:53:54 +0800

    GH-566: Created witness section on main web home

--- ui/src/components/witness/WitnessHomeContainer.js
+++ ui/src/components/witness/WitnessHomeContainer.js
@@ -1,5 +1,13 @@
 import React from 'react';
-import { Box, Container, Image, Text, Button } from '@chakra-ui/react';
+import {
+  Box,
+  Container,
+  Image,
+  Text,
+  Button,
+  VStack,
+  Flex,
+} from '@chakra-ui/react';
 import { EditIcon, TriangleDownIcon } from '@chakra-ui/icons';
 import WitnessHomeVideoSection from './home-sections/WitnessHomeVideoSection';
 
@@ -14,61 +22,72 @@ const WitnessHomeContainer = (props) => {
           bgSize="cover"
           textAlign="center"
           justifyContent="center"
-          py={[8, 120]}
+          pt={[20, 120]}
+          pb={[10, 20]}
           m={0}
           width="100%"
         >
-          <Image 
-            marginRight="auto" 
-            marginLeft="auto"
+          <Image
+            mr="auto"
+            ml="auto"
             src={process.env.PUBLIC_URL + '/images/witness/title.png'}
+            w={['80%', '50%']}
           />
-          <Box m={[8, 0]}>
-            <Text 
+          <Box m={[8, 0]} justifyContent="center">
+            <Text
               textColor="rgba(255, 255, 255, 1)"
-              fontWeight="bold" 
-              fontSize="24px"  
-              pb={4} 
+              fontWeight="bold"
+              fontSize="24px"
+              pb={4}
               pt={4}
             >
               HMCC 2022 - 2023
             </Text>
-            <Text 
-              textColor="rgba(255, 255, 255, 1)" 
-              fontWeight="300" 
-              pb={3} 
-              textStyle={"dm_sans"}
-            >
-              The theme for HMCC 2022 - 2023 is Witness. As a church, it is our hope that our daily and personal witness for Jesus will translate into our passionate witness for Jesus to others around us.
-            </Text>
-            <Text 
-              textColor="rgba(255, 255, 255, 1)" 
-              fontWeight="300" 
-              color = "white" 
-              pb={7}
-            >
-              Throughout this year, we want to invite you to share your personal witness of Jesus here and witness God's work in our church! Check out the different testimonies below!
-            </Text>
-            <TriangleDownIcon w="40px" h="40px" pb={4} color="#FFD6DC"/>
-            <Text 
-              textColor="white" 
-              fontWeight="bold" 
-              fontSize = "24px"  
-              marginBottom="15px" 
+            <Flex justifyContent="center">
+              <VStack w={['100%', '50%']}>
+                <Text
+                  textColor="rgba(255, 255, 255, 1)"
+                  fontWeight="300"
+                  pb={3}
+                  textStyle={'dm_sans'}
+                >
+                  The theme for HMCC 2022 - 2023 is Witness. As a church, it is
+                  our hope that our daily and personal witness for Jesus will
+                  translate into our passionate witness for Jesus to others
+                  around us.
+                </Text>
+                <Text
+                  textColor="rgba(255, 255, 255, 1)"
+                  fontWeight="300"
+                  color="white"
+                  pb={7}
+                >
+                  Throughout this year, we want to invite you to share your
+                  personal witness of Jesus here and witness God's work in our
+                  church! Check out the different testimonies below!
+                </Text>
+              </VStack>
+            </Flex>
+            <TriangleDownIcon w="40px" h="40px" pb={4} color="#FFD6DC" />
+            <Text
+              textColor="white"
+              fontWeight="bold"
+              fontSize="24px"
+              marginBottom="15px"
             >
               HOW ARE YOU WITNESSING GOD?
             </Text>
-            <Button 
-              borderRadius='md' 
-              bg='#8D2C72' 
-              color='white'
-              // h="66px" 
+            <Button
+              borderRadius="md"
+              bg="#8D2C72"
+              color="white"
+              // h="66px"
               // w={418.1}
               p={[5, 8]}
               fontSize={[20, 24]}
-              size='lg'
+              size="lg"
             >
-              <EditIcon h={8} mr={4}/> SHARE YOUR TESTIMONY
+              <EditIcon h={8} mr={4} /> SHARE YOUR TESTIMONY
             </Button>
           </Box>
         </Box>
