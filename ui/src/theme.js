5e367955e557c5bcedaaa9931f79945353a5e714
Alvin Harjanto <alvinharjanto@Alvins-MacBook-Pro.local>
2022-10-11 12:15:10 +0800

    GH-566: Fixed theme and UI

--- ui/src/theme.js
+++ ui/src/theme.js
@@ -1,9 +1,20 @@
 import { extendTheme } from '@chakra-ui/react';
+import '@fontsource/dm-sans';
 
 const customTheme = extendTheme({
   fonts: {
     heading: `'Inter', -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol"`,
     body: `'Inter', -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol"`,
+  },
+  textStyles: {
+    dm_sans: {
+      fontFamily: 'DM Sans',
+      fontWeight: '500',
+    },
+    dm_sans_bold: {
+      fontFamily: 'DM Sans',
+      fontWeight: '700',
+    },
   },
 });
 
