33765ca7844b762f8a771375ba4223a524e63402
Richie Wee Salim <richie@navynicy.com>
2022-10-26 14:25:55 +0800

    GH-599: struture empty files

---
+++ ui/src/components/missions-month/MissionsMonthHero.js
@@ -0,0 +1,23 @@
+import { Container, Flex } from "@chakra-ui/react";
+
+const MissionsMonthHero = () => {
+  return (
+    <Flex
+      w="full"
+      h="auto"
+      direction="column"
+      alignItems="center"
+      paddingTop="2em"
+      paddingBottom="2em"
+    >
+      <Container
+        maxW="container.lg"
+        justifyContent="center"
+        display="flex"
+        marginTop="2em"
+      ></Container>
+    </Flex>
+  );
+};
+
+export default MissionsMonthHero
