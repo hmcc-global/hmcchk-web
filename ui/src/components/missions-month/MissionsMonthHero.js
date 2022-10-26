7892db6b8a69e7d6bb495763f98283c3787aaf30
Richie Wee Salim <richie@navynicy.com>
2022-10-26 18:52:43 +0800

    GH-599: hero section structure done

--- ui/src/components/missions-month/MissionsMonthHero.js
+++ ui/src/components/missions-month/MissionsMonthHero.js
@@ -1,23 +1,42 @@
-import { Container, Flex } from "@chakra-ui/react";
+import { Box, Container, Flex, Heading, Stack, Text } from '@chakra-ui/react';
 
 const MissionsMonthHero = () => {
+  const heroText = 'NO OTHER NAME';
+  const heroDesc1 = 'MISSIONS MONTH';
+  const heroDesc2 = 'NOVEMBER 2022';
   return (
     <Flex
       w="full"
-      h="auto"
+      h="50vh"
       direction="column"
       alignItems="center"
       paddingTop="2em"
       paddingBottom="2em"
+      bgImage={
+        process.env.PUBLIC_URL + '/images/missions-month/no_other_name.jpg'
+      }
+      bgSize="cover"
+      bgPosition="center center"
     >
-      <Container
-        maxW="container.lg"
-        justifyContent="center"
-        display="flex"
-        marginTop="2em"
-      ></Container>
+      <Container maxW="container.lg" justifyContent="center" display="flex">
+        <Stack direction="column">
+          <Heading>{heroText}</Heading>
+          <Box
+            style={{
+              width: '100%',
+              justifyContent: 'center',
+              alignItems: 'center',
+              display: 'flex',
+              flexDirection: 'column',
+            }}
+          >
+            <Text>{heroDesc1}</Text>
+            <Text>{heroDesc2}</Text>
+          </Box>
+        </Stack>
+      </Container>
     </Flex>
   );
 };
 
-export default MissionsMonthHero
+export default MissionsMonthHero;
