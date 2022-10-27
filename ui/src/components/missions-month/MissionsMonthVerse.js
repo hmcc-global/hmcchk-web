4b8c4baf0eb3e8507ab0faba6be0537e55e8e0e5
Richie Wee Salim <richie@navynicy.com>
2022-10-27 16:01:31 +0800

    GH-599: Mobile and desktop view done

--- ui/src/components/missions-month/MissionsMonthVerse.js
+++ ui/src/components/missions-month/MissionsMonthVerse.js
@@ -1,6 +1,7 @@
-import { Container, Flex } from "@chakra-ui/react";
+import { Container, Flex, Heading, Image, Stack, Text } from '@chakra-ui/react';
 
 const MissionsMonthVerse = () => {
+  const verse = `And there is salvation in no one else, for there is no other name under heaven given among men by which we must be saved.”`;
   return (
     <Flex
       w="full"
@@ -9,15 +10,50 @@ const MissionsMonthVerse = () => {
       alignItems="center"
       paddingTop="2em"
       paddingBottom="2em"
+      background="#DDE9FF"
     >
-      <Container
-        maxW="container.lg"
-        justifyContent="center"
-        display="flex"
-        marginTop="2em"
-      ></Container>
+      <Container maxW="container.lg" justifyContent="center" display="flex">
+        <Stack direction="row" position="relative">
+          <Image
+            src={`${process.env.PUBLIC_URL}/images/missions-month/start_quotes.svg`}
+            position="relative"
+            bottom="70%"
+            width={{ base: '15%', md: '10%' }}
+          />
+          <Stack
+            direction="column"
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}
+          >
+            <Heading
+              letterSpacing="0.05em"
+              color="#FE6F3A"
+              fontSize={{ base: '24px', md: '48px' }}
+              fontWeight={600}
+            >
+              Acts 4:12
+            </Heading>
+            <Text
+              style={{ textAlign: 'center' }}
+              fontSize={{ base: '12px', md: '16px' }}
+            >
+              {verse}
+            </Text>
+          </Stack>
+          <Image
+            src={`${process.env.PUBLIC_URL}/images/missions-month/end_quotes.svg`}
+            position="relative"
+            top="70%"
+            width={{ base: '15%', md: '10%' }}
+          />
+        </Stack>
+      </Container>
     </Flex>
   );
 };
 
-export default MissionsMonthVerse
+export default MissionsMonthVerse;
