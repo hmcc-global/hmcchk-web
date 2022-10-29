cf570bfa638df8ad3da9a470f3b573c5963301d5
Baeck016 <73063819+Baeck016@users.noreply.github.com>
2022-10-29 23:06:33 +0800

    GH-600: Deleted and changed font size

--- ui/src/components/missions-month/MissionsMonthTabs.js
+++ ui/src/components/missions-month/MissionsMonthTabs.js
@@ -178,9 +178,96 @@ export default function MissionMonthTabs() {
             <Image src={process.env.PUBLIC_URL + 'images/mission-month2.png'} />
           </Container>
         </TabPanel>
-        <TabPanel>
+        <TabPanel w="100%">
+          <Container minWidth="100%" bg="#FFDBCE" margin="0px">
+            <Box
+              padding="2"
+              color="#00328D"
+              w="100%"
+              fontFamily="Akshar"
+              fontSize={{ base: '5vw', md: '4vw', lg: '2.5vw' }}
+              fontWeight="1000"
+              textAlign="center"
+              letterSpacing="3px"
+            >
+              WEEKLY PRAYER GUIDES
+            </Box>
+          </Container>
           <Container>
-            <Box></Box>
+            <Box
+              w="100%"
+              padding="10px"
+              fontFamily="Lexend Deca"
+              fontSize={{ base: '12px', md: '15px' }}
+            >
+              <Center fontFamily="Lexend Deca" as="i" textAlign="center">
+                Weekly Prayer Guides will be uploaded every Monday in November!
+              </Center>
+              <Center>...</Center>
+              <br />
+              <UnorderedList>
+                {/* WHEN WEEKLY PRAYER GUIDES BECOME AVAILABLE!! REPLACE LINKS each week!!
+                <ListItem><Link color='black' href='www.hmcc.tv' isExternal>
+                    Week 1
+                </Link></ListItem>
+                
+                <ListItem> <Link color='black' href='www.hmcc.tv' isExternal>
+                    Week 2
+                </Link></ListItem>
+                <ListItem><Link color='black' href='www.hmcc.tv' isExternal>
+                    Week 3
+                </Link></ListItem>
+                <ListItem><Link color='gray' href='www.hmcc.tv' isExternal>
+                    Week 4
+                </Link></ListItem>
+                */}
+              </UnorderedList>
+            </Box>
+          </Container>
+
+          <Container minWidth="100%" bg="#DDE9FF" margin="0px">
+            <Box
+              padding="2"
+              color="#00328D"
+              w="100%"
+              fontFamily="Akshar"
+              fontSize={{ base: '5vw', md: '4vw', lg: '2.5vw' }}
+              fontWeight="1000"
+              textAlign="center"
+              letterSpacing="3px"
+            >
+              OTHER LINKS
+            </Box>
+          </Container>
+
+          <Container>
+            <Box
+              maxWidth={{ base: '100%', md: '100%' }}
+              padding={{ base: '10px', md: '10px' }}
+              fontFamily="Lexend Deca"
+              fontSize={{ base: '12px', md: '15px' }}
+            >
+              <UnorderedList>
+                <ListItem>
+                  <Link
+                    color="black"
+                    href="https://hongkong.hmcc.net/witness/home"
+                    isExternal
+                  >
+                    Check out and share Witness Testimonies!
+                  </Link>
+                </ListItem>
+                <ListItem>
+                  <Link
+                    color="black"
+                    href="https://open.spotify.com/user/hmccofhk"
+                    isExternal
+                  >
+                    Spotify Playlists
+                  </Link>
+                </ListItem>
+              </UnorderedList>
+            </Box>
           </Container>
         </TabPanel>
       </TabPanels>
