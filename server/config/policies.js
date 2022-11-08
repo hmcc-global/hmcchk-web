c173d393cc1045c0aabdc8524f9737443cb91766
andreajeka <ajk@connect.ust.hk>
2022-11-08 22:46:10 +0800

    GH-605: Add new API to get user submissions

--- server/config/policies.js
+++ server/config/policies.js
@@ -46,6 +46,7 @@ module.exports.policies = {
   'forms/get-form': true,
   'forms/post-create-submission': true,
   'forms/get-submission': ['isLoggedIn', 'aboveTech'],
+  'forms/get-user-submission': ['isLoggedIn', 'aboveTech'],
 
   // Last Updated
   'lastUpdated/get-last-updated': ['isLoggedIn', 'aboveAdmin'],
