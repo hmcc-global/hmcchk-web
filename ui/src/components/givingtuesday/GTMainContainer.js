d199a92ab296f528a82b9a98789cb30081460ffa
Baeck016 <73063819+Baeck016@users.noreply.github.com>
2022-11-11 16:40:22 +0800

    GH-632: Created container for GT

---
+++ ui/src/components/givingtuesday/GTMainContainer.js
@@ -0,0 +1,7 @@
+import { Container } from '@chakra-ui/react';
+
+const GTMainContainer = () => {
+  return <Container minW="100%">{/* Container for GT */}</Container>;
+};
+
+export default GTMainContainer;
