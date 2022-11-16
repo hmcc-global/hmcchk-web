d46f8ed8973626a273aec837f9fa79ace8340151
Baeck016 <73063819+Baeck016@users.noreply.github.com>
2022-11-16 16:52:18 +0800

    GH-632: pulled

--- ui/src/components/givingTuesday/LastYearGivingTuesday.js
+++ ui/src/components/givingTuesday/LastYearGivingTuesday.js
@@ -1,4 +1,4 @@
-import { Stack, Link, Box, Text, Center, Image } from "@chakra-ui/react";
+import { Stack, Link, Box, Text, Center, Image } from '@chakra-ui/react';
 
 const LastYearGivingTuesday = (props) => {
   return (
@@ -15,18 +15,18 @@ const LastYearGivingTuesday = (props) => {
       <Text
         color="#B1A38F"
         fontWeight="900"
-        fontSize={["1.1rem", "1.75rem"]}
+        fontSize={['1.1rem', '1.75rem']}
         pt={[3, 7]}
       >
         Last Year's Giving Tuesday
       </Text>
-      <Stack fontSize={["0.7rem", "md"]} pl={7} pr={7} spacing={[5, 3]}>
+      <Stack fontSize={['0.7rem', 'md']} pl={7} pr={7} spacing={[5, 3]}>
         <Text>
           <b>111 people gave for a total of HK$192,331.09</b>
         </Text>
         <Box>
           <Text>
-            <b>HK$60k</b> total given to{" "}
+            <b>HK$60k</b> total given to{' '}
             <b>missionaries / missions organizations</b>:
           </Text>
           <Text>
@@ -35,26 +35,6 @@ const LastYearGivingTuesday = (props) => {
           </Text>
         </Box>
       </Stack>
-
-      <Center margin="auto" pt={5} pb={7}>
-        <Stack direction="row" w="fit-content">
-          <Image
-            h={["6vh", "10vh"]}
-            borderRadius={["xl", "3xl"]}
-            src={process.env.PUBLIC_URL + "/images/givingTuesday/last-1.png"}
-          />
-          <Image
-            h={["6vh", "10vh"]}
-            borderRadius={["xl", "3xl"]}
-            src={process.env.PUBLIC_URL + "/images/givingTuesday/last-2.png"}
-          />
-          <Image
-            h={["6vh", "10vh"]}
-            borderRadius={["xl", "3xl"]}
-            src={process.env.PUBLIC_URL + "/images/givingTuesday/last-3.png"}
-          />
-        </Stack>
-      </Center>
     </Stack>
   );
 };
