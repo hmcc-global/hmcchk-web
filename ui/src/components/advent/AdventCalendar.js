55596c9d88ec8a5dbd4f8c5ced4f54e5e4e1c1d8
randall <randall2000n@gmail.com>
2022-11-20 18:50:50 +0800

    GH-645: advent card content

--- ui/src/components/advent/AdventCalendar.js
+++ ui/src/components/advent/AdventCalendar.js
@@ -1,6 +1,8 @@
 import { Box, Grid, GridItem, Image } from '@chakra-ui/react';
 import AdventCard from './AdventCard';
 import { DateTime } from 'luxon';
+import blurb from './AdventCardConfig.json';
+console.log(blurb);
 
 const AdventCalendar = () => {
   const cardArray = Array.from({ length: 24 }, (_, i) => i + 1);
@@ -42,7 +44,12 @@ const AdventCalendar = () => {
             left={['5vw', '7vw', '7vw', '7vw', '10vw']}
           >
             {cardArray.map((el, i) => (
-              <AdventCard isActive={isActive(i)} date={el} key={i} />
+              <AdventCard
+                isActive={isActive(i)}
+                date={el}
+                key={i}
+                content={blurb[i]}
+              />
             ))}
           </Grid>
         </GridItem>
--- ui/src/components/advent/AdventCard.js
+++ ui/src/components/advent/AdventCard.js
@@ -5,7 +5,16 @@ import {
   ModalContent,
   Image,
   useDisclosure,
+  ModalHeader,
+  Box,
+  AspectRatio,
+  Center,
+  Text,
+  VStack,
 } from '@chakra-ui/react';
+
+import ReactMarkdown from 'react-markdown';
+import ChakraUIRenderer from 'chakra-ui-markdown-renderer';
 
 // Replace with actual card data
 const CardData = Array.from({ length: 24 }, () => {
@@ -15,30 +24,61 @@ const CardData = Array.from({ length: 24 }, () => {
 });
 
 const AdventCard = (props) => {
-  const { isActive, date } = props;
+  const { isActive, date, content } = props;
   const { isOpen, onOpen, onClose } = useDisclosure();
   const thisCardData = CardData[date - 1];
+  console.log(content);
 
   return (
     <>
       <Modal isOpen={isOpen} onClose={onClose} isCentered>
         <ModalOverlay filter="blur(8px)" />
-        <ModalContent bg="none">
-          <Image
-            src={
-              process.env.PUBLIC_URL + '/images/advent/adventCalendar/card.png'
-            }
-            w="100%"
-            margin="auto"
-          />
+        <ModalContent>
+          <ModalHeader>{content.date}</ModalHeader>
+          <Box>
+            <VStack>
+              <Box w="14em">
+                {content.image && (
+                  <Image
+                    src={process.env.PUBLIC_URL + content.image}
+                    w="100%"
+                    margin="auto"
+                  />
+                )}
+                {content.video && (
+                  <AspectRatio mb="5" width="100%" ratio={16 / 9}>
+                    <iframe
+                      width="560"
+                      height="315"
+                      src={content.video}
+                      title="Video player"
+                      frameBorder="0"
+                      allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture; autoplay;"
+                      allowFullScreen
+                    ></iframe>
+                  </AspectRatio>
+                )}
+              </Box>
+              <Box>
+                <Center>
+                  <Text>{content.title}</Text>
+                </Center>
+              </Box>
+              <Box fontSize="sm" mt="5" color="#4C80A5" textAlign="justify">
+                <ReactMarkdown
+                  components={ChakraUIRenderer()}
+                  children={content.description}
+                  skipHtml
+                />
+              </Box>
+            </VStack>
+          </Box>
           <ModalCloseButton />
         </ModalContent>
       </Modal>
       {thisCardData && (
         <Image
-          src={
-            process.env.PUBLIC_URL + '/images/advent/adventCalendar/card.png'
-          }
+          src={process.env.PUBLIC_URL + content.thumbnail}
           w="90%"
           position="relative"
           onClick={isActive ? onOpen : null}
