eed6febcf89f0a154ed902411a948c8ecc0e7bda
Richie Wee Salim <richie@navynicy.com>
2022-11-25 01:01:39 +0800

    GH-667: change some text during the day

--- ui/src/components/givingTuesday/GivingUpdates.js
+++ ui/src/components/givingTuesday/GivingUpdates.js
@@ -132,9 +132,8 @@ const GivingUpdates = (props) => {
           <>
             <Text mt={3} fontSize={['0.7rem', 'md']}>
               <b>
-                Join us as we commit to spending this coming Tuesday to
-                cultivate generous hearts and give towards the following causes
-                or categories:
+                Join us as we commit to cultivate generous hearts and give
+                toward the following causes or categories:
               </b>
             </Text>
             <Text fontSize={['0.5rem', 'sm']} fontWeight={400}>
@@ -143,76 +142,68 @@ const GivingUpdates = (props) => {
             </Text>
           </>
         )}
-        {eventStatus !== 'before' && (
+      </Box>
+      {eventStatus !== 'before' && (
+        <Box>
           <Text
             textAlign="right"
             color="#6C7E97"
-            fontSize={['0.5rem', 'sm']}
+            fontSize={['0.7rem', 'sm']}
             fontStyle="italic"
+            mb={2}
           >
             *as of{' '}
             {DateTime.fromISO(givingData.updatedAt).toFormat(
               'MMMM dd - hh:mm a'
             )}
           </Text>
-        )}
-      </Box>
-      {eventStatus !== 'before' && (
-        <Box>
-          <Text
-            textAlign="right"
-            color="#0628A3"
-            fontSize={['0.7rem', 'sm']}
-            fontStyle="italic"
-            mb={2}
-          >
-            *as of {DateTime.fromISO(givingData.updatedAt).toFormat('MMMM dd - hh:mm a')}
-          </Text>
           <Stack
-          direction={['column', 'row']}
-          borderWidth={1}
-          borderRadius="2xl"
-          borderColor="#5891FB"
-          background="#5891FB"
-          color="white"
-          shadow="md"
-          fontSize={['1rem', '1.75rem']}
-          fontWeight={700}
-          display="flex"
-          justifyContent="space-evenly"
-          px={[5, 30]}
-          py={5}
-        >
-          <Stack direction={['row', 'column']} flex={1}>
-            <Text
-              flex={1}
-              textAlign={['left', 'center']}
-              fontSize={['0.75rem', '1.75rem']}
-            >
-              Total Amount Raised:
-            </Text>
-            <Text flex={1} textAlign={['right', 'center']}>
-              HKD {numberWithCommas(givingSum)}
-            </Text>
+            direction={['column', 'row']}
+            borderWidth={1}
+            borderRadius="2xl"
+            borderColor="#5891FB"
+            background="#5891FB"
+            color="white"
+            shadow="md"
+            fontSize={['1rem', '1.75rem']}
+            fontWeight={700}
+            display="flex"
+            justifyContent="space-evenly"
+            px={[5, 30]}
+            py={5}
+          >
+            <Stack direction={['row', 'column']} flex={1}>
+              <Text
+                flex={1}
+                textAlign={['left', 'center']}
+                fontSize={['0.75rem', '1.75rem']}
+              >
+                Total Amount Raised:
+              </Text>
+              <Text flex={1} textAlign={['right', 'center']}>
+                HKD {numberWithCommas(givingSum)}
+              </Text>
+            </Stack>
+            <Box
+              display={{ base: 'none', md: 'flex' }}
+              width="1px"
+              background="white"
+            ></Box>
+            <Stack direction={['row', 'column']} flex={1}>
+              <Text
+                flex={1}
+                textAlign={['left', 'center']}
+                fontSize={['0.75rem', '1.75rem']}
+              >
+                Number of Givers:
+              </Text>
+              <Text flex={1} textAlign={['right', 'center']}>
+                {givingData.categories[0].givers === -1
+                  ? '-'
+                  : givingData.categories[0].givers}
+              </Text>
+            </Stack>
           </Stack>
-          <Box display={{base:"none", md:"flex"}} width="1px" background="white">
-            
-          </Box>
-          <Stack direction={['row', 'column']} flex={1}>
-            <Text
-              flex={1}
-              textAlign={['left', 'center']}
-              fontSize={['0.75rem', '1.75rem']}
-            >
-              Number of Givers:
-            </Text>
-            <Text flex={1} textAlign={['right', 'center']}>
-              {givingData.categories[0].givers === -1
-                ? '-'
-                : givingData.categories[0].givers}
-            </Text>
-          </Stack>
-        </Stack>
         </Box>
       )}
 
