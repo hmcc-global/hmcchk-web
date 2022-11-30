8697a2e3619e0404842f8a31de3a20850f8101d6
--unset <cathleen.trixie@gmail.com>
2022-11-30 22:14:01 +0800

    resolve conflicts

--- ui/src/components/advent/adventTheme.js
+++ ui/src/components/advent/adventTheme.js
@@ -1,14 +1,19 @@
 import { extendTheme } from '@chakra-ui/react';
 import '@fontsource/elsie-swash-caps';
+import '@fontsource/nosifer';
 
 const adventTheme = extendTheme({
-  textStyles: {
+  fonts: {
     LettersForLearners: {
       fontFamily: 'LettersforLearners',
       fontWeight: '400',
     },
     ElsieSwashCaps: {
       fontFamily: 'Elsie Swash Caps',
+      fontWeight: '400',
+    },
+    Nosifer: {
+      fontFamily: 'Nosifer',
       fontWeight: '400',
     },
   },
