757c189249423935ded0b803e4d3b967d9528db2
samyakobus <33675629+samyakobus@users.noreply.github.com>
2023-02-18 04:02:15 +0800

    GH-713: Added Button Jump Scroll Feature

--- ui/src/components/MainContainer.js
+++ ui/src/components/MainContainer.js
@@ -39,11 +39,11 @@ import WitnessHomeContainer from './witness/WitnessHomeContainer';
 import AdminTestimonyContainer from './admin/testimony/AdminTestimonyContainer';
 import AdminPrayerContainer from './admin/prayer/AdminPrayerContainer';
 import TextDetails from './witness/text-testimony/TextDetails';
-<<<<<<< HEAD
+
 import PrayerDetails from './witness/text-prayer/TextDetails';
-=======
+
 import Easter2023Container from './easter-2023/Easter2023Container';
->>>>>>> f92755c (GH-713: importing easter theme to maincontainer.js)
+
 
 const MainContainer = () => {
   return (
--- ui/src/components/easter-2023/EasterStory.js
+++ ui/src/components/easter-2023/EasterStory.js
@@ -16,9 +16,29 @@ import {
     Center,
     Spacer,
     Heading,
+    Fade,
+    useBoolean,
+    useDisclosure,
+    ScaleFade,
   } from '@chakra-ui/react';
-  
+
+import React,{useRef} from "react";
+
+
+  import {useInViewport} from 'react-in-viewport';
+  import { AnimationOnScroll } from 'react-animation-on-scroll';
+
   const EasterStory = () => {
+    const { isOpen, onToggle } = useDisclosure()
+    const ref = useRef(null);
+    const ref2 = useRef(null);
+    const{enterCount} = useInViewport(
+        ref,
+        {rootMargin: "-300px"},
+        {disconnectOnLeave: false},
+        {}
+    );
+   
     return (
         <VStack w="100%" spacing="3vw" py="3vw" paddingLeft = '10%' paddingRight = '10%' paddingBottom = '0'>
         <Box
@@ -26,28 +46,36 @@ import {
           bgSize="cover"
         >
           <VStack px="3" py="6">
-            <Heading
-              as="h4"
-              fontSize={{base:18,sm:20,md:25,lg:30}}
-              align="center"
-              color="#7B0D0D"
-              fontWeight="bold"
-              py={{base:2,sm:3,md:5,lg:6}}
-            >
-              Easter is the time where Christians all over the world gather to celebrate the resurrection of Jesus Christ.
-            </Heading>
-
-            <Heading
-              as="h4"
-              fontSize={{base:18,sm:20,md:25,lg:30}}
-              align="center"
-              color="#7B0D0D"
-              fontWeight="bold"
-              py={{base:2,sm:3,md:5,lg:6}}
-            >
-               Because of His passionate love for us, demonstrated through his death and resurrection on the cross, we can have love, joy, and greater freedom in our lives. 
-            </Heading>
-
+          
+          <Fade initialScale={2} in={enterCount > 0}>
+                <Box ref = {ref}>
+                    <Heading
+                    as="h4"
+                    fontSize={{base:18,sm:20,md:25,lg:30}}
+                    align="center"
+                    color="#7B0D0D"
+                    fontWeight="bold"
+                    py={{base:2,sm:3,md:5,lg:6}}
+                    >
+                    Easter is the time where Christians all over the world gather to celebrate the resurrection of Jesus Christ.
+                    </Heading>
+                </Box>
+            </Fade>
+           
+                <Box >
+                    <Heading
+                    as="h4"
+                    fontSize={{base:18,sm:20,md:25,lg:30}}
+                    align="center"
+                    color="#7B0D0D"
+                    fontWeight="bold"
+                    py={{base:2,sm:3,md:5,lg:6}}
+                    >
+                    Because of His passionate love for us, demonstrated through his death and resurrection on the cross, we can have love, joy, and greater freedom in our lives. 
+                    </Heading>
+                </Box>
+      
+           
             <Heading
               as="h4"
               fontSize={{base:18,sm:20,md:25,lg:30}}
