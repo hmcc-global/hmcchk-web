93af9f826e029a6adb93abea482ffaee6c1dac85
samyakobus <33675629+samyakobus@users.noreply.github.com>
2023-02-18 22:38:29 +0800

    GH-718: Changed link from home page pop-up to witness/home and enable calendar functions

--- ui/src/components/MainContainer.js
+++ ui/src/components/MainContainer.js
@@ -253,7 +253,8 @@ const MainContainer = () => {
           exact
           path="/witness/home"
           permissions={['public']}
-          component={WitnessHomeContainer}
+          // component={WitnessHomeContainer}
+          component={Easter2023Container}
         />
         <PrivateRoute
           path="/witness/testimonies/text/:id"
