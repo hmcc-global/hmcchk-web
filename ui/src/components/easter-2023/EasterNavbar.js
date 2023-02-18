986cd0e69649141ecddbfbb1014816af6848c5cb
samyakobus <33675629+samyakobus@users.noreply.github.com>
2023-02-18 15:35:40 +0800

    GH-718: Finished Sticky Header as added highlights to the EASTER section

--- ui/src/components/easter-2023/EasterNavbar.js
+++ ui/src/components/easter-2023/EasterNavbar.js
@@ -23,6 +23,8 @@ import {
 
   
   const EasterNavbar= () => {
+   
+
     const CircleIcon = (props) => (
         <Icon viewBox='0 0 200 200' {...props}>
           <path
@@ -43,7 +45,7 @@ import {
       };
 
     const handleClickScrollPrayerWall = () => {
-      const element = document.getElementById('easter-calendar');
+      const element = document.getElementById('prayer-wall');
       if (element) {
         // 👇 Will scroll smoothly to the top of the next section
         element.scrollIntoView({ behavior: 'smooth' });
@@ -51,7 +53,7 @@ import {
     };
 
     const handleClickScrollTestimonies = () => {
-        const element = document.getElementById('easter-calendar');
+        const element = document.getElementById('easter-witness');
         if (element) {
           // 👇 Will scroll smoothly to the top of the next section
           element.scrollIntoView({ behavior: 'smooth' });
@@ -60,56 +62,60 @@ import {
     
     return (
         
-        <hStack w="100%" spacing="3vw" py="3vw" paddingLeft = '10%' paddingRight = '10%' paddingBottom = '0'>
+        <hStack w="100%" spacing="0" py="0" paddingLeft = '0%' paddingRight = '0%' paddingBottom = '0'>
         <Box
           bgPosition="center"
           bgSize="cover"
         >
-          <hStack px="3" py="6">
+            <hStack px="3" py="6">
             <Button
-          color = '#FF810F'
-          paddingTop ={{ base: 23, sm: 23, md: 25, lg: 29 }}
-          paddingBottom ={{ base: 23, sm: 23, md: 25, lg: 29 }}
-          as={Link}
-          onClick={handleClickScrollEaster}
-          variant="link"
-          w={[200, 400]}
-          fontSize={["1.2em", "1.8em"]}
-        >
-          easter 
-        </Button>
-        <CircleIcon color='#FF810F'/>
-        <Button
-          color = '#FF619F'
-          paddingTop ={{ base: 23, sm: 23, md: 25, lg: 29 }}
-          paddingBottom ={{ base: 23, sm: 23, md: 25, lg: 29 }}
-          as={Link}
-          size="sm"
-          onClick={handleClickScrollPrayerWall}
-          variant="link"
-          w={[200, 400]}
-          borderRadius={10}
-          fontSize={["1.2em", "1.8em"]}
-        >
-          prayer wall
-        </Button>
-        <CircleIcon color='#FF619F'/>
-        <Button
-          color = '#FF3E02'
-          paddingTop ={{ base: 23, sm: 23, md: 25, lg: 29 }}
-          paddingBottom ={{ base: 23, sm: 23, md: 25, lg: 29 }}
-          as={Link}
-          size="sm"
-          onClick = {handleClickScrollTestimonies}
-          variant="link"
-          w={[200, 400]}
-          borderRadius={10}
-          fontSize={["1.2em", "1.8em"]}
-        >
-          witness testimonies 
-        </Button>
+                color = '#FF619F'
+                paddingTop ={{ base: 10, sm: 9, md: 25, lg: 29 }}
+                paddingBottom ={{ base: 10, sm: 9, md: 25, lg: 29 }}
+                as={Link}
+                size="sm"
+                onClick={handleClickScrollPrayerWall}
+                variant="link"
+                w={[70, 200]}
+                fontSize={["0.75em", "1.5em"]}
+            >
+                prayer wall
+            </Button>
+
+            <CircleIcon color='#FF619F'/>
+        
+            <Button
+                color = '#FF810F'
+                paddingTop ={{ base: 10, sm: 9, md: 25, lg: 29 }}
+                paddingBottom ={{ base: 10, sm: 9, md: 25, lg: 29 }}
+                as={Link}
+                onClick={handleClickScrollEaster}
+                variant="link"
+                w={[50, 70, 200]}
+                fontSize={["0.75em", "1.5em"]}
+            >
+                easter 
+            </Button>
+            
+            <CircleIcon color='#FF810F'/>
+     
+            <Button
+                color = '#FF3E02'
+                paddingTop ={{ base: 10, sm: 9, md: 25, lg: 29 }}
+                paddingBottom ={{ base: 10, sm: 9, md: 25, lg: 29 }}
+                as={Link}
+                size="sm"
+                onClick = {handleClickScrollTestimonies}
+                variant="link"
+                w={[120, 150, 300]}
+                borderRadius={10}
+                fontSize={["0.75em", "1.5em"]}
+            >
+                witness testimonies 
+            </Button>
           </hStack>
         </Box>
+        
       </hStack>
     )
   }
--- ui/src/components/easter-2023/EasterStory.js
+++ ui/src/components/easter-2023/EasterStory.js
@@ -46,34 +46,66 @@ import React,{useRef} from "react";
           bgSize="cover"
         >
           <VStack px="3" py="6">
-          
-          <Fade initialScale={2} in={enterCount > 0}>
-                <Box ref = {ref}>
-                    <Heading
-                    as="h4"
-                    fontSize={{base:18,sm:20,md:25,lg:30}}
-                    align="center"
-                    color="#7B0D0D"
-                    fontWeight="bold"
-                    py={{base:2,sm:3,md:5,lg:6}}
-                    >
-                    Easter is the time where Christians all over the world gather to celebrate the resurrection of Jesus Christ.
-                    </Heading>
-                </Box>
-            </Fade>
-           
-                <Box >
-                    <Heading
-                    as="h4"
-                    fontSize={{base:18,sm:20,md:25,lg:30}}
-                    align="center"
-                    color="#7B0D0D"
-                    fontWeight="bold"
-                    py={{base:2,sm:3,md:5,lg:6}}
-                    >
-                    Because of His passionate love for us, demonstrated through his death and resurrection on the cross, we can have love, joy, and greater freedom in our lives. 
-                    </Heading>
-                </Box>
+            <Text
+                textStyle={'playfair_display'}
+                textColor="#FF3E02"
+                fontWeight="1000"
+                lineHeight="110%"
+                fontSize={['30px', '60px']}
+                marginBottom="15px"
+                textAlign="center"
+                paddingTop = {['10','4']}
+              >
+                EASTER
+            </Text>
+            <Box>
+                <Heading
+                as="h4"
+                fontSize={{base:18,sm:20,md:25,lg:30}}
+                align="center"
+                color="#7B0D0D"
+                fontWeight="bold"
+                py={{base:2,sm:3,md:5,lg:6}}
+                >
+                <Text >
+                Easter is the time where Christians all over the world gather to celebrate the 
+                <Text
+                paddingLeft = '3'
+                paddingBottom = '5'
+                display= 'inline-block'
+                bgRepeat = 'no-repeat'
+                bgSize='100%'
+                bgImage={ ` 
+                url('${process.env.PUBLIC_URL}/images/easter-2023/jesus-christ.png')`}
+                >resurrection of Jesus Christ.</Text>
+                </Text>
+                
+                </Heading>
+            </Box>
+            <Box >
+                <Heading
+                as="h4"
+                fontSize={{base:18,sm:20,md:25,lg:30}}
+                align="center"
+                color="#7B0D0D"
+                fontWeight="bold"
+                py={{base:2,sm:3,md:5,lg:6}}
+                >
+                 
+                <Text >
+                Because of His passionate love for us, demonstrated through his death and resurrection on the cross, we can have
+                <Text
+                paddingLeft = '3'
+                paddingBottom = '5'
+                display= 'inline-block'
+                bgRepeat = 'no-repeat'
+                bgSize='100%'
+                bgImage={ ` 
+                url('${process.env.PUBLIC_URL}/images/easter-2023/greater-freedom.png')`}
+                >love, joy, and greater freedom in our lives. </Text>
+                </Text>
+                </Heading>
+            </Box>
       
            
             <Heading
@@ -84,7 +116,19 @@ import React,{useRef} from "react";
               fontWeight="bold"
               py={{base:2,sm:3,md:5,lg:6}}
             >
-              We no longer have to be held down by the world, but we can look to Jesus who is our ultimate hope.
+              
+              <Text >
+              We no longer have to be held down by the world, but we can look to Jesus
+                <Text
+                paddingLeft = '3'
+                paddingBottom = '5'
+                display= 'inline-block'
+                bgRepeat = 'no-repeat'
+                bgSize='100%'
+                bgImage={ ` 
+                url('${process.env.PUBLIC_URL}/images/easter-2023/our-ultimate-hope.png')`}
+                >who is our ultimate hope. </Text>
+                </Text>
             </Heading>
           </VStack>
         </Box>
