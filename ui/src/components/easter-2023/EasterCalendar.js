66279bccecd419c79a9eac411f1444be0a976fc1
Baeck016 <73063819+Baeck016@users.noreply.github.com>
2023-02-18 20:08:20 +0800

    GH-718: added calendar

--- ui/src/components/easter-2023/EasterCalendar.js
+++ ui/src/components/easter-2023/EasterCalendar.js
@@ -1,27 +1,45 @@
 import {
-    Container,
-    Text,
-    VStack,
-    Box,
-    Tabs,
-    Tab,
-    TabPanel,
-    TabPanels,
-    TabList,
-    Image,
-    Stack,
-    Button,
-    Flex,
-    Link,
-    Center,
-    Spacer,
-    Heading,
-  } from '@chakra-ui/react';
-import "@fontsource/playfair-display"; // Defaults to weight 400.
-  const EasterCalendar = () => {
-    return (
-      
-      <Flex flexWrap="wrap" gap='0'>
+  Container,
+  Text,
+  VStack,
+  Box,
+  Tabs,
+  Tab,
+  TabPanel,
+  TabPanels,
+  TabList,
+  Image,
+  Stack,
+  Button,
+  Flex,
+  Link,
+  Center,
+  Spacer,
+  Heading,
+} from '@chakra-ui/react';
+
+import { generateGoogleCalendarLink } from '../helpers/eventsHelpers';
+
+const goodFriday = {
+  time: '8:00 PM',
+  title: 'Good Friday Service',
+  startDate: '2023-04-07T00:00:00.000+08:00',
+  endDate: '2023-04-07T00:00:00.000+08:00',
+  recurrence: '7',
+  location: 'TBA',
+};
+const easter = {
+  time: '10:00 AM',
+  title: 'Easter Celebration',
+  startDate: '2023-04-09T00:00:00.000+08:00',
+  endDate: '2023-04-09T00:00:00.000+08:00',
+  recurrence: '7',
+  location: 'TBA',
+};
+
+const EasterCalendar = () => {
+  return (
+    <Flex flexWrap="wrap" gap="0">
       <Box
         flex={1}
         //borderWidth="1px"
@@ -30,44 +48,64 @@ import "@fontsource/playfair-display"; // Defaults to weight 400.
         bgPosition="center"
         bgSize="cover"
         //shadow="lg"
-       
+
         textAlign="center"
         px={[8, 10]}
         py={[8, 16]}
         m={2}
       >
-        <Heading as="h2" fontSize={["2em", "3.5em"]}  color="#67298E" fontWeight={700} fontFamily = 'PlayfairDisplay'>
+        <Heading
+          as="h2"
+          fontSize={['2em', '3.5em']}
+          color="#67298E"
+          fontWeight={700}
+          fontFamily="playfair_display"
+        >
           GOOD FRIDAY
         </Heading>
-        <Heading as="h2" fontSize={["2em", "3.5em"]}  color="#67298E" fontWeight={700}>
-          <Text textStyle={'playfair_display'} fontWeight='black'>
-          SERVICE
+        <Heading
+          as="h2"
+          fontSize={['2em', '3.5em']}
+          color="#67298E"
+          fontWeight={700}
+        >
+          <Text textStyle={'playfair_display'} fontWeight="black">
+            SERVICE
           </Text>
-          
         </Heading>
-        <Text fontSize={["1.2em", "1.8em"]} color ="#7B0D0D" fontWeight={800} paddingTop = {{ base: 3, sm: 4, md: 5, lg: 6 }}>
+        <Text
+          fontSize={['1.2em', '1.8em']}
+          color="#7B0D0D"
+          fontWeight={800}
+          paddingTop={{ base: 3, sm: 4, md: 5, lg: 6 }}
+        >
           Friday, 7 Apr 2023
         </Text>
-        <Text fontSize={["1.2em", "1.8em"]} color ="#7B0D0D" fontWeight={800} >
+        <Text fontSize={['1.2em', '1.8em']} color="#7B0D0D" fontWeight={800}>
           8:00 PM
         </Text>
-        <Text fontSize={["1.2em", "1.8em"]} color ="#7B0D0D" fontWeight={800}  paddingBottom = {{ base: 3, sm: 4, md: 5, lg: 6 }}>
+        <Text
+          fontSize={['1.2em', '1.8em']}
+          color="#7B0D0D"
+          fontWeight={800}
+          paddingBottom={{ base: 3, sm: 4, md: 5, lg: 6 }}
+        >
           TBA
-        </Text >
+        </Text>
         <Button
-          bgColor = "#67298E"
-          color = '#FFFFFF'
-          paddingTop ={{ base: 23, sm: 23, md: 25, lg: 29 }}
-          paddingBottom ={{ base: 23, sm: 23, md: 25, lg: 29 }}
+          bgColor="#67298E"
+          color="#FFFFFF"
+          paddingTop={{ base: 23, sm: 23, md: 25, lg: 29 }}
+          paddingBottom={{ base: 23, sm: 23, md: 25, lg: 29 }}
           as={Link}
           size="sm"
-          href="/visit-us"
+          href={generateGoogleCalendarLink(goodFriday)}
           variant="outline"
           w={[200, 400]}
           borderRadius={10}
-          fontSize={["1.2em", "1.8em"]}
+          fontSize={['1.2em', '1.8em']}
         >
-          Add to Calendar 
+          Add to Calendar
         </Button>
       </Box>
       <Box
@@ -83,42 +121,61 @@ import "@fontsource/playfair-display"; // Defaults to weight 400.
         py={[8, 16]}
         m={2}
       >
-        <Heading as="h2" fontSize={["2em", "3.5em"]} color = "#FF619F" fontWeight={700} fontFamily = 'Playfair'>
-          EASTER 
+        <Heading
+          as="h2"
+          fontSize={['2em', '3.5em']}
+          color="#FF619F"
+          fontWeight={700}
+          fontFamily="Playfair"
+        >
+          EASTER
         </Heading>
-        <Heading as="h2" fontSize={["2em", "3.5em"]} color = "#FF619F" fontWeight={700} fontFamily = 'Playfair'>
+        <Heading
+          as="h2"
+          fontSize={['2em', '3.5em']}
+          color="#FF619F"
+          fontWeight={700}
+          fontFamily="Playfair"
+        >
           CELEBRATION
         </Heading>
-        <Text fontSize={["1.2em", "1.8em"]} color ="#7B0D0D" fontWeight={800}  paddingTop = {{ base: 3, sm: 4, md: 5, lg: 6 }} >
+        <Text
+          fontSize={['1.2em', '1.8em']}
+          color="#7B0D0D"
+          fontWeight={800}
+          paddingTop={{ base: 3, sm: 4, md: 5, lg: 6 }}
+        >
           Sunday, 9 Apr 2023
         </Text>
-        <Text fontSize={["1.2em", "1.8em"]} color ="#7B0D0D" fontWeight={800} >
+        <Text fontSize={['1.2em', '1.8em']} color="#7B0D0D" fontWeight={800}>
           10:00 AM
         </Text>
-        <Text fontSize={["1.2em", "1.8em"]} color ="#7B0D0D" fontWeight={800}  paddingBottom = {{ base: 3, sm: 4, md: 5, lg: 6 }}>
+        <Text
+          fontSize={['1.2em', '1.8em']}
+          color="#7B0D0D"
+          fontWeight={800}
+          paddingBottom={{ base: 3, sm: 4, md: 5, lg: 6 }}
+        >
           TBA
         </Text>
         <Button
-          bgColor = "#FF619F"
-          color = '#FFFFFF'
-          paddingTop ={{ base: 23, sm: 23, md: 25, lg: 29 }}
-          paddingBottom ={{ base: 23, sm: 23, md: 25, lg: 29 }}
+          bgColor="#FF619F"
+          color="#FFFFFF"
+          paddingTop={{ base: 23, sm: 23, md: 25, lg: 29 }}
+          paddingBottom={{ base: 23, sm: 23, md: 25, lg: 29 }}
           as={Link}
           size="sm"
-          href="/events"
+          href={generateGoogleCalendarLink(easter)}
           variant="outline"
           w={[200, 400]}
           borderRadius={10}
-          fontSize={["1.2em", "1.8em"]}
+          fontSize={['1.2em', '1.8em']}
         >
           Add to Calendar
         </Button>
       </Box>
     </Flex>
-    )
-  }
-  
- 
-  
+  );
+};
 
 export default EasterCalendar;
