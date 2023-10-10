6922ac81e2c63f90a997e17d9daf1547fe5467b0
Kristian Suhartono <blackxephos2@gmail.com>
2023-10-10 10:55:05 +0800

    GH-821: replaced witness banner and section to ripple out

--- ui/src/components/NavigationBar/NavBar.js
+++ ui/src/components/NavigationBar/NavBar.js
@@ -32,7 +32,8 @@ import { signout } from '../../reducers/userSlice';
 import { customAxios as axios } from '../helpers/customAxios';
 import MainMenu from './MainMenu';
 import { useHistory } from 'react-router-dom';
-import WitnessBanner from '../witness/WitnessBanner';
+// import WitnessBanner from '../witness/WitnessBanner';
+import RippleOutBanner from '../ripple-out/RippleOutBanner';
 import { BsFillPersonFill } from 'react-icons/bs';
 
 const NavBar = (props) => {
@@ -463,7 +464,7 @@ const NavBar = (props) => {
           </Container>
         </Flex>
       </Flex>
-      {isHomePage && <WitnessBanner />}
+      {isHomePage && <RippleOutBanner />}
       {/* {currDate === 'Wed' && !isOnlineSermon ? (
         <Flex
           w="100vw"
---
+++ ui/src/components/ripple-out/RippleOutBanner.js
@@ -0,0 +1,38 @@
+import { Flex, Center, Link, Box, HStack, Text } from '@chakra-ui/react';
+
+const RippleOutBanner = () => {
+  return (
+    <Flex
+      w="100vw"
+      bgColor="#FFFFFF95"
+      h={['10vh', '12vh']}
+      p={2}
+      bgPosition="center"
+      bgSize="cover"
+      justify="center"
+      display="flex"
+    >
+      <Flex w="100vw" justify="space-around">
+        <Center>
+          <Link href="/ripple-out" _hover={{}}>
+            <HStack h="100%" spacing="1">
+              <Box>
+                <Text
+                  position="relative"
+                  fontSize={['0.7em', 'xl']}
+                  as="i"
+                  color="#182E57"
+                  fontWeight={600}
+                >
+                  CLICK HERE TO LEARN MORE ABOUT THE RIPPLE OUT CAMPAIGN !
+                </Text>
+              </Box>
+            </HStack>
+          </Link>
+        </Center>
+      </Flex>
+    </Flex>
+  );
+};
+
+export default RippleOutBanner;
