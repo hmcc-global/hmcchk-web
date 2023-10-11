3fdb910162e194511d9bd8b7e5d32200da867627
Tamara Yustian <tamarayustian@gmail.com>
2023-10-12 00:36:48 +0800

    GH-816: Missing field

--- server/api/models/Faq.js
+++ server/api/models/Faq.js
@@ -1,26 +1,28 @@
 module.exports = {
-  pageTopic: {
-    type: 'string',
-    required: true,
-  },
-  question: {
-    type: 'string',
-    required: true,
-  },
-  answer: {
-    type: 'string',
-    required: true,
-  },
-  createdBy: {
-    type: 'string',
-    required: true,
-  },
-  lastUpdatedBy: {
-    type: 'string',
-    required: true,
-  },
-  isDeleted: {
-    type: 'boolean',
-    defaultsTo: false,
+  attributes: {
+    pageTopic: {
+      type: 'string',
+      required: true,
+    },
+    question: {
+      type: 'string',
+      required: true,
+    },
+    answer: {
+      type: 'string',
+      required: true,
+    },
+    createdBy: {
+      type: 'string',
+      required: true,
+    },
+    lastUpdatedBy: {
+      type: 'string',
+      required: true,
+    },
+    isDeleted: {
+      type: 'boolean',
+      defaultsTo: false,
+    },
   },
 };
