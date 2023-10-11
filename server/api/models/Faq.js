ac1f7807872debe189d04f9378821ca4263bb9c9
Tamara Yustian <tamarayustian@gmail.com>
2023-10-11 13:00:01 +0800

    GH-816: Add FAQ model

---
+++ server/api/models/Faq.js
@@ -0,0 +1,26 @@
+module.exports = {
+  pageTopic: {
+    type: 'string',
+    required: true,
+  },
+  question: {
+    type: 'string',
+    required: true,
+  },
+  answer: {
+    type: 'string',
+    required: true,
+  },
+  createdBy: {
+    type: 'string',
+    required: true,
+  },
+  lastUpdatedBy: {
+    type: 'string',
+    required: true,
+  },
+  isDeleted: {
+    type: 'boolean',
+    defaultsTo: false,
+  },
+};
