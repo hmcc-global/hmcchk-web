0ad2b4d2877f4ec811fad3e036cbfd192755226f
Yoo Yuen Yau <yuenyau989@hotmail.com>
2023-10-13 00:40:55 +0800

    GH-816: Added Admin FAQ Manager

--- ui/src/components/MainContainer.js
+++ ui/src/components/MainContainer.js
@@ -38,6 +38,7 @@ import AdminAnnouncementContainer from './admin/announcements/AdminAnnouncementC
 import PrayerFloatButton from './connect-prayer/PrayerFLoatButton';
 import RippleOutContainer from './ripple-out/RippleOutContainer';
 import AdminFundraiseContainer from './admin/fundraise/AdminFundraiseContainer';
+import AdminFaqContainer from './admin/faqs/AdminFaqContainer';
 
 const MainContainer = () => {
   return (
@@ -224,6 +225,12 @@ const MainContainer = () => {
           path="/admin/announcements"
           permissions={['ministry', 'tc', 't3ch', 'admin', 'stewardship']}
           component={AdminAnnouncementContainer}
+        />
+        <PrivateRoute
+          exact
+          path="/admin/faqs"
+          permissions={['t3ch', 'admin', 'stewardship']}
+          component={AdminFaqContainer}
         />
         <PrivateRoute
           exact
