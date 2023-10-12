c53e3d17aeb3883ea958d114701dbcdf8b4e4741
Tamara Yustian <tamarayustian@gmail.com>
2023-10-12 14:08:35 +0800

    GH-826: Add comments

--- server/api/models/Fundraise.js
+++ server/api/models/Fundraise.js
@@ -1,13 +1,16 @@
 module.exports = {
   attributes: {
+    // 1 campaign can have many categories. but this would be the keyword for the campaign
     campaignName: {
       type: 'string',
       required: true,
     },
+    // name for giving category e.g. "Food for the poor"
     categoryName: {
       type: 'string',
       required: true,
     },
+    // this would be the key for the category e.g. "food-for-the-poor"
     categoryKey: {
       type: 'string',
       required: true,
@@ -22,6 +25,16 @@ module.exports = {
       type: 'json',
       columnType: 'array',
       defaultsTo: [],
+      example: [
+        {
+          milestoneName: 'Baskets of food',
+          milestoneAmount: 1000,
+        },
+        {
+          milestoneName: 'Bags of rice',
+          milestoneAmount: 40000,
+        },
+      ],
     },
     createdBy: {
       type: 'string',
