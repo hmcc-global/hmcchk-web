eb846a15ddbc03b9f2327a31e86dd5a81a5564c8
randall <randall2000n@gmail.com>
2023-10-12 22:42:28 +0800

    GH-817: Center image

--- ui/src/components/ripple-out/RippleOutContainer.js
+++ ui/src/components/ripple-out/RippleOutContainer.js
@@ -109,7 +109,7 @@ const RippleOutContainer = () => {
         {/* Ripple steps drops image - desktop & mobile */}
         <Box
           my={9}
-          h={['50vh', '60vh', '60vh', '80vh']}
+          h={['60vh', '60vh', '60vh', '90vh']}
           w="screen"
           bgImage={`url(${process.env.PUBLIC_URL}/images/ripple-out/ripple-steps.png)`}
           bgSize="contain"
@@ -118,7 +118,7 @@ const RippleOutContainer = () => {
         />
         <Box
           my={9}
-          h={['50vh', '60vh', '60vh', '80vh']}
+          h={['60vh', '60vh', '60vh', '90vh']}
           w="screen"
           bgImage={`url(${process.env.PUBLIC_URL}/images/ripple-out/ripple-steps-mobile.png)`}
           bgSize="contain"
