9b8dad54a907b3447fcc8149902a56b380e91ec3
tamarayustian <tamarayustian@gmail.com>
2023-10-17 00:08:35 +0800

    GH-844: Add pledge section and fix extra padding bug (#846)
    * GH-844: Allow one campaign to have multiple categories
    
    * GH-844: Remove extra padding and add background color for weird screen sizes
    
    * GH-844: Add pledge section
    
    * GH-844: Add text for pledge section
    
    * GH-844: Fix typo

--- ui/src/components/ripple-out/ProgressBar.js
+++ ui/src/components/ripple-out/ProgressBar.js
@@ -1,6 +1,6 @@
 import Tippy from '@tippyjs/react';
 import 'tippy.js/dist/tippy.css';
-import { Box, Text } from '@chakra-ui/react';
+import { Box } from '@chakra-ui/react';
 
 const ProgressBar = (props) => {
   const { bgcolor, amount, milestones, target } = props;
@@ -9,22 +9,22 @@ const ProgressBar = (props) => {
 
   const containerStyles = {
     width: '100%',
-    backgroundColor: "#BEC7DA",
+    backgroundColor: '#BEC7DA',
     borderRadius: 50,
     margin: 10,
-    position: "relative",
-    overflow: "hidden",
+    position: 'relative',
+    overflow: 'hidden',
   };
 
   const dotStyle = {
-    backgroundColor: "white",
-    borderRadius: "50%",
-    display: "inline-block",
-    boxShadow: "0 5px 2px -2px gray",
+    backgroundColor: 'white',
+    borderRadius: '50%',
+    display: 'inline-block',
+    boxShadow: '0 5px 2px -2px gray',
   };
 
   const fillerStyles = {
-    position: "absolute",
+    position: 'absolute',
     height: '100%',
     width: `${completed}%`,
     backgroundColor: bgcolor,
@@ -39,35 +39,38 @@ const ProgressBar = (props) => {
     <Box style={containerStyles} height={[50, 100]}>
       <div style={fillerStyles} />
       {reversedMilestones.map((milestone) => {
-          const percentage = (milestone.milestoneAmount / target) * 100;
+        const percentage = (milestone.milestoneAmount / target) * 100;
 
-          const dotContainerStyle = {
-            position: "absolute",
-            textAlign: 'right',
-            width: `${percentage}%`,
-            height: '100%',
-            display: "flex",
-            justifyContent: "flex-end",
-            alignItems: "center",
-          };
+        const dotContainerStyle = {
+          position: 'absolute',
+          textAlign: 'right',
+          width: `${percentage}%`,
+          height: '100%',
+          display: 'flex',
+          justifyContent: 'flex-end',
+          alignItems: 'center',
+        };
         return (
           <div style={dotContainerStyle}>
-            <Tippy content={
-              <Box textAlign="center" borderRadius={30}>
-                <Box>
-                  {`$${Number(milestone.milestoneAmount).toLocaleString()}`}
+            <Tippy
+              content={
+                <Box textAlign="center" borderRadius={30}>
+                  <Box>
+                    {`$${Number(milestone.milestoneAmount).toLocaleString()}`}
+                  </Box>
+                  <Box>{milestone.milestoneName}</Box>
                 </Box>
-                <Box>
-                  {milestone.milestoneName}
-                </Box>
-              </Box>
-            }
-            placement="bottom"
+              }
+              placement="bottom"
             >
-              <Box style={dotStyle} height={["20px", "30px"]} width={["20px", "30px"]} />
+              <Box
+                style={dotStyle}
+                height={['20px', '30px']}
+                width={['20px', '30px']}
+              />
             </Tippy>
           </div>
-        )
+        );
       })}
     </Box>
   );
