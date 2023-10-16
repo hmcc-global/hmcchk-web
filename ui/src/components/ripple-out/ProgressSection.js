a6dfaa78cfdaa1c56beb8df300bb9ecbc7fce914
tamarayustian <tamarayustian@gmail.com>
2023-10-17 00:08:35 +0800

    GH-844: Add pledge section and fix extra padding bug (#846)
    * GH-844: Allow one campaign to have multiple categories
    
    * GH-844: Remove extra padding and add background color for weird screen sizes
    
    * GH-844: Add pledge section
    
    * GH-844: Add text for pledge section
    
    * GH-844: Fix typo

--- ui/src/components/ripple-out/ProgressSection.js
+++ ui/src/components/ripple-out/ProgressSection.js
@@ -1,7 +1,7 @@
-import { Box, Container, Flex, Text, Center } from '@chakra-ui/react';
+import { Box, Container, Flex, Text, Center, Button } from '@chakra-ui/react';
 import ProgressBar from './ProgressBar';
 import { useEffect, useState } from 'react';
-import { customAxios as axios } from "../helpers/customAxios";
+import { customAxios as axios } from '../helpers/customAxios';
 import MilestoneProgressBar from './MilestoneProgressBar';
 
 // TODO: Remove later. Keeping it here in case someone wants to test locally with dummy data.
@@ -27,8 +27,9 @@ import MilestoneProgressBar from './MilestoneProgressBar';
 //   ]
 // }
 
-const ProgressSection = () => {
+const ProgressSection = ({ bodyFontSize }) => {
   const [fundraiseData, setFundraiseData] = useState(null);
+  const [pledgeData, setPledgeData] = useState(null);
 
   useEffect(() => {
     fetchFundraiseData();
@@ -37,12 +38,18 @@ const ProgressSection = () => {
   const fetchFundraiseData = async () => {
     const res = await axios.get('/api/fundraise/get', {
       params: {
-        campaignName: 'Ripple Out'
-      }
+        campaignName: 'Ripple Out',
+      },
     });
     const { data } = res;
 
-    setFundraiseData(data[0]);
+    for (const item of data) {
+      if (item.categoryKey === 'funds') {
+        setFundraiseData(item);
+      } else if (item.categoryKey === 'pledge') {
+        setPledgeData(item);
+      }
+    }
   };
 
   const getTarget = (milestones) => {
@@ -54,42 +61,46 @@ const ProgressSection = () => {
     const milestone = milestones[i];
     var diff;
 
-    if (i == 0) {
+    if (i === 0) {
       diff = milestone.milestoneAmount;
     } else {
-      const prevMilestone = milestones[i-1];
+      const prevMilestone = milestones[i - 1];
       diff = milestone.milestoneAmount - prevMilestone.milestoneAmount;
     }
 
-    return `${milestone.milestoneName} (+$${Number(diff).toLocaleString()} BY ${milestone.milestoneDeadline})`;
-  }
+    return `${milestone.milestoneName} (+$${Number(diff).toLocaleString()} BY ${
+      milestone.milestoneDeadline
+    })`;
+  };
 
   const getCurrentMilestoneIndex = (milestones, amount) => {
-    return milestones.findIndex((milestone) => milestone.milestoneAmount > amount);
-  }
+    return milestones.findIndex(
+      (milestone) => milestone.milestoneAmount > amount
+    );
+  };
 
   const getMilestoneLevels = (milestones, amount, i) => {
     const milestone = milestones[i];
     var diff;
 
-    if (i == 0) {
+    if (i === 0) {
       diff = milestone.milestoneAmount;
     } else {
-      const prevMilestone = milestones[i-1];
+      const prevMilestone = milestones[i - 1];
       diff = milestone.milestoneAmount - prevMilestone.milestoneAmount;
     }
 
     var prevMilestoneAmount;
-    if (i == 0) {
+    if (i === 0) {
       prevMilestoneAmount = 0;
     } else {
-      prevMilestoneAmount = milestones[i-1].milestoneAmount;
+      prevMilestoneAmount = milestones[i - 1].milestoneAmount;
     }
 
     const currentMilestone = getCurrentMilestoneIndex(milestones, amount);
 
     var achieved;
-    if (i == currentMilestone) {
+    if (i === currentMilestone) {
       achieved = amount - prevMilestoneAmount;
     } else if (currentMilestone === -1) {
       achieved = diff;
@@ -103,7 +114,7 @@ const ProgressSection = () => {
       achieved,
       target: diff,
     };
-  }
+  };
 
   const amount = fundraiseData?.amount;
   const givers = fundraiseData?.givers;
@@ -116,69 +127,193 @@ const ProgressSection = () => {
       display={['block', 'block']}
       marginY={[0, 10]}
     >
-      {
-        fundraiseData != null &&
+      {fundraiseData != null && (
         <div>
           <Container maxW="container.xl" h="100%" paddingX={0}>
             <Box>
-              <Text fontWeight="extrabold" fontSize={["3xl", "5xl"]}>MILESTONES</Text>
+              <Text fontWeight="extrabold" fontSize={['3xl', '5xl']}>
+                MILESTONES
+              </Text>
             </Box>
-            <Flex justifyContent="space-between" marginX={[0, 2]} lineHeight="30px" marginTop={5}>
+            <Flex
+              justifyContent="space-between"
+              marginX={[0, 2]}
+              lineHeight="30px"
+              marginTop={5}
+            >
               <Box textAlign="left">
                 <Box>
-                  <Text as="b" fontSize={["xl", "3xl"]}>RAISED</Text>
+                  <Text as="b" fontSize={['md', '3xl']}>
+                    RAISED
+                  </Text>
                 </Box>
                 <Box>
-                  <Text as="b" fontSize={["2xl", "4xl"]}>{`$${Number(amount).toLocaleString()}`}</Text>
+                  <Text as="b" fontSize={['lg', '4xl']}>
+                    {`$${Number(amount).toLocaleString()}`}
+                  </Text>
                 </Box>
               </Box>
               <Box textAlign="center">
                 <Box>
-                  <Text as="b" fontSize={["xl", "3xl"]}>SUPPORTERS</Text>
+                  <Text as="b" fontSize={['xl', '3xl']}>
+                    SUPPORTERS
+                  </Text>
                 </Box>
                 <Box>
-                  <Text as="b" fontSize={["2xl", "4xl"]}>{givers}</Text>
+                  <Text as="b" fontSize={['2xl', '4xl']}>
+                    {givers}
+                  </Text>
                 </Box>
               </Box>
               <Box textAlign="right">
                 <Box>
-                  <Text as="b" fontSize={["xl", "3xl"]}>GOAL</Text>
+                  <Text as="b" fontSize={['md', '3xl']}>
+                    GOAL
+                  </Text>
                 </Box>
                 <Box>
-                  <Text as="b" fontSize={["2xl", "4xl"]}>{`$${Number(getTarget(milestones)).toLocaleString()}`}</Text>
+                  <Text as="b" fontSize={['lg', '4xl']}>
+                    {`$${Number(getTarget(milestones)).toLocaleString()}`}
+                  </Text>
                 </Box>
               </Box>
             </Flex>
             <Center flexDir="column">
-              <ProgressBar bgcolor="#7C9AD4" amount={amount} milestones={milestones} target={getTarget(milestones)} />
+              <ProgressBar
+                bgcolor="#7C9AD4"
+                amount={amount}
+                milestones={milestones}
+                target={getTarget(milestones)}
+              />
             </Center>
             <Flex justifyContent="center" marginX="2">
-              <Text fontSize={["xl", "3xl"]}>Click/hover on the white circles for milestones info!</Text>
+              <Text fontSize={['xl', '3xl']}>
+                Click/hover on the white circles for milestones info!
+              </Text>
             </Flex>
             <Box marginTop={8}>
-              <Text fontWeight="extrabold" fontSize={["3xl", "5xl"]}>NEEDS</Text>
+              <Text fontWeight="extrabold" fontSize={['3xl', '5xl']}>
+                NEEDS
+              </Text>
             </Box>
             <Box lineHeight="20px">
-              <Text fontSize={["xl", "3xl"]}>Reach a milestone to unlock the next one!</Text>
+              <Text fontSize={['xl', '3xl']}>
+                Reach a milestone to unlock the next one!
+              </Text>
             </Box>
             <Box borderColor="black" borderWidth={1} marginTop={6} padding={3}>
               {milestones.map((_, i) => {
                 const label = getMilestoneLabel(milestones, i);
                 const levels = getMilestoneLevels(milestones, amount, i);
 
-                var shouldDisplay = i <= getCurrentMilestoneIndex(milestones, amount) || getCurrentMilestoneIndex(milestones, amount) === -1;
+                var shouldDisplay =
+                  i <= getCurrentMilestoneIndex(milestones, amount) ||
+                  getCurrentMilestoneIndex(milestones, amount) === -1;
 
                 return (
                   <Box>
-                    <Text as="b" fontSize={["xl", "3xl"]} color={shouldDisplay ? "black" : "grey"}>{`MILESTONE ${i+1}: ${label}`}</Text>
-                    {shouldDisplay && <MilestoneProgressBar bgcolor="#7C9AD4" achieved={levels.achieved} target={levels.target} />}
+                    <Text
+                      as="b"
+                      fontSize={['xl', '3xl']}
+                      color={shouldDisplay ? 'black' : 'grey'}
+                    >{`MILESTONE ${i + 1}: ${label}`}</Text>
+                    {shouldDisplay && (
+                      <MilestoneProgressBar
+                        bgcolor="#7C9AD4"
+                        achieved={levels.achieved}
+                        target={levels.target}
+                      />
+                    )}
                   </Box>
                 );
               })}
             </Box>
+            {pledgeData && (
+              <>
+                <Box mt={8}>
+                  <Text fontWeight="extrabold" fontSize={['3xl', '5xl']}>
+                    PLEDGES
+                  </Text>
+                </Box>
+                <Box>
+                  <Text as="b" fontSize={bodyFontSize}>
+                    What is a Pledge?
+                  </Text>
+                  <Text fontSize={bodyFontSize}>
+                    A pledge is a commitment, both financial and symbolic, to
+                    support our Ripple Out Campaign. When you make a pledge, you
+                    are promising to donate a specific amount of money over a
+                    designated period of time by March 2024.
+                  </Text>
+
+                  <Text as="b" fontSize={bodyFontSize}>
+                    Why Make a Pledge?
+                  </Text>
+                  <Text fontSize={bodyFontSize}>
+                    By making a pledge, you play a crucial role in helping us
+                    realize our vision for a vibrant and nurturing space where
+                    we can reach out to more people with the Gospel. Your
+                    support will enable us to create an environment that fosters
+                    spiritual growth, offers a gathering place for our
+                    community, and facilitates meaningful outreach programs.
+                  </Text>
+                  <Text fontSize={bodyFontSize}>
+                    Pledge your support to the Ripple Out Campaign by filling
+                    out this form!
+                  </Text>
+                  <Box textAlign={['center', 'left']}>
+                    <Button
+                      my={2}
+                      as="a"
+                      href="https://bit.ly/ripple-pledge"
+                      target="_blank"
+                      variant="outline"
+                      fontSize="xl"
+                      bgColor="#ffffff"
+                      fontWeight="bold"
+                      borderColor="#182E57"
+                      color="#182E57"
+                    >
+                      SUBMIT YOUR PLEDGE
+                    </Button>
+                  </Box>
+                </Box>
+                <Flex
+                  justifyContent="space-between"
+                  marginX={['5%', '20%']}
+                  lineHeight="30px"
+                  marginTop={5}
+                >
+                  <Box textAlign="center">
+                    <Box>
+                      <Text as="b" fontSize={['lg', '3xl']}>
+                        TOTAL PLEDGED AMOUNT
+                      </Text>
+                    </Box>
+                    <Box>
+                      <Text as="b" fontSize={['xl', '4xl']}>
+                        {`$${pledgeData.amount.toLocaleString()}`}
+                      </Text>
+                    </Box>
+                  </Box>
+                  <Box textAlign="center">
+                    <Box>
+                      <Text as="b" fontSize={['lg', '3xl']}>
+                        PARTICIPANTS
+                      </Text>
+                    </Box>
+                    <Box>
+                      <Text as="b" fontSize={['xl', '4xl']}>
+                        {`${pledgeData.givers.toLocaleString()}`}
+                      </Text>
+                    </Box>
+                  </Box>
+                </Flex>
+              </>
+            )}
           </Container>
         </div>
-      }
+      )}
     </Box>
   );
 };
