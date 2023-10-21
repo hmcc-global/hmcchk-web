824c247981fc63487a24a315fe0b3dab67d35bd8
JoshNg123 <jnjw.999@gmail.com>
2023-10-21 22:39:12 +0800

    GH-856: Make basic layout of Goals Section

--- ui/src/components/witness/WitnessHomeContainer.js
+++ ui/src/components/witness/WitnessHomeContainer.js
@@ -7,17 +7,241 @@ import {
   VStack,
   Flex,
   Divider,
+  UnorderedList,
+  ListItem,
 } from '@chakra-ui/react';
-import { TriangleDownIcon } from '@chakra-ui/icons';
-import WitnessHomeVideoSection from './home-sections/WitnessHomeVideoSection';
-import WitnessHomeTextSection from './home-sections/WitnessHomeTextSection';
-import ShareTestimonyButton from './text-testimony/ShareTestimonyButton';
+// import { TriangleDownIcon } from '@chakra-ui/icons';
+// import WitnessHomeVideoSection from './home-sections/WitnessHomeVideoSection';
+// import WitnessHomeTextSection from './home-sections/WitnessHomeTextSection';
+// import ShareTestimonyButton from './text-testimony/ShareTestimonyButton';
 
 const WitnessHomeContainer = (props) => {
   return (
     <>
       <Container maxW="100%" m={0} p={0}>
-        <Box
+        <Flex justifyContent="flex-end" alignItems="center" mb="0.8em">
+          <Box>
+            <Text
+              color="#0053A4"
+              fontSize={['1.5em', '3em']}
+              fontWeight="bold"
+              lineHeight="1em"
+            >
+              THE <br /> GOALS
+            </Text>
+          </Box>
+          <Image
+            src={process.env.PUBLIC_URL + '/images/saturate/Saturate_Ring.svg'}
+            ml="0.5em"
+            boxSize={['3em', '10em']}
+          ></Image>
+        </Flex>
+        <Box mb="2em">
+          <Box bg="#0053A4" height={['2.5em', '4em']} mb="1em" pl="0.5em">
+            <Flex>
+              <Box
+                bg="white"
+                borderColor="#0053A4"
+                borderWidth="5px"
+                borderRadius="50%"
+                width={['50px', '100px']}
+                height={['50px', '100px']}
+                position="relative"
+                transform="translateY(-10%)"
+                display="flex"
+                justifyContent="center"
+                alignItems="center"
+                mr="0.4em"
+              >
+                <Image
+                  src={
+                    process.env.PUBLIC_URL +
+                    '/images/saturate/Saturate_Foundation.svg'
+                  }
+                  boxSize={['2em', '4em']}
+                ></Image>
+              </Box>
+              <Text color="white" fontSize={['1.8em', '3em']} fontWeight="700">
+                FOUNDATIONS
+              </Text>
+            </Flex>
+          </Box>
+          <Box pl={['2.5em', '5em']}>
+            <UnorderedList fontSize={['0.9em', '1.5em']}>
+              <ListItem>
+                <span style={{ color: '#0053A4', fontWeight: 'bold' }}>
+                  {' '}
+                  80%{' '}
+                </span>{' '}
+                of our members going through the DMF Curriculum.
+              </ListItem>
+              <ListItem>
+                <span style={{ color: '#0053A4', fontWeight: 'bold' }}>
+                  {' '}
+                  75%
+                </span>{' '}
+                of our members discipling 1 other person
+              </ListItem>
+            </UnorderedList>
+          </Box>
+        </Box>
+        <Box mb="2em">
+          <Box bg="#3B8AD7" height={['2.5em', '4em']} mb="1em" pl="0.5em">
+            <Flex>
+              <Box
+                bg="white"
+                borderColor="#3B8AD7"
+                borderWidth="5px"
+                borderRadius="50%"
+                width={['50px', '100px']}
+                height={['50px', '100px']}
+                position="relative"
+                transform="translateY(-10%)"
+                display="flex"
+                justifyContent="center"
+                alignItems="center"
+                mr="0.4em"
+              >
+                <Image
+                  src={
+                    process.env.PUBLIC_URL +
+                    '/images/saturate/Saturate_Impact.svg'
+                  }
+                  boxSize={['2em', '4em']}
+                ></Image>
+              </Box>
+              <Text color="white" fontSize={['1.8em', '3em']} fontWeight="700">
+                IMPACT
+              </Text>
+            </Flex>
+          </Box>
+          <Box pl={['2.5em', '5em']}>
+            <UnorderedList fontSize={['0.9em', '1.5em']}>
+              <ListItem>
+                <span style={{ color: '#0053A4', fontWeight: 'bold' }}>
+                  {' '}
+                  20{' '}
+                </span>{' '}
+                Campus Microchurches
+              </ListItem>
+              <ListItem>
+                <span style={{ color: '#0053A4', fontWeight: 'bold' }}>
+                  {' '}
+                  14{' '}
+                </span>{' '}
+                City Microchurches
+              </ListItem>
+            </UnorderedList>
+          </Box>
+        </Box>
+        <Box mb="2em">
+          <Box bg="#81B1E1" height={['2.5em', '4em']} mb="1em" pl="0.5em">
+            <Flex>
+              <Box
+                bg="white"
+                borderColor="#81B1E1"
+                borderWidth="5px"
+                borderRadius="50%"
+                width={['50px', '100px']}
+                height={['50px', '100px']}
+                position="relative"
+                transform="translateY(-10%)"
+                display="flex"
+                justifyContent="center"
+                alignItems="center"
+                mr="0.4em"
+              >
+                <Image
+                  src={
+                    process.env.PUBLIC_URL +
+                    '/images/saturate/Saturate_Link.svg'
+                  }
+                  boxSize={['2em', '4em']}
+                ></Image>
+              </Box>
+              <Text color="white" fontSize={['1.8em', '3em']} fontWeight="700">
+                LINK UP
+              </Text>
+            </Flex>
+          </Box>
+          <Box pl={['2.5em', '5em']}>
+            <UnorderedList fontSize={['0.9em', '1.5em']}>
+              <ListItem>
+                <span style={{ color: '#0053A4', fontWeight: 'bold' }}>
+                  {' '}
+                  5{' '}
+                </span>{' '}
+                Missional initiatives started by at least 5 different LGs
+                collborating together to reach 5 different HK districts
+              </ListItem>
+              <ListItem>
+                <span style={{ color: '#0053A4', fontWeight: 'bold' }}>
+                  {' '}
+                  1{' '}
+                </span>{' '}
+                church site in another district
+              </ListItem>
+            </UnorderedList>
+          </Box>
+        </Box>
+        <Box mb="2em">
+          <Box bg="#C3E0FD" height={['2.5em', '4em']} mb="1em" pl="0.5em">
+            <Flex>
+              <Box
+                bg="white"
+                borderColor="#C3E0FD"
+                borderWidth="5px"
+                borderRadius="50%"
+                width={['50px', '100px']}
+                height={['50px', '100px']}
+                position="relative"
+                transform="translateY(-10%)"
+                display="flex"
+                justifyContent="center"
+                alignItems="center"
+                mr="0.4em"
+              >
+                <Image
+                  src={
+                    process.env.PUBLIC_URL +
+                    '/images/saturate/Saturate_Launch.svg'
+                  }
+                  boxSize={['2em', '4em']}
+                ></Image>
+              </Box>
+              <Text
+                color="#0053A4"
+                fontSize={['1.8em', '3em']}
+                fontWeight="700"
+              >
+                LAUNCH
+              </Text>
+            </Flex>
+          </Box>
+          <Box pl={['2.5em', '5em']}>
+            <UnorderedList fontSize={['0.9em', '1.5em']}>
+              <ListItem>
+                <span style={{ color: '#0053A4', fontWeight: 'bold' }}>
+                  {' '}
+                  1{' '}
+                </span>{' '}
+                Transformational Hub
+              </ListItem>
+              <ListItem>
+                <span style={{ color: '#0053A4', fontWeight: 'bold' }}>
+                  {' '}
+                  1{' '}
+                </span>{' '}
+                international church plant on or near a university campus in an
+                Asian international city
+              </ListItem>
+            </UnorderedList>
+          </Box>
+        </Box>
+      </Container>
+
+      <Container maxW="100%" m={0} p={0}>
+        {/* <Box
           bgImage={[
             `linear-gradient(0deg, rgba(256, 256, 256), rgba(256, 256, 256, 0), rgba(256, 256, 256, 0), rgba(256, 256, 256, 0)), 
           url('${process.env.PUBLIC_URL}/images/witness/banner-home-mobile.png')`,
@@ -90,10 +314,46 @@ const WitnessHomeContainer = (props) => {
               </Box>
             </Flex>
           </Box>
-        </Box>
+        </Box> */}
       </Container>
+      {/* <Container maxW="100%" overflow="hidden" m={0} p={0}>
+        <Box display="flex" z-index="20" position="relative">
+          <Box bg="#0053A4">
+            <Text>
+              Hello Testing nvjajvnajvjadnvajvnjj anfjenfakeknakjafenfeakn naj
+            </Text>
+          </Box>
+        </Box>
+        <Box
+          width="60em"
+          height="60em"
+          position="relative"
+          borderRadius="50%"
+          boxShadow="inset 0 0 0 3em #c3e0fd, inset 0 0 0 5em #fff,
+          inset 0 0 0 8em #81b1e1, inset 0 0 0 10em #fff, inset 0 0 0 13em #3b8ad7,
+          inset 0 0 0 15em #fff, inset 0 0 0 18em #0a6ebd;"
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          z-index="-1"
+        >
+          <Text
+            position="relative"
+            right="60px"
+            fontWeight="900"
+            fontSize="3.5em"
+            textAlign="right"
+            color="#0053A4"
+          >
+            The
+            <br />
+            Goals
+          </Text>
+        </Box>
+      </Container> */}
+
       <Container maxW="100%" p={[0, 10]} centerContent>
-        <WitnessHomeVideoSection />
+        {/* <WitnessHomeVideoSection />
         <Divider
           orientation="horizontal"
           my={10}
@@ -101,9 +361,9 @@ const WitnessHomeContainer = (props) => {
           height="3px"
           backgroundColor="#DAC7BC"
         />
-        <WitnessHomeTextSection />
+        <WitnessHomeTextSection /> */}
       </Container>
-      <Container
+      {/* <Container
         maxW="container.sm"
         my={10}
         centerContent
@@ -122,7 +382,7 @@ const WitnessHomeContainer = (props) => {
         <Flex justifyContent="center" w={['90%', '70%']}>
           <ShareTestimonyButton />
         </Flex>
-      </Container>
+      </Container> */}
     </>
   );
 };
