fd6df2d312848aacae3285ea103431a0ec95db95
Albert Paredandan <albertparedandan@gmail.com>
2023-10-22 16:21:14 +0800

    GH-859: Add Saturate entries

--- ui/src/components/home/HomeContainer.js
+++ ui/src/components/home/HomeContainer.js
@@ -8,7 +8,8 @@ import NewHereSection from './NewHereSection';
 import PopupContainer from './PopupContainer';
 import HelloSermonSection from './HelloSermonSection';
 import { customAxios as axios } from '../helpers/customAxios';
-import WitnessSection from '../witness/WitnessSection';
+// import WitnessSection from '../witness/WitnessSection';
+import SaturateVisionSection from './SaturateVisionSection';
 
 const HomeContainer = (props) => {
   const { user } = props;
@@ -42,8 +43,9 @@ const HomeContainer = (props) => {
     <Flex direction="column">
       <HeroSection anchorTarget={sermonRef} />
       <HelloSermonSection ref={sermonRef} />
-      <WitnessSection />
+      {/* <WitnessSection /> */}
       <AboutSection />
+      <SaturateVisionSection />
       <EventsSection />
       <LifeGroupSection />
       <NewHereSection />
