b9c0eba78c0aec9539db4d6e9af882750b993b17
Alvin Harjanto <alvin.chip99@yahoo.com>
2023-11-05 12:13:36 +0800

    GH-880 Enlarged slick dots

--- ui/src/components/ripple-out/RippleOutStoryContainer.js
+++ ui/src/components/ripple-out/RippleOutStoryContainer.js
@@ -8,8 +8,6 @@ import {
   useMediaQuery,
 } from '@chakra-ui/react';
 import Slider from 'react-slick';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
 import RippleOutFaqSection from './RippleOutFaqSection';
 import RippleOutHeroSection from './RippleOutHeroSection';
 import { fontColor, headerFontSize } from './RippleOutTextStyle';
