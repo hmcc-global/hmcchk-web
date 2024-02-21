e96624130e0f9de0256ce05f6f65398697bc44ab
Baeck016 <73063819+Baeck016@users.noreply.github.com>
2024-02-21 23:18:19 +0800

    GH-931: user sermon notes models

---
+++ server/api/controllers/userSermonNotes/create-user-sermon-notes.js
@@ -0,0 +1,63 @@
+module.exports = {
+  friendlyName: 'Create user sermon notes',
+
+  description: 'Create a new user sermon notes',
+
+  inputs: {
+    sermonNoteId: {
+      type: 'string',
+      required: true,
+    },
+    userId: {
+      type: 'string',
+      required: true,
+    },
+    editContended: {
+      type: 'string',
+    },
+    themes: {
+      type: 'json',
+      defaultsTo: [],
+    },
+    stickyNote: {
+      type: 'json',
+      defaultsTo: [],
+    },
+  },
+
+  exits: {
+    success: {
+      description: 'New user sermon note was created successfully.',
+    },
+    error: {
+      description: 'Failed to create new user sermon note.',
+    },
+  },
+
+  fn: async function (
+    { sermonNoteId, userId, editContended, themes, stickyNote },
+    exits
+  ) {
+    const user = this.req.user.fullName;
+    sails.log.info(`${user}: Creating user sermon note: ${sermonNoteId}`);
+
+    try {
+      const newChildSermon = await UserSermonNote.create({
+        sermonNoteId,
+        userId,
+        editContended,
+        themes,
+        stickyNote,
+      });
+
+      if (!newChildSermon) {
+        return exits.nonSuccess(err);
+      }
+
+      return exits.success(newChildSermon);
+    } catch (err) {
+      sails.log(err);
+      return exits.error(err);
+    }
+  },
+};
