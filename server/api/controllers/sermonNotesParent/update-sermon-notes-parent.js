a63a71536c9410d6ec2f4349bc9c4c073547e47a
Baeck016 <73063819+Baeck016@users.noreply.github.com>
2024-03-01 18:29:25 +0800

    GH-931: made correction to the data types

--- server/api/controllers/sermonNotesParent/update-sermon-notes-parent.js
+++ server/api/controllers/sermonNotesParent/update-sermon-notes-parent.js
@@ -1,138 +1,139 @@
-// module.exports = {
-//     friendlyName: "Update Sermon Note parent",
+module.exports = {
+  friendlyName: 'Update Sermon Note parent',
 
-//     description: "Update Sermon Note parent",
+  description: 'Update Sermon Note parent',
 
-//     inputs: {
-//         sermonId: {
-//             required: true,
-//             type: 'string',
-//             unique: 'true',
-//         },
-//         title: {
-//             required: true,
-//             type: 'string'
-//         },
-//         subtitle: {
-//             required: false,
-//             type: 'string'
-//         },
-//         speaker: {
-//             required: true,
-//             type: 'string',
-//         },
-//         sermonSeries: {
-//             required: false,
-//             type: 'string',
-//         },
-//         date: {
-//             required: true,
-//             type: 'string',
-//         },
-//         imageLink: {
-//             required: false,
-//             type: 'string',
-//         },
-//         originalContent: {
-//             required: true,
-//             type: 'string',
-//         },
-//         userContent: {
-//             required: true,
-//             type: 'string',
-//         },
-//         sermonLink: {
-//             required: true,
-//             type: 'string'
-//         },
-//         serviceType: {
-//             required: true,
-//             type: 'string'
-//         },
-//         passage: {
-//             required: true,
-//             type: 'string'
-//         },
-//         isDeleted: {
-//             type: 'boolean',
-//             defaultsTo: false,
-//         },
-//         isPublished: {
-//             type: 'boolean',
-//             defaultsTo: false,
-//         },
-//     },
+  inputs: {
+    sermonId: {
+      required: true,
+      type: 'string',
+      unique: 'true',
+    },
+    title: {
+      required: true,
+      type: 'string',
+    },
+    subtitle: {
+      required: false,
+      type: 'string',
+    },
+    speaker: {
+      required: true,
+      type: 'string',
+    },
+    sermonSeries: {
+      required: false,
+      type: 'string',
+    },
+    date: {
+      required: true,
+      type: 'string',
+    },
+    imageLink: {
+      required: false,
+      type: 'string',
+    },
+    originalContent: {
+      required: true,
+      type: 'string',
+    },
+    userContent: {
+      type: 'string',
+    },
+    sermonLink: {
+      required: true,
+      type: 'string',
+    },
+    serviceType: {
+      required: true,
+      type: 'string',
+    },
+    passage: {
+      required: true,
+      type: 'string',
+    },
+    isDeleted: {
+      type: 'boolean',
+      defaultsTo: false,
+    },
+    isPublished: {
+      type: 'boolean',
+      defaultsTo: false,
+    },
+  },
 
-//     exits: {
-//         nonSuccess: {
-//             description: 'Error',
-//         },
-//         duplicateError: {
-//             description: 'Duplicate data found',
-//             statusCode: 409,
-//         },
-//     },
+  exits: {
+    nonSuccess: {
+      description: 'Error',
+    },
+    duplicateError: {
+      description: 'Duplicate data found',
+      statusCode: 409,
+    },
+  },
 
-//     fn: async function ({
-//         sermonId,
-//         title,
-//         subtitle,
-//         speaker,
-//         sermonSeries,
-//         date,
-//         imageLink,
-//         originalContent,
-//         userContent,
-//         sermonLink,
-//         serviceType,
-//         passage,
-//         isDeleted,
-//         isPublished,
-//     }, exits) {
-//         const user = this.req.user.fullName;
-//         sails.log.info(`${user}: Updating Sermon Notes: ${title}`);
-//         sails.log.info(`Title: ${title},  Desc: ${subtitle}`);
+  fn: async function (
+    {
+      sermonId,
+      title,
+      subtitle,
+      speaker,
+      sermonSeries,
+      date,
+      imageLink,
+      originalContent,
+      userContent,
+      sermonLink,
+      serviceType,
+      passage,
+      isDeleted,
+      isPublished,
+    },
+    exits
+  ) {
+    const user = this.req.user.fullName;
+    sails.log.info(`${user}: Updating Sermon Notes: ${title}`);
+    sails.log.info(`Title: ${title},  Desc: ${subtitle}`);
 
-//         try {
-//             if (isDeleted && isPublished) {
-//                 isPublished = false;
-//             }
+    try {
+      if (isDeleted && isPublished) {
+        isPublished = false;
+      }
 
-//             if (isPublished) {
-//                 res = await SermonNotesParent.update({ isPublished: true }).set({
-//                     isPublished: false,
-//                 });
-//             }
-//             let lastUpdatedBy = user;
+      if (isPublished) {
+        res = await SermonNotesParent.update({ isPublished: true }).set({
+          isPublished: false,
+        });
+      }
+      let lastUpdatedBy = user;
 
-//             const existing = await SermonNotesParent.updateOne({ id }).set({
-//                 title,
-//                 subtitle,
-//                 speaker,
-//                 sermonSeries,
-//                 date,
-//                 imageLink,
-//                 originalContent,
-//                 userContent,
-//                 sermonLink,
-//                 serviceType,
-//                 passage,
-//                 isDeleted,
-//                 isPublished,
-//             });
+      const existing = await SermonNotesParent.updateOne({ sermonId }).set({
+        title,
+        subtitle,
+        speaker,
+        sermonSeries,
+        date,
+        imageLink,
+        originalContent,
+        userContent,
+        sermonLink,
+        serviceType,
+        passage,
+        isDeleted,
+        isPublished,
+      });
 
-//             if (!existing) {
-//                 throw `Name: ${title}, with id: ${sermonId} , Sermon note parent doesn't exist`;
-//             }
+      if (!existing) {
+        throw `Name: ${title}, with id: ${sermonId} , Sermon note parent doesn't exist`;
+      }
 
-//             return exits.success(existing);
-//         } catch (err) {
-//             sails.log(err);
-//             if (err.code === 'E_UNIQUE') {
-//                 return exits.duplicateError(err);
-//             }
-//             return exits.nonSuccess(err);
-//         }
-//     },
-// };
-
+      return exits.success(existing);
+    } catch (err) {
+      sails.log(err);
+      if (err.code === 'E_UNIQUE') {
+        return exits.duplicateError(err);
+      }
+      return exits.nonSuccess(err);
+    }
+  },
+};
