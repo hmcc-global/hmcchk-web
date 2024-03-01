214f6dff166d78d0628cd46977cf72ef1288d43d
Alvin Harjanto <alvin.chip99@yahoo.com>
2024-03-02 01:11:48 +0800

    GH-949: Removed unnecessary code

--- ui/src/components/easter-2024/EasterContainer.js
+++ ui/src/components/easter-2024/EasterContainer.js
@@ -2,17 +2,17 @@ import { Box, Container, useBreakpointValue } from '@chakra-ui/react';
 import EasterNavbar from './EasterNavbar';
 import EasterBodyWeb from './EasterBodyWeb';
 import EasterBodyMobile from './EasterBodyMobile';
-import { useEffect } from 'react';
+
 const EasterContainer = (props) => {
   const isMobile = useBreakpointValue({ base: true, lg: false });
 
   return (
-    <Box overflow="initial">
+    <>
       <EasterNavbar />
       <Container maxW="container.lg" minHeight="fill" p={0} scrollBehavior="smooth">
         {isMobile ? <EasterBodyMobile /> : <EasterBodyWeb />}
       </Container>
-    </Box>
+    </>
   );
 };
 
