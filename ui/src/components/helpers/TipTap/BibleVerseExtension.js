88ae555e5a779378a987279371a57e200fd15ad6
Richie <ricia1212@gmail.com>
2024-03-24 05:33:14 +0800

    GH-948: show modal for bible verse on hover

--- ui/src/components/helpers/TipTap/BibleVerseExtension.js
+++ ui/src/components/helpers/TipTap/BibleVerseExtension.js
@@ -1,19 +1,96 @@
-// import Link from '@tiptap/extension-link'
+import { Node, mergeAttributes } from '@tiptap/core';
+import { Modal, ModalContent, ModalHeader, ModalBody } from '@chakra-ui/react';
+import { NodeViewWrapper, ReactNodeViewRenderer } from '@tiptap/react';
+import { getBiblePassage } from '../SermonNotes';
+import React, { useState } from 'react';
 
-import { Extension } from '@tiptap/core';
+export const BibleVersseNode = Node.create({
+  name: 'bibleVerse',
+  inline: true,
+  group: 'inline',
 
-const BibleVerseExtension = Extension.create({
-  name: 'bibleVerseExtension',
-  priority: 9000,
+  addAttributes() {
+    return {
+      bibleVerse: {
+        default: null,
+      },
+    };
+  },
   addCommands() {
     return {
-      setBibleVerse:
+      insertBibleVerse:
         (bibleVerse) =>
-        ({ commands }) => {
-          return commands.insertContent(bibleVerse);
+        ({ commands, editor }) => {
+          const position = editor.state.selection.from;
+          return commands.insertContentAt(position, {
+            type: 'bibleVerse',
+            attrs: { bibleVerse },
+          });
         },
     };
   },
+  parseHTML() {
+    return [
+      {
+        tag: 'bible-verse',
+      },
+    ];
+  },
+
+  renderHTML({ HTMLAttributes }) {
+    return ['bible-verse', mergeAttributes(HTMLAttributes)];
+  },
+  addNodeView() {
+    return ReactNodeViewRenderer((props) => {
+      return <BibleVerseWithModal {...props} />;
+    });
+  },
 });
 
-export default BibleVerseExtension;
+const BibleVerseModal = ({ isOpen, onClose, bibleVerse }) => {
+  const [passage, setPassage] = useState('');
+  (async () => {
+    const res = await getBiblePassage(bibleVerse);
+    setPassage(res);
+  })();
+  return (
+    <Modal isOpen={true} onClose={onClose}>
+      <ModalContent width="unset" height="unset">
+        <ModalHeader>{bibleVerse + ' (ESV)'}</ModalHeader>
+        <ModalBody>{passage}</ModalBody>
+      </ModalContent>
+    </Modal>
+  );
+};
+const BibleVerseWithModal = (props) => {
+  const verse = props.node.attrs.bibleVerse;
+  const [isOpen, setIsOpen] = React.useState(false);
+
+  const handleMouseEnter = () => {
+    setIsOpen(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsOpen(true);
+  };
+
+  return (
+    <NodeViewWrapper
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
+      <span style={{ textDecoration: 'underline', cursor: 'pointer' }}>
+        {verse}
+      </span>
+      {isOpen && (
+        <BibleVerseModal
+          isOpen={isOpen}
+          onClose={() => setIsOpen(false)}
+          bibleVerse={verse}
+        />
+      )}
+    </NodeViewWrapper>
+  );
+};
+
+// export default BibleVerseExtension;
--- ui/src/components/helpers/TipTap/index.js
+++ ui/src/components/helpers/TipTap/index.js
@@ -8,7 +8,7 @@ import StarterKit from '@tiptap/starter-kit';
 import React from 'react';
 import MenuBar from './MenuBar.js';
 import Link from '@tiptap/extension-link';
-import BibleVerseExtension from './BibleVerseExtension.js';
+import { BibleVersseNode } from './BibleVerseExtension.js';
 
 export default () => {
   const editor = useEditor({
@@ -20,7 +20,7 @@ export default () => {
       TaskList,
       TaskItem,
       Link,
-      BibleVerseExtension
+      BibleVersseNode
     ],
   });
 
