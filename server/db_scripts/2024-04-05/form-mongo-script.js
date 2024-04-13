a5c57f766cbfb90ad26e9004aa5e1617182b543c
Kristian Suhartono <ksuhartono97@users.noreply.github.com>
2024-04-14 00:02:35 +0800

    Release conditional forms (#1001)
    * Revert "GH-935: adjusted to check if last submission is within validity perio…" (#983)
    
    This reverts commit aab685d8a1038389d26e115f9100cb649fd52df5.
    
    * GH-988: replace old way of managing fields with new formfield class. (#991)
    
    * GH-988: replace old way of managing fields with new formfield class.
    
    * GH-988: remove console log
    
    * GH-988: move formfield and add uuid
    
    * GH-988: DB script to adapt form to new formField class (#994)
    
    * GH-988: Script to migrate form to new class
    
    * GH-988: Add instructions
    
    * GH-988: Add instruction to back up
    
    * GH-988: Add prefill to beginning of array (#1000)
    
    * GH-988: conditional forms backend (#993)
    
    * GH-988: replace old way of managing fields with new formfield class.
    
    * GH-988: move formfield and add uuid
    
    * GH-988: enabled manipulation of conditional form children
    
    * GH-988
    
    * GH-988: skeleton code for front end changes
    
    * GH-988: some of the code didn't get saved
    
    * GH-988: Add logic to conditionally render child forms
    
    * GH-988: fixed issues for conditional form fields
    
    * GH-988: Fix some conditional fields not binding value, unrendering conditional fields should reset its value
    
    * GH-988: Show all conditional fields in formDataViewer
    
    * GH-988: spacing between conditional form fields and normal form fields and required tick box
    
    * GH-988: edited form editor to be text area
    
    * GH-988: added more spacing between fields for conditional fields
    
    * GH-988: addressed missed changes
    
    ---------
    
    Co-authored-by: Albert Paredandan <albertparedandan@gmail.com>
    
    * GH-935: adjusted to check if last submission is within validity period (#1002)
    
    ---------
    
    Co-authored-by: tamarayustian <tamarayustian@gmail.com>
    Co-authored-by: Albert Paredandan <albertparedandan@gmail.com>

---
+++ server/db_scripts/2024-04-05/form-mongo-script.js
@@ -0,0 +1,63 @@
+/**
+ * How to run script:
+ * 1. Open MongoDB Compass
+ *
+ * 2. Back up the db first before you do anything. Export the
+ * original json/csv of the collection that you will be touching
+ *
+ * 3. Open the mongo shell at the bottom. Please test at
+ * the test db first before you start on prod db
+ *
+ * 4. Connect to the MongoDB server by running either one
+ * of these commands:
+ * use hmcchk_db;
+ * use hmcchk_db_test;
+ *
+ * 5. Copy and paste the script below to the shell
+ */
+
+// Add id for all formFields
+db.form.updateMany(
+  { formFields: { $ne: null, $type: 'array' } },
+  { $set: { 'formFields.$[].id': ObjectId() } }
+);
+
+// Find documents in the collection
+const documents = db.form.find({ formFields: { $ne: null, $type: 'array' } });
+
+// Loop through the documents
+documents.forEach((doc) => {
+  // Loop through the options field in formFields array
+  let tempPrefill = [];
+
+  doc.formFields.forEach((field) => {
+    if (field.fieldName !== 'prefill') return; // Skip if not prefill field
+
+    field.options.forEach((option) => {
+      console.log(`${doc._id} | ${option} -- UPDATING`);
+
+      // Create a new formField object
+      const newFormField = {
+        id: ObjectId(),
+        fieldName: option,
+        fieldType: 'prefill',
+        required: true,
+      };
+
+      tempPrefill.push(newFormField);
+    });
+  });
+
+  // Insert the new formField object into the formFields array
+  doc.formFields.unshift(...tempPrefill);
+
+  // Remove the original prefill field
+  doc.formFields = doc.formFields.filter((field) => {
+    return field.fieldName !== 'prefill';
+  });
+
+  // Update the document with the modified formFields array
+  db.form.updateOne({ _id: doc._id }, { $set: { formFields: doc.formFields } });
+
+  console.log(`${doc._id} -- UPDATED`);
+});
