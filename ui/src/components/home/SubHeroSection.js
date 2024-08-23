2c4fb54483e204989091f9b8df936e0109bd3452
Kristian Suhartono <blackxephos2@gmail.com>
2024-08-23 08:46:28 +0800

    GH-1: attempt to fix fade effect

--- ui/src/components/home/SubHeroSection.js
+++ ui/src/components/home/SubHeroSection.js
@@ -119,7 +119,7 @@ const LinkCard = (props) => {
 const SubHeroSection = () => {
   return (
     <>
-      <InView rootMargin="-200px" triggerOnce={true}>
+      <InView rootMargin="-200px" fallbackInView={true} triggerOnce={true}>
         {({ inView, ref }) => (
           <Fade transition={{ enter: { duration: 1 } }} in={inView}>
             <Container
