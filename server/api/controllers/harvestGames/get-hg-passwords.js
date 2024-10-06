076731bf2a0b60b1dba8d98c46321109ac23f378
Baeck016 <73063819+Baeck016@users.noreply.github.com>
2024-10-06 08:25:03 +0800

    GH-1120: added link to instructions

--- server/api/controllers/harvestGames/get-hg-passwords.js
+++ server/api/controllers/harvestGames/get-hg-passwords.js
@@ -45,8 +45,22 @@ module.exports = {
           'https://hongkong.sub.hmcc.net/wp-content/uploads/hg_game1_pg3.gif',
         ];
       } else if (gameId == 1) {
-        imageLink = ['', '', '', '', ''];
+        imageLink = [
+          'https://hongkong.sub.hmcc.net/wp-content/uploads/hg_game2_pg1.gif',
+          'https://hongkong.sub.hmcc.net/wp-content/uploads/hg_game2_pg2.gif',
+          'https://hongkong.sub.hmcc.net/wp-content/uploads/hg_game2_pg3.gif',
+          'https://hongkong.sub.hmcc.net/wp-content/uploads/hg_game2_pg4.gif',
+          'https://hongkong.sub.hmcc.net/wp-content/uploads/hg_game2_pg5.gif',
+        ];
       } else if (gameId == 2) {
+        imageLink = [
+          'https://hongkong.sub.hmcc.net/wp-content/uploads/hg_game3_pg1.gif',
+          'https://hongkong.sub.hmcc.net/wp-content/uploads/hg_game3_pg2.gif',
+          'https://hongkong.sub.hmcc.net/wp-content/uploads/hg_game3_pg3.gif',
+          'https://hongkong.sub.hmcc.net/wp-content/uploads/hg_game3_pg4.gif',
+          'https://hongkong.sub.hmcc.net/wp-content/uploads/hg_game3_pg5.gif',
+          'https://hongkong.sub.hmcc.net/wp-content/uploads/hg_game3_pg6.gif',
+        ];
       }
 
       return exits.success({ imageLinkData });
