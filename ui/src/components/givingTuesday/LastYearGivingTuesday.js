6c37b7229954ee04961b6cd98daf53824712f492
Kristian Suhartono <blackxephos2@gmail.com>
2024-11-30 20:31:55 +0800

    GH-1: Loaded in necessary files to support GT site.

---
+++ ui/src/components/givingTuesday/LastYearGivingTuesday.js
@@ -0,0 +1,43 @@
+import { Stack, Box, Text } from '@chakra-ui/react';
+
+const LastYearGivingTuesday = (props) => {
+  return (
+    <Stack
+      direction="column"
+      spacing={0}
+      borderWidth={1}
+      borderRadius="3xl"
+      borderColor="white"
+      background="white"
+      shadow="md"
+      textAlign="center"
+      paddingBottom={[6, 9]}
+    >
+      <Text
+        color="#F9916B"
+        fontWeight="900"
+        fontSize={['1.1rem', '1.75rem']}
+        pt={[3, 7]}
+        my="2"
+      >
+        Last Year's Giving Tuesday
+      </Text>
+      <Stack fontSize={['0.7rem', 'md']} pl={7} pr={7} spacing={[5, 3]}>
+        <Text>
+          <b>157 people</b> gave for a <b> total of HK$286,171.58</b>
+        </Text>
+        <Box>
+          <Text>
+            <b>Giving towards 3 categories:</b>
+          </Text>
+          <Text>
+            1) Our Church Budget, 2) Partnering NGOs, 3) Churches Affected by
+            Covid
+          </Text>
+        </Box>
+      </Stack>
+    </Stack>
+  );
+};
+
+export default LastYearGivingTuesday;
