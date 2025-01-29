da13c5010f8fe0930c0f1eed6585ba4a005f5c4c
Yoo Yuen Yau <yuenyau989@hotmail.com>
2025-01-29 14:35:10 +0800

    GH-1147: Aligning online page UI

--- ui/src/components/sermons/OnlineSermonContainer.js
+++ ui/src/components/sermons/OnlineSermonContainer.js
@@ -4,18 +4,14 @@ import {
   HStack,
   VStack,
   Box,
-  Button,
-  Stack,
   AspectRatio,
-  Link,
   Text,
   Container,
 } from '@chakra-ui/react';
-import { ChevronLeftIcon } from '@chakra-ui/icons';
-import { SocialIcon } from 'react-social-icons';
 import { DateTime } from 'luxon';
 import OnlinePageButtons from './OnlinePageButtons';
 import OnlinePageTabs from './OnlinePageTabs';
+import SermonSocialMediaButtons from './SermonSocialMediaButtons';
 
 const OnlineSermonContainer = (props) => {
   const { user, history } = props;
@@ -64,12 +60,13 @@ const OnlineSermonContainer = (props) => {
   return (
     <>
       {onlineSermon && (
-        <Container maxW={{ base: '100%', lg: '85%' }}>
-          <VStack mb="4">
+        <Container maxW="container.xl" mt={{ base: '0.75rem', lg: '1.75rem' }}>
+          <Box mb={{ base: '1rem', md: '2.5rem' }}>
             <Text
-              alignSelf={{ base: 'center', md: 'self-start' }}
-              fontFamily="DMSerifDisplay_Italic"
-              fontSize={{ base: '2rem', md: '3rem' }}
+              fontFamily={'DMSerifDisplay_Italic'}
+              fontWeight={400}
+              fontSize={{ base: '2rem', md: '3.375rem' }}
+              textAlign={{ base: 'center', md: 'left' }}
             >
               {onlineSermon.sermonSeries}
             </Text>
@@ -78,21 +75,26 @@ const OnlineSermonContainer = (props) => {
               flexDirection={{ base: 'column', lg: 'row' }}
               alignItems={{ base: 'center', lg: 'unset' }}
               w="100%"
+              mb={'2rem'}
             >
-              <Box mb="20px" w={{ base: '100%', lg: '60%' }}>
-                <VStack alignItems="left" alignContent="left">
-                  <AspectRatio mb="0" width="100%" ratio={16 / 9}>
-                    <iframe
-                      width="560"
-                      height="315"
-                      src={onlineSermon.streamLink}
-                      title="Video player"
-                      frameBorder="0"
-                      allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture; autoplay;"
-                      allowFullScreen
-                    ></iframe>
-                  </AspectRatio>
-
+              <Box
+                w={{ base: '100%', lg: '60%' }}
+                display={'flex'}
+                flexDir={'column'}
+                gap={'1rem'}
+              >
+                <AspectRatio mb="0" width="100%" ratio={16 / 9}>
+                  <iframe
+                    width="560"
+                    height="315"
+                    src={onlineSermon.streamLink}
+                    title="Video player"
+                    frameBorder="0"
+                    allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture; autoplay;"
+                    allowFullScreen
+                  ></iframe>
+                </AspectRatio>
+                <VStack spacing={'0.5rem'} alignItems="left" mb={'1rem'}>
                   <Text
                     fontWeight="400"
                     fontSize={{ base: '1.125rem', md: '2rem' }}
@@ -100,137 +102,56 @@ const OnlineSermonContainer = (props) => {
                   >
                     {onlineSermon.title}
                   </Text>
-                  <Stack spacing={{ base: '2', md: '8' }}>
-                    <Box>
-                      <Stack
-                        spacing={{ base: '1', md: '3' }}
-                        direction="row"
-                        mb="3"
-                        fontSize={{ base: '0.625rem', md: '0.75rem' }}
-                      >
-                        <HStack spacing="1">
-                          <Text>Date:</Text>
-                          <Text>{getSermonDate()}</Text>
-                        </HStack>
-                        <Text>|</Text>
-                        <HStack spacing="1">
-                          <Text>Speaker: </Text>
-                          <Text>{onlineSermon.speaker}</Text>
-                        </HStack>
-                        <Text>|</Text>
-                        <HStack spacing="1">
-                          <Text>Passage: </Text>
-                          <Text>{onlineSermon.sermonPassage}</Text>
-                        </HStack>
-                      </Stack>
-                      <Text
-                        fontFamily="Manrope"
-                        fontSize={{ base: '0.75rem', md: '0.875rem' }}
-                        fontWeight="500"
-                        color="black"
-                      >
-                        {onlineSermon.sermonDescription}
-                      </Text>
-                    </Box>
-                    <HStack
-                      textColor="#4A6EEB"
-                      fontFamily="Manrope"
-                      fontSize="1rem"
-                      gap={{ base: '0', md: '10' }}
-                      fontWeight="700"
-                      justifyContent={{ base: 'center', md: 'flex-start' }}
-                    >
-                      <Link
-                        target="_blank"
-                        href="https://open.spotify.com/user/hmccofhk?si=bd64100596904a95"
-                      >
-                        <HStack>
-                          <Box
-                            height={{ base: '4.5em', md: '2.5em' }}
-                            width={{ base: '4.5em', md: '2.5em' }}
-                          >
-                            <SocialIcon
-                              target="_blank"
-                              bgColor="transparent"
-                              fgColor="#4A6EEB"
-                              style={{ height: '100%', width: '100%' }}
-                              url="https://open.spotify.com/user/hmccofhk?si=bd64100596904a95"
-                            />
-                          </Box>
-                          <Text
-                            fontWeight="700"
-                            borderBottomWidth="2px"
-                            borderColor=" #4A6EEB"
-                            display={{ base: 'none', md: 'block' }}
-                          >
-                            Listen on Spotify
-                          </Text>
-                        </HStack>
-                      </Link>
+                  <HStack
+                    spacing={0}
+                    fontSize={{ base: '0.625rem', md: '0.875rem' }}
+                    alignItems="center"
+                    justifyContent="flex-start"
+                    flexWrap={'wrap'}
+                  >
+                    <Text>{'Date: ' + getSermonDate()}</Text>
+                    <Text px={{ base: '0.375rem', md: '1rem' }}>|</Text>
+                    <Text>{'Speaker: ' + onlineSermon.speaker}</Text>
+                    <Text px={{ base: '0.375rem', md: '1rem' }}>|</Text>
+                    <Text>{'Passage: ' + onlineSermon.sermonPassage}</Text>
+                  </HStack>
+                  <Text
+                    py={{ base: '0', lg: '0.75rem' }}
+                    fontWeight={600}
+                    fontSize={{ base: '0.875rem', md: '1rem' }}
+                  >
+                    {onlineSermon.sermonDescription}
+                  </Text>
 
-                      <Link
-                        target="_blank"
-                        href="https://www.youtube.com/channel/UC1O1T7RaKWTGHd7R_0KMZ8Q"
-                      >
-                        <HStack>
-                          <Box
-                            height={{ base: '5em', md: '2.5em' }}
-                            width={{ base: '5em', md: '2.5em' }}
-                          >
-                            <SocialIcon
-                              target="_blank"
-                              bgColor="transparent"
-                              fgColor="#4A6EEB"
-                              style={{ height: '100%', width: '100%' }}
-                              url="https://www.youtube.com/channel/UC1O1T7RaKWTGHd7R_0KMZ8Q"
-                            />
-                          </Box>
-                          <Text
-                            fontWeight="700"
-                            borderBottomWidth="2px"
-                            borderColor="#4A6EEB"
-                            display={{ base: 'none', md: 'block' }}
-                          >
-                            Watch on Youtube
-                          </Text>
-                        </HStack>
-                      </Link>
-                    </HStack>
-                  </Stack>
+                  <SermonSocialMediaButtons
+                    ytLink={
+                      'https://www.youtube.com/channel/UC1O1T7RaKWTGHd7R_0KMZ8Q'
+                    }
+                  />
                 </VStack>
               </Box>
               <Box
                 ml={{ base: '0px', lg: '20px' }}
-                mb="20px"
                 w={{ base: '100%', lg: '40%' }}
                 overflowY="auto"
                 position="relative"
                 borderRadius={10}
-                boxShadow={{
-                  base: 'none',
-                  sm: '0px 4px 18px rgba(0, 0, 0, 0.25)',
-                }}
               >
-                <Box
-                  position={{ base: 'unset', lg: 'absolute ' }}
-                  top={{ base: 'unset', lg: 0 }}
-                  left={{ base: 'unset', lg: 0 }}
-                  w="100%"
-                  h="100%"
-                >
+                <Box w="100%" h="100%">
                   <OnlinePageTabs
                     sermonNoteId={onlineSermon.sermonNotes}
                     user={user}
                     history={history}
+                    sermonSeries={onlineSermon.sermonSeries}
                   />
                 </Box>
               </Box>
             </Box>
-          </VStack>
+          </Box>
           <Text
-            fontFamily="DMSerifDisplay_Italic"
-            fontSize="2rem"
-            display={{ base: 'none', sm: 'block' }}
+            fontFamily={'DMSerifDisplay_Italic'}
+            fontWeight={400}
+            fontSize={{ base: '1.75rem', md: '2rem' }}
           >
             Other Links
           </Text>
