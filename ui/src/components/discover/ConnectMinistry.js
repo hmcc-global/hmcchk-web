310812f3aa6e4cc6c284dafd9ff79cb229ad5c10
kuritsuan <133067409+kuritsuan@users.noreply.github.com>
2025-03-02 17:36:13 +0800

    GH-1161: added pictures and edit text size

--- ui/src/components/discover/ConnectMinistry.js
+++ ui/src/components/discover/ConnectMinistry.js
@@ -1,9 +1,4 @@
-import {
-  Box,
-  Container,
-  VStack,
-  Text
-} from '@chakra-ui/react';
+import { Box, Container, VStack, Text } from '@chakra-ui/react';
 import MinistryCard from './MinistryCard';
 import FaqConnect from './FaqConnect';
 import BackButton from './BackButton';
@@ -12,32 +7,35 @@ const ConnectMinistry = (props) => {
   return (
     <Box>
       <BackButton />
-      <Container maxW="container.lg" py={10}  pt="60px">
-        <VStack spacing={[4, 7]} align={["center","stretch"]}>
-          <Text 
+      <Container maxW="container.lg" py={10} pt="60px">
+        <VStack spacing={[4, 7]} align={['center', 'stretch']}>
+          <Text
             color="#0C0C20"
             fontFamily="DMSerifDisplay_Italic"
-            fontSize={['2rem', '3rem', '3.75rem']} // Responsive font size
+            fontSize={['2.25rem', '3rem', '3.75rem']} // Responsive font size
             fontWeight="400"
             lineHeight="97%"
             letterSpacing="-2px"
-            textAlign={["center", "inherit"]}
+            textAlign={['center', 'inherit']}
           >
-          Connect with Our Ministries
+            Connect with Our Ministries
           </Text>
-          <Text 
+          <Text
             color="#0C0C20"
             fontFamily="Manrope"
-            fontSize={['0.85rem', '0.9rem', '1rem']}
+            fontSize={['0.875rem', '0.9rem', '1.125rem']}
             fontStyle="normal"
             fontWeight="400"
             lineHeight="normal"
             // maxW="800px"
             mx="auto"
-            textAlign={["center", "inherit"]}
+            textAlign={['center', 'inherit']}
           >
-            At Harvest Mission Community Church, we are actively reaching out to people from all walks of life, specifically youths, college students, working adults, and families.            </Text>
-          
+            At Harvest Mission Community Church, we are actively reaching out to
+            people from all walks of life, specifically youths, college
+            students, working adults, and families.{' '}
+          </Text>
+
           <MinistryCard />
           <FaqConnect />
         </VStack>
--- ui/src/components/discover/MinistryCard.js
+++ ui/src/components/discover/MinistryCard.js
@@ -1,44 +1,46 @@
-import { 
-  Box, 
-  Button, 
-  Flex, 
-  Heading, 
-  Link, 
+import {
+  Box,
+  Button,
+  Flex,
+  Heading,
+  Link,
   Text,
-  Image
-
+  Image,
 } from '@chakra-ui/react';
-
 
 const cards = [
   {
-    title: "University Students",
-    description: "Reaching the college students in the university campuses of Hong Kong because we believe that college is a pivotal time for people to discover their God-given destiny.",
-    image: `${process.env.PUBLIC_URL}/images/connect/undergrad.jpg`, 
+    title: 'University Students',
+    description:
+      'Reaching the college students in the university campuses of Hong Kong because we believe that college is a pivotal time for people to discover their God-given destiny.',
+    image: `${process.env.PUBLIC_URL}/images/connect/university_students.jpg`,
     showButton: false, // Control whether the button is shown
   },
   {
-    title: "Single Working Adults",
-    description: "Creating an avenue for single working adults to fellowship and learn how to best exercise their God-given influence in their respective schools and workplaces.",
-    image: `${process.env.PUBLIC_URL}/images/connect/undergrad.jpg`,
+    title: 'Single Working Adults',
+    description:
+      'Creating an avenue for single working adults to fellowship and learn how to best exercise their God-given influence in their respective schools and workplaces.',
+    image: `${process.env.PUBLIC_URL}/images/connect/single_working_adults.jpg`,
     showButton: false,
   },
   {
-    title: "Married Couples and Families",
-    description: "Bringing together Godly families that will love on and impact the next generation fo their families and city.",
-    image: `${process.env.PUBLIC_URL}/images/connect/undergrad.jpg`,
+    title: 'Married Couples and Families',
+    description:
+      'Bringing together Godly families that will love on and impact the next generation fo their families and city.',
+    image: `${process.env.PUBLIC_URL}/images/connect/married_couples_and_families.jpg`,
     showButton: false,
   },
   {
-    title: "Youth",
-    description: "Reaching and training the youth of this generation to reach the world by starting from the centre, which is our relationship with Jesus Christ",
-    image: `${process.env.PUBLIC_URL}/images/connect/undergrad.jpg`,
+    title: 'Youth',
+    description:
+      'Reaching and training the youth of this generation to reach the world by starting from the centre, which is our relationship with Jesus Christ',
+    image: `${process.env.PUBLIC_URL}/images/connect/youth.jpg`,
     showButton: false,
   },
   {
-    title: "Children",
-    description: "Building Blocks is HMCC’s Children Ministry.",
-    image: `${process.env.PUBLIC_URL}/images/connect/undergrad.jpg`,
+    title: 'Children',
+    description: 'Building Blocks is HMCC’s Children Ministry.',
+    image: `${process.env.PUBLIC_URL}/images/connect/children.jpg`,
     showButton: true,
   },
 ];
@@ -56,9 +58,9 @@ const MinistryCard = (props) => {
       {cards.map((card, index) => (
         <Box
           key={index}
-          flexBasis={["100%", "calc(33.333% - 16px)"]} // 1 card per row on mobile, 3 cards per row on desktop
-          maxW={["100%", "calc(33.333% - 16px)"]} // Ensure cards don't exceed the width
-          border={"1px solid #8C8C8C"}
+          flexBasis={['100%', 'calc(33.333% - 16px)']} // 1 card per row on mobile, 3 cards per row on desktop
+          maxW={['100%', 'calc(33.333% - 16px)']} // Ensure cards don't exceed the width
+          border={'1px solid #8C8C8C'}
           borderRadius="16px"
           textAlign="center"
           overflow="hidden"
@@ -72,39 +74,40 @@ const MinistryCard = (props) => {
           />
 
           <Box p={4} pb={card.showButton ? 0 : 10}>
-            <Text 
+            <Text
               mb={2}
               textAlign="left"
-              fontFamily={"DMSerifDisplay_Italic"}
-              fontSize={['1.4rem', '1.4rem', '1.5rem']}
+              fontFamily={'DMSerifDisplay_Italic'}
+              fontSize={'2rem'}
             >
               {card.title}
             </Text>
             <Text
               textAlign="left"
-              fontFamily={"Manrope"}
-              fontSize={['0.85rem', '0.9rem', '0.95rem']}
-            >{card.description}</Text>
-
+              fontFamily={'Manrope'}
+              fontSize={['0.875rem', '0.9rem', '1.125rem']}
+            >
+              {card.description}
+            </Text>
           </Box>
 
           {card.showButton && ( // Only render the button if `showButton` is true
             <Box p={4}>
-              <Button 
-               bg={"#21A0A7"}
-               borderRadius={"15px"}
-               width="100%"
-               textColor={"white"}
-               fontFamily={"Manrope"}
-               fontWeight={"bold"}
-               letterSpacing={["2px","1px","2px"]}
-               px={"30px"}
-               py={"22px"}
-               fontSize={['0.8rem', '0.6rem', '0.8rem']}
-
-               >
-                
-               LEARN MORE →
+              <Button
+                as="a"
+                href="https://hongkong.sub.hmcc.net/building-blocks/"
+                bg={'#21A0A7'}
+                borderRadius={'15px'}
+                width="100%"
+                textColor={'white'}
+                fontFamily={'Manrope'}
+                fontWeight={'bold'}
+                letterSpacing={['2px', '1px', '2px']}
+                px={'30px'}
+                py={'22px'}
+                fontSize={['0.8rem', '0.6rem', '0.8rem']}
+              >
+                LEARN MORE →
               </Button>
             </Box>
           )}
