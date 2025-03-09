2f58b45d988a7a3d69815a02a9681866a03339e1
Yoo Yuen Yau <yuenyauu@gmail.com>
2025-03-09 17:40:34 +0800

    GH-1170: Updates

---
+++ ui/src/components/discover/LifeGroupSignupButton.js
@@ -0,0 +1,24 @@
+import { Button, Link, useMediaQuery } from '@chakra-ui/react';
+
+const LifeGroupSignupButton = () => {
+  return (
+    <Button
+      as={Link}
+      href="https://bit.ly/LGSignup2024"
+      borderRadius={'0.9375rem'}
+      bgColor="#D46764"
+      py={'1.75rem'}
+      px={'1.75rem'}
+      fontSize={{ base: '0.875rem', lg: '1rem' }}
+      fontWeight="700"
+      _hover={{ boxShadow: 'none' }}
+      letterSpacing={'0.25rem'}
+      color={'#F6FAFF'}
+      fontFamily={'Manrope'}
+    >
+      SIGN UP FOR LIFE GROUP
+    </Button>
+  );
+};
+
+export default LifeGroupSignupButton;
