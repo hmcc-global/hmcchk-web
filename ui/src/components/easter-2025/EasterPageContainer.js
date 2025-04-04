a12facb9c297ba6fa3373ae802bf7a34f18381bd
Albert Paredandan <albertparedandan@gmail.com>
2025-04-04 21:08:01 +0800

    GH-1184: Add Instagram iframe

--- ui/src/components/easter-2025/EasterPageContainer.js
+++ ui/src/components/easter-2025/EasterPageContainer.js
@@ -1,25 +1,12 @@
-import { Box, useBreakpointValue } from "@chakra-ui/react";
+import { Flex } from "@chakra-ui/react";
+import InstagramSection from "./InstagramSection";
 
 const EasterPageContainer = (props) => {
 
-  const imageSrc = useBreakpointValue({
-    base: process.env.PUBLIC_URL + 'images/easter-2025/easter-mobile.png',
-    md: process.env.PUBLIC_URL + 'images/easter-2025/easter-desktop.png',
-  });
-
   return (
-    <>
-      <Box
-        width="100%"
-        height={0}
-        backgroundImage={imageSrc}
-        backgroundPosition="center"
-        backgroundRepeat="no-repeat"
-        backgroundSize="cover"
-        paddingTop={{base: "350%", md: "120%"}}
-      >
-      </Box>
-    </>
+    <Flex direction="column">
+      <InstagramSection />
+    </Flex>
   );
 };
 
