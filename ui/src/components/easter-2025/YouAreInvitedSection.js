500f1c287b625778cd7899a651777ec86f2be651
JoshNg123 <jnjw,999@gmail.com>
2025-04-11 02:23:36 +0800

    GH-1180: Resolve conflicts

--- ui/src/components/easter-2025/YouAreInvitedSection.js
+++ ui/src/components/easter-2025/YouAreInvitedSection.js
@@ -56,162 +56,168 @@ const YouAreInvitedSection = () => {
 
   return (
     <>
-      <Container
-        maxW="100%"
-        border="1px solid black"
-        borderRadius={{
-          base: '0 30px 30px 0',
-          md: '30px',
-        }}
-        marginBottom={12}
-        padding={0}
-        position="relative"
-        left={['-15px', '-2.5%']}
-        paddingLeft={['6px', '0']} // Adjust padding for left on mobile
-      >
-        <Container maxW={['100%', '90%']} marginTop={12} mb={12}>
-          <Box width={['100%', '50%', '50%']} mb={[6, 4, 8]}>
-            <Text
-              fontFamily="'Lexend Peta', sans-serif"
-              fontWeight={900}
-              bgGradient="linear(87deg, #45173C 0%, #732527 35%, #20444A 75%, #0FACD0 100%)"
-              backgroundClip="text"
-              fontSize={{ base: '1.6rem', md: '2.5rem', lg: '3.5rem' }}
-              letterSpacing={['-4px', '-9px']}
-            >
-              YOU ARE INVITED
-            </Text>
-            <Text
-              fontFamily="'Lexend Peta', sans-serif"
-              fontWeight={900}
-              background="linear-gradient(87deg, #45173C 0%, #732527 35%, #20444A 75%, #0FACD0 100%)"
-              backgroundClip="text"
-              fontSize={{ base: '1.6rem', md: '2.5rem', lg: '3.5rem' }}
-              letterSpacing={['-4px', '-9px']}
-            >
-              TO JOIN US AT ...
-            </Text>
-          </Box>
-          <Box>
-            <Flex direction={{ base: 'column', md: 'row' }}>
-              {eventData.map((event, index) => (
-                <Box
-                  key={index}
-                  borderRadius={20}
-                  padding={[0, 6]}
-                  width={{ base: '100%', md: '33%' }}
-                  mb={[6, 0]}
-                >
-                  <Box mb={4}>
-                    <Image
-                      src={event.imageSrc}
-                      alt={event.title}
-                      objectFit="cover"
-                      maxHeight={50}
-                    />
-                  </Box>
-                  <Text
-                    fontWeight={700}
-                    fontSize={['md', 'xl']}
-                    mb={2}
-                    fontFamily="'Lexend Peta', sans-serif"
-                    letterSpacing="-2px"
+      <Box bgGradient="linear(to-l, #FFDBC9, #FFFDD7, #0FACD0)">
+        <Container
+          maxW="100%"
+          border="1px solid black"
+          borderRadius={{
+            base: '0 30px 30px 0',
+            md: '30px',
+          }}
+          marginBottom={12}
+          padding={0}
+          position="relative"
+          left={['-15px', '-2.5%']}
+          paddingLeft={['8px', '0']}
+        >
+          <Container maxW={['100%', '90%']} marginTop={12} mb={12}>
+            <Box width={['100%', '50%', '50%']} mb={[6, 4, 8]}>
+              <Text
+                fontFamily="'LexendPeta', sans-serif"
+                fontWeight={900}
+                bgGradient="linear(87deg, #45173C 0%, #732527 35%, #20444A 75%, #0FACD0 100%)"
+                backgroundClip="text"
+                fontSize={{ base: '1.6rem', md: '2.5rem', lg: '3.5rem' }}
+                letterSpacing={['-4px', '-9px']}
+              >
+                YOU ARE INVITED
+              </Text>
+              <Text
+                fontFamily="'LexendPeta', sans-serif"
+                fontWeight={900}
+                background="linear-gradient(87deg, #45173C 0%, #732527 35%, #20444A 75%, #0FACD0 100%)"
+                backgroundClip="text"
+                fontSize={{ base: '1.6rem', md: '2.5rem', lg: '3.5rem' }}
+                letterSpacing={['-4px', '-9px']}
+              >
+                TO JOIN US AT ...
+              </Text>
+            </Box>
+            <Box>
+              <Flex direction={{ base: 'column', md: 'row' }}>
+                {eventData.map((event, index) => (
+                  <Box
+                    key={index}
+                    borderRadius={20}
+                    padding={[0, 6]}
+                    width={{ base: '100%', md: '33%' }}
+                    mb={[6, 0]}
                   >
-                    {event.title}
-                  </Text>
-                  <Text
-                    fontSize={['sm', 'md']}
-                    mb={1}
-                    fontFamily="'Lexend Peta', sans-serif"
-                    fontWeight={400}
-                    letterSpacing="-2px"
-                  >
-                    {event.eventDate}
-                  </Text>
-                  <Text
-                    fontSize={['sm', 'md']}
-                    fontWeight={400}
-                    mb={[1, 6]}
-                    fontFamily="'Lexend Peta', sans-serif"
-                    letterSpacing="-2px"
-                  >
-                    {event.time}
-                  </Text>
-                  {(() => {
-                    const [firstPart, ...restParts] = event.location.split(',');
-                    return (
-                      <>
-                        <Text
-                          fontSize={['sm', 'md']}
-                          mb={1}
-                          fontFamily="'Lexend', sans-serif"
-                        >
-                          {firstPart.trim()}
-                        </Text>
-                        <Text
-                          fontSize={['xs', 'sm']}
-                          mb={[1, 3]}
-                          fontFamily="'Lexend', sans-serif"
-                          color="#006E87"
-                        >
-                          {restParts.join(',').trim()}
-                        </Text>
-                      </>
-                    );
-                  })()}
-                  <Box>
-                    <Flex
-                      direction={['row', 'column']}
-                      gap={[1, 2]}
-                      alignItems="left"
+                    <Box mb={4}>
+                      <Image
+                        src={event.imageSrc}
+                        alt={event.title}
+                        objectFit="cover"
+                        maxHeight={50}
+                      />
+                    </Box>
+                    <Text
+                      fontWeight={700}
+                      fontSize={['md', 'xl']}
+                      mb={2}
+                      fontFamily="'LexendPeta', sans-serif"
+                      letterSpacing="-2px"
                     >
-                      <Box width={['50%', 'auto']}>
-                        {generateGoogleCalendarLink(event) && (
+                      {event.title}
+                    </Text>
+                    <Text
+                      fontSize={['sm', 'md']}
+                      mb={1}
+                      fontFamily="'LexendPeta', sans-serif"
+                      fontWeight={400}
+                      letterSpacing="-2px"
+                    >
+                      {event.eventDate}
+                    </Text>
+                    <Text
+                      fontSize={['sm', 'md']}
+                      fontWeight={400}
+                      mb={[1, 6]}
+                      fontFamily="'LexendPeta', sans-serif"
+                      letterSpacing="-2px"
+                    >
+                      {event.time}
+                    </Text>
+                    {(() => {
+                      const [firstPart, ...restParts] =
+                        event.location.split(',');
+                      return (
+                        <>
+                          <Text
+                            fontSize={['sm', 'md']}
+                            mb={1}
+                            fontFamily="'Lexend', sans-serif"
+                          >
+                            {firstPart.trim()}
+                          </Text>
+                          <Text
+                            fontSize={['xs', 'sm']}
+                            mb={[1, 3]}
+                            fontFamily="'Lexend', sans-serif"
+                            color="#006E87"
+                          >
+                            {restParts.join(',').trim()}
+                          </Text>
+                        </>
+                      );
+                    })()}
+                    <Box>
+                      <Flex
+                        direction={['row', 'column']}
+                        gap={[1, 2]}
+                        alignItems="left"
+                      >
+                        <Box width={['50%', 'auto']}>
+                          {generateGoogleCalendarLink(event) && (
+                            <Button
+                              fontFamily="'LexendPeta', sans-serif"
+                              as={Link}
+                              size={['60%', 'sm']}
+                              fontSize={['60%', 'sm']}
+                              variant="outline"
+                              target="_blank"
+                              px={[2, 4]}
+                              py={[1, 2]}
+                              _hover={{
+                                opacity: '90%',
+                                textDecoration: 'none',
+                              }}
+                              href={generateGoogleCalendarLink(event)}
+                              rightIcon={<CiCalendar />}
+                              borderRadius="full"
+                              width="auto"
+                            >
+                              ADD TO CALENDAR
+                            </Button>
+                          )}
+                        </Box>
+                        <Box width={['50%', 'auto']}>
                           <Button
-                            fontFamily="'Lexend Peta', sans-serif"
-                            as={Link}
+                            px={[2, 4]}
+                            py={[1, 2]}
+                            fontFamily="'LexendPeta', sans-serif"
                             size={['60%', 'sm']}
                             fontSize={['60%', 'sm']}
                             variant="outline"
-                            target="_blank"
-                            px={[2, 4]}
-                            py={[1, 2]}
-                            _hover={{ opacity: '90%', textDecoration: 'none' }}
-                            href={generateGoogleCalendarLink(event)}
-                            rightIcon={<CiCalendar />}
                             borderRadius="full"
                             width="auto"
+                            rightIcon={<MdOutlineDirections />}
+                            as={Link}
+                            href={generateGoogleMapsLink(event.location)}
+                            target="_blank"
                           >
-                            ADD TO CALENDAR
+                            DIRECTIONS
                           </Button>
-                        )}
-                      </Box>
-                      <Box width={['50%', 'auto']}>
-                        <Button
-                          px={[2, 4]}
-                          py={[1, 2]}
-                          fontFamily="'Lexend Peta', sans-serif"
-                          size={['60%', 'sm']}
-                          fontSize={['60%', 'sm']}
-                          variant="outline"
-                          borderRadius="full"
-                          width="auto"
-                          rightIcon={<MdOutlineDirections />}
-                          as={Link}
-                          href={generateGoogleMapsLink(event.location)}
-                          target="_blank"
-                        >
-                          DIRECTIONS
-                        </Button>
-                      </Box>
-                    </Flex>
+                        </Box>
+                      </Flex>
+                    </Box>
                   </Box>
-                </Box>
-              ))}
-            </Flex>
-          </Box>
+                ))}
+              </Flex>
+            </Box>
+          </Container>
         </Container>
-      </Container>
+      </Box>
     </>
   );
 };
