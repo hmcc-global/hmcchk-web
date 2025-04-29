554371f90d8fd42e48e26c1a368f74c9adf6e484
randall <randall2000n@gmail.com>
2025-04-29 16:01:58 +0800

    GH-1179: add hero

--- ui/src/components/easter-2025/EasterPageContainer.js
+++ ui/src/components/easter-2025/EasterPageContainer.js
@@ -1,9 +1,10 @@
 import { Flex } from '@chakra-ui/react';
 import InstagramSection from './InstagramSection';
-
+import EasterHero from './EasterHero';
 const EasterPageContainer = (props) => {
   return (
     <Flex direction="column">
+      <EasterHero />
       <InstagramSection />
     </Flex>
   );
