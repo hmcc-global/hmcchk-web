8b55f03377765413e72390334ab44673cccb8489
cornyachtsorry <andrea.justkidding@gmail.com>
2025-04-29 16:01:58 +0800

    GH-1180: Edit for iPad dimension

--- ui/src/components/easter-2025/RedeemedAndChosenSection.js
+++ ui/src/components/easter-2025/RedeemedAndChosenSection.js
@@ -8,13 +8,13 @@ const RedeemedAndChosenSection = () => {
         <Image
           src={process.env.PUBLIC_URL + 'images/easter-2025/redeemed-chosen-mountain.png'}
             alt="Redeemed and Chosen"
-            width={{ base: '26.875rem', md: '60rem' }}
+            width={{ base: '26.875rem', md: '30rem', xl: '60rem' }}
             height="auto"
             objectFit="cover"
             marginBottom={-12}
             marginTop={{ base: '3', md: '10' }}
             marginRight={{ base: '7rem', md: '0' }}
-            marginLeft={{ base: '0rem', md: '-12rem' }}
+            marginLeft={{ base: '0rem', md: '-10rem', xl: '-12rem' }}
             zIndex={1}
             order={{ base: 2, md: 1 }}
         />
@@ -24,10 +24,10 @@ const RedeemedAndChosenSection = () => {
           <Text
             fontFamily="LexendPeta"
             fontWeight={900}
-            fontSize={{ base: '1.125rem', md: '2rem' }}
+            fontSize={{ base: '1.125rem', md: '1.5rem', xl: '2rem' }}
             letterSpacing={{ base: '-0.188rem', md: '-0.313rem' }}
             align={{ base: 'center', md: 'left' }}
-            marginBottom={{ base: '-2.5', md: '-5' }}
+            marginBottom={{ base: '-2.5', md: '-4', xl:'-5' }}
             marginTop={{ base: '4', md: '0' }}
           >
             BECAUSE OF WHAT JESUS DID
@@ -35,7 +35,7 @@ const RedeemedAndChosenSection = () => {
           <Text
             fontFamily="LexendPeta"
             fontWeight={900}
-            fontSize={{ base: '1.125rem', md: '2rem' }}
+            fontSize={{ base: '1.125rem', md: '1.5rem', xl: '2rem' }}
             marginBottom={{ base: '2', md: '3' }}
             letterSpacing={{ base: '-0.188rem', md: '-0.313rem' }}
             align={{ base: 'center', md: 'left' }}
@@ -45,7 +45,7 @@ const RedeemedAndChosenSection = () => {
           <Text
             fontFamily="LexendPeta"
             fontWeight={900}
-            fontSize={{ base: '1.125rem', md: '2rem' }}
+            fontSize={{ base: '1.125rem', md: '1.5rem', xl: '2rem' }}
             letterSpacing={{ base: '-0.188rem', md: '-0.313rem' }}
             align={{ base: 'center', md: 'left' }}
             marginBottom={{ base: '-2.5', md: '-5' }}
@@ -57,7 +57,7 @@ const RedeemedAndChosenSection = () => {
             fontWeight={900}
             bgGradient="radial-gradient(233.98% 351.14% at 31.64% -118.61%, #20444A 23.68%, #0FACD0 54.26%, #FBF574 100%)"
             backgroundClip="text"
-            fontSize={{ base: '2.875rem', md: '4.875rem' }}
+            fontSize={{ base: '2.875rem', md: '3.875rem', xl: '4.875rem' }}
             letterSpacing={{ base: '-0.5rem', md: '-0.875rem' }}
             align={{ base: 'center', md: 'left' }}
             marginBottom={{ base: '-4', md: '-8' }}
@@ -69,7 +69,7 @@ const RedeemedAndChosenSection = () => {
             fontWeight={900}
             bgGradient="linear-gradient(87deg, #20444A 16.04%, #0FACD0 57.53%, #6D852F 94.98%)"
             backgroundClip="text"
-            fontSize={{  base: '2.875rem', md: '4.875rem'}}
+            fontSize={{ base: '2.875rem', md: '3.875rem', xl: '4.875rem' }}
             letterSpacing={{ base: '-0.5rem', md: '-0.875rem' }}
             align={{ base: 'center', md: 'left' }}
             marginBottom={{ base: '-2.5', md: '-5' }}
@@ -79,7 +79,7 @@ const RedeemedAndChosenSection = () => {
           <Text
             fontFamily="LexendPeta"
             fontWeight={900}
-            fontSize={{ base: '1.125rem', md: '2rem' }}
+            fontSize={{ base: '1.125rem', md: '1.5rem', xl: '2rem' }}
             letterSpacing={{ base: '-0.188rem', md: '-0.313rem' }}
             align={{ base: 'center', md: 'right' }}
           >
