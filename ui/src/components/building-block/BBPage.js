091485ee95928401bdf8a1cc1e6281b39a964b29
Baeck016 <73063819+Baeck016@users.noreply.github.com>
2025-05-19 14:39:59 +0800

    setup container and link to bb-page

---
+++ ui/src/components/building-block/BBPage.js
@@ -0,0 +1,11 @@
+import { Container, VStack } from '@chakra-ui/react';
+
+const BBPage = () => {
+  return (
+    <Container w="100%">
+      <VStack w="100%">{/* Can put your components here */}</VStack>
+    </Container>
+  );
+};
+
+export default BBPage;
