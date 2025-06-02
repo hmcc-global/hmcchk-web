c0bab972045b84c398345bbc676b1b7a8fa566cf
Baeck016 <73063819+Baeck016@users.noreply.github.com>
2025-06-02 17:49:11 +0800

    GH-1208: MVP & mission added for bb page

--- ui/src/components/building-block/BBPage.js
+++ ui/src/components/building-block/BBPage.js
@@ -4,16 +4,18 @@ import Curriculum from './Curriculum';
 import BBSchedule from './BBSchedule';
 import BBFaq from './BBFaq';
 import ContactUs from './ContactUs';
+import BBHeroSection from './BBHeroSection';
 
 const BBPage = () => {
   return (
     <Container minW="100%" py={{ base: '4rem', lg: '5rem' }}>
       <VStack w="100%" gap={{ base: '3rem', lg: '5rem' }}>
+        <BBHeroSection />
         <OurValues />
         <Curriculum />
         <BBSchedule />
-        <BBFaq/>
-        <ContactUs/>
+        <BBFaq />
+        <ContactUs />
       </VStack>
     </Container>
   );
