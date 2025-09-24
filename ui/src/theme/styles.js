f515e163fb26167fae1a562e96afd7c731160389
kuritsuan <133067409+kuritsuan@users.noreply.github.com>
2025-09-24 22:12:43 +0800

    1262: base implementation

---
+++ ui/src/theme/styles.js
@@ -0,0 +1,25 @@
+// theme/styles.js
+import { mode } from "@chakra-ui/theme-tools";
+import '@fontsource/dm-sans';
+
+const styles = {
+  global: (props) => ({
+    body: {
+      fontFamily: "body",
+      color: mode("gray.800", "whiteAlpha.900")(props),
+      bg: mode("white", "gray.900")(props),
+      lineHeight: "base",
+    },
+    "*::placeholder": {
+      color: mode("gray.400", "whiteAlpha.400")(props),
+    },
+    "*, *::before, &::after": {
+      borderColor: mode("gray.200", "whiteAlpha.300")(props),
+      wordWrap: "break-word",
+    },
+  }),
+};
+
+export default styles;
+
+
