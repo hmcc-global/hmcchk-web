43fa84c3d9ca4417681af3af687cc3bc01915d24
Yuen Yau Yoo <yuenyauu@gmail.com>
2025-09-25 23:08:55 +0800

    GH-1263: Adding HG-related basic components and routes (#1264)

---
+++ ui/src/components/admin/harvest-games/AdminHarvestGamesContainer.js
@@ -0,0 +1,7 @@
+import { Container } from '@chakra-ui/react';
+
+const AdminHarvestGamesContainer = () => {
+  return <Container>Admin HG 2025</Container>;
+};
+
+export default AdminHarvestGamesContainer;
---
+++ ui/src/components/harvest-games/HarvestGamesContainer.js
@@ -0,0 +1,7 @@
+import { Container } from '@chakra-ui/react';
+
+const HarvestGamesContainer = () => {
+  return <Container>HG 2025</Container>;
+};
+
+export default HarvestGamesContainer;
