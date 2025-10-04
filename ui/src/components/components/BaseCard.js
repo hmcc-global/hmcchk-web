7e7d4a4fe6e25cec8b21d46272ef56fec58a338c
unknown <ricia1212@gmail.com>
2025-10-05 00:38:25 +0800

    GH-1274: set the struct for the base and event card

---
+++ ui/src/components/components/BaseCard.js
@@ -0,0 +1,16 @@
+
+
+
+import React from "react"
+/**
+
+     * Renders a card with a customizable card header and card body. The header can be any valid React node.
+     * The body can be stack of text links or any other react nodes. 
+     * @param {React.ReactNode} title - The title to display in the card.
+     * @returns {React.ReactNode} The total price.
+*/
+const BaseCard = ({title}) => {
+  return<></>
+}
+
+export default BaseCard
