ccfae97f28ea4443fed7f1910d7be6bcb2cbaf3a
unknown <ricia1212@gmail.com>
2025-10-05 00:39:43 +0800

    GH-1274: set the struct for the base and event card

--- ui/src/components/components/BaseCard.js
+++ ui/src/components/components/BaseCard.js
@@ -1,7 +1,5 @@
-
-
-
-import React from "react"
+import React from 'react';
+import { Card } from '@chakra-ui/react';
 /**
 
      * Renders a card with a customizable card header and card body. The header can be any valid React node.
@@ -9,8 +7,9 @@ import React from "react"
      * @param {React.ReactNode} title - The title to display in the card.
      * @returns {React.ReactNode} The total price.
 */
-const BaseCard = ({title}) => {
-  return<></>
-}
 
-export default BaseCard
+const BaseCard = ({ title }) => {
+  return <Card></Card>;
+};
+
+export default BaseCard;
