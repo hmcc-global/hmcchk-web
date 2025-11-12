9155a9f70cda2f6a62ee5ea6b99a74ab642829e8
cornyachtsorry <andrea.justkidding@gmail.com>
2025-11-12 12:20:50 +0800

    GH-1244: Add mobile version

--- ui/src/components/10-year/TenYearContainer.js
+++ ui/src/components/10-year/TenYearContainer.js
@@ -3,8 +3,19 @@ import TenYearVideo from './TenYearVideo';
 import { tenYearTheme } from './theme';
 
 const TenYearContainer = () => {
+  const backgroundImage = `${process.env.PUBLIC_URL}/images/10-year/10y_bg.png`;
+  
   return (
-    <Flex direction="column" bgColor={tenYearTheme.colors.background} py={10} align="center">
+    <Flex 
+      direction="column" 
+      bgColor={tenYearTheme.colors.background}
+      backgroundImage={`url(${backgroundImage})`}
+      backgroundSize="cover"
+      backgroundPosition="center"
+      backgroundRepeat="no-repeat"
+      py={10} 
+      align="center"
+    >
       <Box w="100%" px={[4, 8]}>
         <TenYearVideo />
       </Box>
