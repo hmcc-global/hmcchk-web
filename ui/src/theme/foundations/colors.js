e15f21f567bb79c3c9c920c6b1eebd4a5baa2f6e
kuritsuan <133067409+kuritsuan@users.noreply.github.com>
2025-11-13 15:32:10 +0800

    1262: standardized color

---
+++ ui/src/theme/foundations/colors.js
@@ -0,0 +1,26 @@
+const colors = {
+    primary: {
+        primary_1: "#4A6EEB",
+        primary_2: "#97D0D4",
+        primary_3: "#EBBB41",
+        primary_4: "#F2A9A7",
+    },
+    secondary: {
+        secondary_1: "#DFE7FF",
+        secondary_2: "#DBF1F1",
+        secondary_3: "#F5F0E0",
+        secondary_4: "#F1E6E6",
+    },
+    neutral: {
+        neutral_1: "#090500",
+    },
+    background: {
+        background_1: "#F6FAFF",
+    },
+    additional: {
+        additional_1: "#D46764",
+        additional_2: "#EFFAFD",
+    }
+  };
+  
+  export default colors;
