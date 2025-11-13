0da2aa573f2ae97f0c3d42a8fae66e2b22baa7e5
Richie Wee <56828805+Midnite1212@users.noreply.github.com>
2025-11-13 15:32:10 +0800

    1274 custom card component (#1275)
    * GH-1274: set the struct for the base and event card
    
    * GH-1274: set the struct for the base and event card
    
    * GH-1274: rename components folder to pages, add a proper components folder with base component struct

---
+++ ui/src/components/EventCard.js
@@ -0,0 +1,19 @@
+import BaseCard from './BaseCard';
+
+/**
+     * @typedef {object} Event
+     * @property {string} title
+     * @property {string} date
+     * @property {number} link
+     *
+
+     * Renders a card with a customizable card header and card body. The header can be any valid React node.
+     * The body can be stack of products 
+     * @param {Event} eventData - An array of product objects.
+     * @returns {React.ReactNode} The total price.
+*/
+const EventCard = ({eventData}) => {
+  return <BaseCard title="Event" />;
+};
+
+export default EventCard;
