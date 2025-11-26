89bc9b18a2f4aadb862777da103107e69222e07e
kuritsuan <133067409+kuritsuan@users.noreply.github.com>
2025-11-27 01:12:42 +0800

    GH-1286: remove DM Serif Display from the font list

--- ui/src/theme/foundations/fonts.js
+++ ui/src/theme/foundations/fonts.js
@@ -1,7 +1,7 @@
 
 
 const fonts = {
-    heading: `'DM Serif Display','Inter', -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol"`,
+    heading: `'Inter', -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol"`,
     body: `'Manrope','Inter', -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol"`,
 };
   
