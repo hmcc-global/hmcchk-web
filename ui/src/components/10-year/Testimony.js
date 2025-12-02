06d5e22bf70c98f54745d33bc3fb6564504734cb
Baeck016 <73063819+Baeck016@users.noreply.github.com>
2025-12-02 14:17:08 +0800

    GH-1250: add 10 to text

--- ui/src/components/10-year/Testimony.js
+++ ui/src/components/10-year/Testimony.js
@@ -5,7 +5,23 @@ import { tenYearTheme } from './theme';
 const Testimony = () => {
   return (
     <Flex direction="column" {...tenYearTheme.components.fullPageSection}>
-      <Text {...tenYearTheme.typography.h1}>Years The Memories</Text>
+      <Text {...tenYearTheme.typography.h1}>
+        <Text
+          {...tenYearTheme.typography.hero}
+          fontFamily="Qindom"
+          fontSize={['50px', '90px']}
+          textColor="#95CFFF"
+          display="inline"
+          position="relative"
+          lineHeight="inherit"
+          verticalAlign="middle"
+          right="-0.5rem"
+          zIndex={1}
+        >
+          10
+        </Text>
+        Years The Memories
+      </Text>
       <iframe
         src="https://padlet.com/embed/8z5rbl41cl1ji4ax"
         width={['100%', '80%']}
