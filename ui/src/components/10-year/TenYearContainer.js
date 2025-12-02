ea8f8febb90049e51637e6706ec7ebdcf0063957
Baeck016 <73063819+Baeck016@users.noreply.github.com>
2025-12-02 14:17:07 +0800

    GH-1248: Finish Hero Section

--- ui/src/components/10-year/TenYearContainer.js
+++ ui/src/components/10-year/TenYearContainer.js
@@ -1,9 +1,11 @@
-import { Flex, Box } from '@chakra-ui/react';
+import { Flex, Box, VStack } from '@chakra-ui/react';
 import TenYearVideo from './TenYearVideo';
 import OfferingSection from './OfferingSection';
 import ClosingRemarks from './ClosingRemarks';
 
 import { tenYearTheme } from './theme';
+
+import HeroSection from './HeroSection';
 
 const TenYearContainer = () => {
   const backgroundImage = `${process.env.PUBLIC_URL}/images/10-year/10y_bg.png`;
@@ -19,6 +21,7 @@ const TenYearContainer = () => {
       py={10}
       align="center"
     >
+      <HeroSection />
       <Box w="100%" px={[4, 8]}>
         <TenYearVideo />
       </Box>
