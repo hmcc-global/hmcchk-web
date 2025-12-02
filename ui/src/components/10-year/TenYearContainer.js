007021cf6e20aae960bfd209f058bd286702cbb8
Baeck016 <73063819+Baeck016@users.noreply.github.com>
2025-12-02 14:17:09 +0800

    GH-1254: changed font, bg and padding space

--- ui/src/components/10-year/TenYearContainer.js
+++ ui/src/components/10-year/TenYearContainer.js
@@ -11,33 +11,35 @@ const TenYearContainer = () => {
   const backgroundImage = `${process.env.PUBLIC_URL}/images/10-year/10y_bg.png`;
 
   return (
-    <Flex
-      direction="column"
-      bgColor={tenYearTheme.colors.background}
-      backgroundImage={`url(${backgroundImage})`}
-      backgroundSize="cover"
-      backgroundPosition="center"
-      backgroundRepeat="no-repeat"
-      align="center"
-    >
+    <Flex direction="column" bgColor={tenYearTheme.colors.background}>
       <HeroSection />
-      <TenYearTimeline />
-      <Testimony />
-      <Box w="100%" px={[4, 8]}>
-        <TenYearVideo />
-      </Box>
+      <Flex
+        direction="column"
+        backgroundImage={`url(${backgroundImage})`}
+        backgroundSize="cover"
+        backgroundPosition="center"
+        backgroundRepeat="no-repeat"
+        align="center"
+        height="100%"
+      >
+        <TenYearTimeline />
+        <Testimony />
+        <Box w="100%" px={[4, 8]}>
+          <TenYearVideo />
+        </Box>
 
-      <OfferingSection
-        title="Years: More To Come"
-        subtitle="For a decade, we've witnessed God's       ransformative work in and through HMCC of Hong Kong. 
+        <OfferingSection
+          title="Years: More To Come"
+          subtitle="For a decade, we've witnessed God's       ransformative work in and through HMCC of Hong Kong. 
 
 As we celebrate this 10-year milestone, we invite you to invest in the next chapter. 
 Your generosity will enable us to respond to God’s call. We pray to see more lives being transformed, LIFE Groups multiplied, churches planted, people being sent out for global missions, and so much more.
 
 Join us to see God transforming lives and transforming the world through HMCC-HK in the coming years! "
-      />
+        />
 
-      <ClosingRemarks />
+        <ClosingRemarks />
+      </Flex>
     </Flex>
   );
 };
