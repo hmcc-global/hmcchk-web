4abd65b3dcbb11abce3814b1aa93cc9832b45f1e
cornyachtsorry <andrea.justkidding@gmail.com>
2025-12-02 14:17:07 +0800

    GH-1242: Edit the mobile and tablet version

--- ui/src/components/10-year/ClosingRemarks.js
+++ ui/src/components/10-year/ClosingRemarks.js
@@ -14,8 +14,9 @@ import { tenYearTheme, getTypography } from './theme';
 const ClosingRemarks = () => {
   return (
     <Box>
-      <VStack gap={tenYearTheme.spacing.section} mt='3rem'>
-        <Flex gap="10" align="center">
+      <VStack gap="4rem" my='3rem' mx="2rem">
+        {/* Desktop and Tablet version */}
+        <Flex gap="10" align="center" display={['none', 'none', 'flex']}>
           <Text
             textAlign={'Center'}
             {...getTypography('h1')}
@@ -44,8 +45,42 @@ const ClosingRemarks = () => {
             </Text>
           </Flex>
         </Flex>
+
+        {/* Mobile version */}
         <Text
           textAlign={'Center'}
+          display={['block', 'none', 'none']}
+          {...getTypography('h1')}
+          lineHeight="161%"
+        >
+          Celebrating a decade of<br />
+          <Flex align="center" gap="2" position="relative" justify="center" mt="2">
+            <Image
+              src={process.env.PUBLIC_URL + '/images/10-year/t.svg'}
+              h="90px"
+              w="auto"
+              alt="T"
+              transform="translateY(-2px)"
+              position="absolute"
+              zIndex="0"
+              left="25px"
+            />
+            <Text
+              textAlign={'Center'}
+              position="relative"
+              zIndex="1"
+              ml="5px"
+              {...getTypography('h1')}
+              lineHeight="161%"
+            >
+              ransformation
+            </Text>
+          </Flex>
+        </Text>
+        {/* Desktop and Tablet version */}
+        <Text
+          textAlign={'Center'}
+          display={['none', 'block', 'block']}
           {...getTypography('h1')}
         >
           Thank you for being a part of HMCC Hong Kong in<br />
@@ -73,18 +108,42 @@ const ClosingRemarks = () => {
             </Text>
           </Box>
         </Text>
-      </VStack>
 
-      <Image
-        display={['none', 'none', 'block']}
-        src={process.env.PUBLIC_URL + '/images/10-year/ClosingRemarks/10.svg'}
-        h="2em"
-        opacity="100"
-        width="56.281px"
-        height="65.116px"
-        flexShrink={0}
-      />
-      <Box shadow="0 0 30px rgba(139,192,236,0.53)" />
+        {/* Mobile version */}
+        <Text
+          textAlign={'Center'}
+          display={['block', 'none', 'none']}
+          {...getTypography('h2')}
+          lineHeight="161%"
+        >
+          Thank you for being a<br />
+          part of HMCC Hongkong<br />
+          in the past{' '}
+          <Box as="span" position="relative" display="inline-block">
+            <Image
+              src={process.env.PUBLIC_URL + '/images/10-year/10.svg'}
+              h="8rem"
+              w="auto"
+              alt="10"
+              position="absolute"
+              zIndex="0"
+              left="22%"
+              top="40%"
+              transform="translate(-50%, -50%)"
+            />
+            <Text
+              as="span"
+              position="relative"
+              zIndex="1"
+              ml="50px"
+              {...getTypography('h1')}
+              lineHeight="161%"
+            >
+              years
+            </Text>
+          </Box>
+        </Text>
+      </VStack>
     </Box>
   );
 };
