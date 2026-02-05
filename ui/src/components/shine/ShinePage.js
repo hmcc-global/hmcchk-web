a823658d8f05aef8a2c9bbd4af83cb60d7a989fc
Dre <8019899+cornyachtsorry@users.noreply.github.com>
2026-02-05 13:24:17 +0800

    GH-1297:  Shine Ministry - Past Opportunities Section (#1305)
    * GH-1297: Past Opportunities Section
    
    * Add the rolling effect to the opportunities text
    
    * GH-1297: Add placeholder images for now
    
    * GH-1297: Make the fade slower
    
    * GH-1297: Put the pictures on
    
    * GH-1297: Make the mobile version
    
    * GH-1297: Add Meifoo Healthcare Outreach phtoos
    
    * GH-1297: Add comments for Albert
    
    * GH-1297: Allow the images to be tappable on mobile
    
    * GH-1297: Change px to rem
    
    * GH-1297: make the images clickable for desktop too
    
    * GH-1297: make the images dimension smaller in mobile
    
    * GH-1297: Compress images
    
    * GH-1297: Adjusting the opportunities section in mobile
    
    * GH-1297: Changed image scale for mobile
    
    * GH-1297: Fix the margins and fonts of the sections

--- ui/src/components/shine/ShinePage.js
+++ ui/src/components/shine/ShinePage.js
@@ -2,6 +2,7 @@ import { Flex, Box } from '@chakra-ui/react';
 import HeroSection from './HeroSection';
 import ValuesSection from './ValuesSection';
 import ApproachSection from './ApproachSection';
+import PastOpportunitiesSection from './PastOpportunitiesSection';
 
 const ShinePage = (props) => {
   return (
@@ -17,13 +18,14 @@ const ShinePage = (props) => {
         px={{ base: 4, md: 6, lg: 8 }}
         gap={{ base: '3', md: '6', lg: '8' }}
       >
-        <Box marginBottom={40}>
+        <Box marginBottom={{ base: 10, md: 20 }}>
           <ValuesSection />
         </Box>
-        <Box marginBottom={40}>
+        <Box marginBottom={{ base: 10, md: 20 }}>
           <ApproachSection />
         </Box>
         {/* Add components and content for the Shine page here */}
+        <PastOpportunitiesSection />
       </Flex>
     </Flex>
   );
