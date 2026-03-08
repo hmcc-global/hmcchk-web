e4dbfe8514d16d9c32750cd8ad116e2dfed431ae
Triangulr <hello@axesys.xyz>
2026-03-08 22:44:42 +0800

    GH-1327: Easter banner revision

---
+++ ui/src/components/easter/EasterBanner.js
@@ -0,0 +1,89 @@
+import React from 'react';
+import {
+  Box,
+  Grid,
+  GridItem,
+  Heading,
+  Image,
+  Stack,
+  Text,
+  chakra,
+} from '@chakra-ui/react';
+
+const EasterBanner = () => {
+  return (
+    <>
+      <Heading
+        as="h1"
+        fontFamily="'DM Serif Display', Georgia, serif"
+        fontStyle="italic"
+        fontWeight="400"
+        fontSize={{ base: '2.25em', lg: '3.75em' }}
+        lineHeight={{ base: '1.03', lg: '0.97' }}
+        letterSpacing={{ base: '-0.028em', lg: '-0.033em' }}
+        bgGradient="linear(to-r, #4C004A, #A62E5E)"
+        bgClip="text"
+        pb={2}
+        mb="0.5em"
+      >
+        Passion Week 2026:{' '}
+        <chakra.span display={{ base: 'block', lg: 'inline' }}>Living Hope</chakra.span>
+      </Heading>
+
+      <Grid
+        templateColumns={{ base: '1fr', md: '5fr 6fr' }}
+        gap={{ base: '1.66em', md: '2.5em' }}
+        alignItems="center"
+      >
+        <GridItem>
+          <Box
+            overflow="hidden"
+            bg="gray.100"
+            w="full"
+            sx={{ aspectRatio: '16/9' }}
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+          >
+            <Image
+              src={process.env.PUBLIC_URL + '/images/easter/ad.png'}
+              alt="Living Hope graphic"
+              w="full"
+              h="full"
+              objectFit="cover"
+            />
+          </Box>
+        </GridItem>
+
+        <GridItem>
+          <Stack spacing={{ base: '1.0625em', lg: '2em' }} textAlign="center">
+            <Text
+              textStyle="instrument_serif_bold"
+              fontSize={{ base: '1.125em', lg: '1.875em' }}
+              color="#A81D4B"
+              lineHeight={{ base: 'normal', lg: '1.07' }}
+            >
+              Romans 15:13 (NIV), &ldquo;May the God of hope fill you with all joy and peace as
+              you trust in him, so that you may overflow with hope by the power of the Holy
+              Spirit.&rdquo;
+            </Text>
+
+            <Text
+              textStyle="manrope"
+              fontSize={{ base: '0.875em', lg: '1.25em' }}
+              color="black"
+              lineHeight="normal"
+            >
+              This Easter, we want to experience the living hope we have in Jesus Christ.
+              Let&rsquo;s believe that Jesus is the reason for our hope, demonstrated through the
+              cross and His resurrection. As we have received this hope we can overflow with hope,
+              joy, and peace in our lives!
+            </Text>
+          </Stack>
+        </GridItem>
+      </Grid>
+    </>
+  );
+};
+
+export default EasterBanner;
