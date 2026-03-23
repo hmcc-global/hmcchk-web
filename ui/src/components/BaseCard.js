e6724a4d100e173950baf0f7acfbbe9a3e5f0b0d
Richie Wee <56828805+Midnite1212@users.noreply.github.com>
2026-03-23 20:27:27 +0800

    1274 custom card component (#1275)
    * GH-1274: set the struct for the base and event card
    
    * GH-1274: set the struct for the base and event card
    
    * GH-1274: rename components folder to pages, add a proper components folder with base component struct

---
+++ ui/src/components/BaseCard.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Card } from '@chakra-ui/react';
+/**
+
+     * Renders a card with a customizable card header and card body. The header can be any valid React node.
+     * The body can be stack of text links or any other react nodes. 
+     * @param {React.ReactNode} title - The title to display in the card.
+     * @returns {React.ReactNode} The total price.
+*/
+
+const BaseCard = ({ title }) => {
+  return <Card></Card>;
+};
+
+export default BaseCard;
