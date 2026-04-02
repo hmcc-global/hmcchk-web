1b0ec6bc934538743071589f0b4b894569a66066
EnochSauKingLam <enoch.k.lam@gmail.com>
2026-04-02 12:17:24 +0800

    GH-1317: Finished hero

--- ui/src/components/10y-commitment/TenYearCommitmentContainer.js
+++ ui/src/components/10y-commitment/TenYearCommitmentContainer.js
@@ -1,7 +1,14 @@
-import { Flex } from '@chakra-ui/react';
+import { Flex, useBreakpointValue } from '@chakra-ui/react';
+import HeroSection from './HeroSection';
 
-const TenYearCommitmentContainer = () => {
-  return <Flex direction="column" bgColor="#F6FAFF"></Flex>;
+const TenYearCommitmentContainer = (props) => {
+  const isMobile = useBreakpointValue({ base: true, md: false });
+
+  return (
+    <Flex direction="column" bgColor="#F6FAFF">
+      <HeroSection />
+    </Flex>
+  );
 };
 
 export default TenYearCommitmentContainer;
