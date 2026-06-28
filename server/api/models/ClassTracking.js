db4951156ccb156976c646735b311e401e54150e
kuritsuan <133067409+kuritsuan@users.noreply.github.com>
2026-06-28 22:33:17 +0800

    GH-1385: change ClassTracking model

--- server/api/models/ClassTracking.js
+++ server/api/models/ClassTracking.js
@@ -4,54 +4,24 @@ module.exports = {
     userId: {
       type: 'string',
     },
-    // classId: {
-    //   type: 'string',
-    //   unique: true,
-    // },
     formId: {
       type: 'string',
+      unique: true,
     },
     submissionId: {
       model: 'Submission',
       unique: true,
     },
-    // createdAt: {
-    //   type: 'string',
-    // },
-    // updatedAt: {
-    //   type: 'string',
-    // },
+    createdAt: {
+      type: 'string',
+    },
+    updatedAt: {
+      type: 'string',
+    },
     tracker: {
       type: 'json',
-      courses: {
-        type: 'json',
-        columnType: 'array',
-        materialId: {
-          type: 'string',
-          unique: true,
-        },
-        platform: {
-          type: 'string',
-        },
-        type: {
-          type: 'string',
-          isIn: ['online', 'offline'],
-          defaultsTo: 'online',
-        },
-        status: {
-          type: 'string',
-          isIn: ['Not Started', 'In Progress', 'Completed'],
-          defaultsTo: 'Not Started',
-        },
-        startedAt: {
-          type: 'string',
-        },
-        completedAt: {
-          type: 'string',
-        },
-        lastAccessedAt: {
-          type: 'string',
-        },
+      defaultsTo: {
+        courses: [],
       },
     },
   },
