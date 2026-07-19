6db0b293b2ff36e2b0a9d5fb9dd59c2999a3f2d6
Richie Wee <56828805+Midnite1212@users.noreply.github.com>
2026-07-19 22:23:22 +0800

    1378 10y reproduce grid animation (#1383)
    * BT-1378: init grid animation
    
    * GH-1378: improve reproduct grid animation

--- ui/src/components/10y-commitment/three-fold/ReproducePanel.js
+++ ui/src/components/10y-commitment/three-fold/ReproducePanel.js
@@ -1,6 +1,6 @@
-import { AspectRatio, Box, Image } from '@chakra-ui/react';
+import React from 'react';
 import CommitmentPanel from './CommitmentPanel';
-import { TYC_IMG } from '../constants';
+import ReproduceDotGrid from './ReproduceDotGrid';
 
 const ReproducePanel = ({ onPrev, onNext }) => {
   return (
@@ -10,17 +10,7 @@ const ReproducePanel = ({ onPrev, onNext }) => {
       onNext={onNext}
       body="We will intentionally raise up 1,000 faithful followers of Christ, men and women, who are equipped to serve and go wherever God calls them."
     >
-      {/* The SVG is exported with preserveAspectRatio="none", so it must live in a
-          box matching its 479.421 x 422.25 viewBox or it stretches/squashes. */}
-      <Box w="100%" maxW="560px" px={{ base: 2, md: 4 }}>
-        <AspectRatio ratio={479.421 / 422.25} w="100%">
-          <Image
-            src={`${TYC_IMG}/reproduce/diagram.svg`}
-            alt="Diagram showing one disciple reproducing into many"
-            loading="lazy"
-          />
-        </AspectRatio>
-      </Box>
+      <ReproduceDotGrid />
     </CommitmentPanel>
   );
 };
